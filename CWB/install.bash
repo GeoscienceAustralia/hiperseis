bbc7bcd14cffd343dfdaba61bb6a63939718485b
Fei Zhang <fei.zhang@ga.gov.au>
2017-08-07 14:51:50 +1000

    updates

--- CWB/install.bash
+++ CWB/install.bash
@@ -1,8 +1,14 @@
-#! /usr/bin/bash
+#! /bin/bash
 
 sudo yum update
+sudo yum install git
+
 sudo yum install xorg-x11-xauth.x86_64 xorg-x11-server-utils.x86_64 dbus-x11.x86
 sudo yum install xeyes
 
 # exit and 
 # ssh -X login again to test xeyes
+
+
+sudo yum install mysql56*
+sudo yum install  MySQL-python27.x86_64
