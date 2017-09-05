55875bb7677934e4b0e81b2035266b21eabe5ff3
Sudipta Basak <basaks@gmail.com>
2017-09-05 16:32:33 +1000

    yaml config and more boilerplate

---
+++ seismic/config.py
@@ -0,0 +1,28 @@
+import logging
+import yaml
+from os import path
+
+log = logging.getLogger(__name__)
+
+
+class Config:
+    """Class representing the global configuration of the picking and
+    location model to simulate.
+
+    Parameters
+    ----------
+    yaml_file : string
+        The path to the yaml config file. For details on the yaml schema
+        see the passive-seismic documentation
+    """
+
+    def __init__(self, yaml_file):
+        with open(yaml_file, 'r') as f:
+            s = yaml.load(f)
+
+        self.name = path.basename(yaml_file).rsplit(".", 1)[0]
+        self.inputs = s['inputs']
+
+        for i in self.inputs:
+            if i['type'] == 'miniseed':
+                self.miniseeds = [path.abspath(p['file']) for p in i['files']]
--- setup.py
+++ setup.py
@@ -56,7 +56,8 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'anulog = convert_logs.decode_datfile:anulog'
+            'anulog = convert_logs.decode_datfile:anulog',
+            'seismic = seismic.scripts.picker:cli'
         ]
     },
 
