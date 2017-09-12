62fe5c17a07b7b27d82c0cf22afc5c436e06e75c
Sudipta Basak <basaks@gmail.com>
2017-09-12 15:27:07 +1000

    reuse fixtures and added test data

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,36 +8,17 @@ TESTS = os.path.dirname(__file__)
 DATA = os.path.join(TESTS, 'mocks', 'data')
 
 
-@pytest.fixture
-def conf():
-    return {'inputs': [],
-            'picker': None}
-
-miniseeds = {'files': [{'file': os.path.join(DATA, 'ev0_6.a01.gse2')},
-                       {'file': os.path.join(DATA, 'ev0_6.a02.gse2')}],
-             'name': 'my miniseed files', 'type': 'miniseed'}
-
-events = {'name': 'my events',
-          'events': [{'event_id': 'ga2017abcdefg'},
-                     {'event_id': 'ga2017hijklmn'}],
-          'type': 'events'}
-
-times = {'times': {'end_time': datetime.datetime(2017, 3, 28, 17, 18, 30),
-                   'start_time': datetime.datetime(2017, 3, 28, 16, 18, 30)},
-         'name': 'my time range', 'type': 'time'}
-
-
-@pytest.fixture(params=[miniseeds, events, times])
-def custom_conf(conf, request):
-    conf['inputs'].append(request.param)
+@pytest.fixture(params=['miniseeds', 'events', 'times'])
+def custom_conf(conf, params_dict, request):
+    conf['inputs'].append(params_dict[request.param])
     return conf
 
 
-@pytest.fixture(params=[times, events, miniseeds])
-def custom_conf_2s(conf, request):
-    all_supported_formats = [miniseeds, events, times]
+@pytest.fixture(params=['miniseeds', 'events', 'times'])
+def custom_conf_2s(conf, params_dict, request):
+    all_supported_formats = ['miniseeds', 'events', 'times']
     all_supported_formats.remove(request.param)
-    conf['inputs'] += all_supported_formats
+    conf['inputs'] += [params_dict[a] for a in all_supported_formats]
     return conf
 
 
@@ -50,8 +31,9 @@ def test_conflict_2s(random_filename, custom_conf_2s):
         config.Config(yaml_file)
 
 
-def test_conflict(random_filename, conf):
-    conf['inputs'] += [miniseeds, events, times]
+def test_conflict(random_filename, conf, params_dict):
+    conf['inputs'] += [params_dict[a]
+                       for a in ['miniseeds', 'events', 'times']]
     yaml_file = random_filename(ext='.yaml')
     with open(yaml_file, 'w') as outfile:
         yaml.dump(conf, outfile, default_flow_style=False)
