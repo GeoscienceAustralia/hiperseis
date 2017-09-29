27888acdbdb9d2c5b72ff38704eef71a740692c4
Niket Chhajed <niket.chhajed@ga.gov.au>
2017-09-29 13:36:58 +1000

    fixes to cwb server install script

--- CWB/install.bash
+++ CWB/install.bash
@@ -1,26 +1,28 @@
 #! /bin/bash
 
-sudo yum update
-sudo yum install git
-sudo yum install wget
+sudo yum update -y
+sudo yum install git -y
+sudo yum install wget -y
 
 
-sudo yum install xorg-x11-xauth.x86_64 xorg-x11-server-utils.x86_64 dbus-x11.x86
-sudo yum install xeyes
+sudo yum install xorg-x11-xauth.x86_64 xorg-x11-server-utils.x86_64 dbus-x11.x86 -y
+sudo yum install xeyes -y
+
+sudo yum install expect expectk -y
 
 # exit and 
 # ssh -X login again to test xeyes
 
 
-sudo yum install mysql56*
-sudo yum install  MySQL-python27.x86_64
+sudo yum install mysql56* -y
+sudo yum install  MySQL-python27.x86_64 -y
 
 sudo chkconfig mysqld on
 
 sudo service mysqld start
 
 # set mysql root passwd
-mysqladmin -u root password 'CWBr00tpwd'
+mysqladmin -u root password \"CWBr00tpwd\"
 # Alternatively run   /usr/libexec/mysql56/mysql_secure_installation
 
 #verify root pass setup, check version
@@ -29,66 +31,43 @@ mysqladmin -u root -pCWBr00tpwd version
 
 # create users and set permissions
 sudo adduser vdl
+sudo passwd -d vdl
+
 # make vdl home group accessible
 sudo chmod -R 750 /home/vdl
 
-sudo adduser davidk
-sudo usermod -g vdl davidk
+sudo adduser ketchum
+sudo usermod -g vdl ketchum
 sudo adduser sudipta
 sudo usermod -g vdl sudipta
+sudo adduser niket
+sudo usermod -g vdl niket
+sudo adduser fei
+sudo usermod -g vdl fei
 
 sudo usermod -g vdl ec2-user
 
+sudo cp /etc/sudoers /tmp/sudoers.bak
+
+sudo chmod 755 /tmp/sudoers.bak
+sudo chown ec2-user /tmp/sudoers.bak
+sudo chgrp ec2-user /tmp/sudoers.bak
+
+sudo echo "niket	ALL=(vdl)	ALL" >> /tmp/sudoers.bak
+sudo echo "ketchum	ALL=(vdl)	ALL" >> /etc/sudoers.bak
+sudo echo "sudipta	ALL=(vdl)	ALL" >> /etc/sudoers.bak
+sudo echo "fei	ALL=(vdl)	ALL" >> /etc/sudoers.bak
+sudo echo "vdl	ALL=(vdl)	NOPASSWD: ALL" >> /etc/sudoers.bak
+
+sudo visudo -cf /tmp/sudoers.bak
+if [ $? -eq 0 ]; then
+	sudo cp /tmp/sudoers.bak /etc/sudoers
+else
+	echo "Could not modify /etc/sudoers file. Please do this manually."
+fi
 
 # become user vdl and get the software from USGS ftp site
-sudo su - vdl
-mkdir TEMP
-
-# cd into the TEMP dir and do the following
-cd TEMP
-wget ftp://hazards.cr.usgs.gov/CWBQuery/EdgeCWBRelease.tar.gz
-
-tar -zxvf EdgeCWBRelease.tar.gz
-tar -xvf bin_release.tar 
-tar -xvf scripts_release.tar 
-
-mv bin ~vdl
-cp -p Jars/* ~vdl/bin/
-chmod -R 755 ~vdl/bin
-
-# make sure java class lib has jars
-ln -s  /home/vdl/bin bin/lib   
-
-cp -r scripts ~vdl/
-
-cp .bash* ~vdl/
-
-cd ~vdl
-
-# the scripts/installCWBRelease.bash has issues. as a reference only
-dirs="bin log LOG EDGEMOM config SEEDLINK EW log/MSLOG log/CWBLOG log/q330";
-# create any missing directories
-for dir in $dirs; do
-if [ ! -e ${dir} ]; then
-        echo "Create ${dir} directory"
-        mkdir ${dir} 
-fi
-done
-
-tar -xvf TEMP/dbconn.tar 
-#.dbconn/dbconn.conf
-#.dbconn/dbconn_mysql_localhost_3306.conf
-#.dbconn/dbconn_mysql_localhost.conf
-
-# creat databases - require type in mysql user info: root, passwd, y
-cd scripts/INSTALL/
-bash ./makedb.bash   # if run success, this will create a few db
-
-bash createketchum.bash
-# type the mysqlroot passpwd
-
-bash createvdlro.bash
-# type the mysqlroot passpwd
+sudo -u vdl bash install-vdl-bash
 
 # restart the server
 
@@ -105,16 +84,14 @@ sudo shutdown -r now
 
 # database ops users setup by X-win GUI:
 #this will create 2 .dbconnn/*.conf  encryptedfile 
- ~vdl/scripts/INSTALL/dbsetup
 
-# created proper ~vdl/*.prop files
+# created proper ~vdl/*.prop files0
 #ip-172-31-2-68::vdl:~>ls *.prop
 #edgecon.prop*  edge_ip-172-31-2-68.prop*  edge.prop*	metagui.prop  msread.prop*  query.prop*  queryserver.prop*
-ip
 
 # create a role file
-ip-172-31-2-68::vdl:~>ls roles_ip-172-31-2-68*
-roles_ip-172-31-2-68
+#ip-172-31-2-68::vdl:~>ls roles_ip-172-31-2-68*
+#roles_ip-172-31-2-68
 
 # Finally install the crontab.
 # install crontab under the user vdl
