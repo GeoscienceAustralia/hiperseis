275605db64e17ae8cbc7becb44ce7b509d9366eb
Niket Chhajed <chhajed@gmail.com>
2017-10-12 12:30:11 +1100

    do it after git install

--- sc3/createenv.sh
+++ sc3/createenv.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-CWD=`pwd`
-cd $HOME
-git clone https://github.com/GeoscienceAustralia/passive-seismic.git
-cd $CWD
-
 # manage env variables
 if [ -z ${ILOCROOT+x} ]; then
     cat $HOME/passive-seismic/sc3/sc3_envs.txt >> $HOME/.bashrc;
