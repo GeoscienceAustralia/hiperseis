17ca914381bbc058678457581b62487e153730e2
Niket Chhajed <chhajed@gmail.com>
2017-11-03 13:52:36 +1100

    alexei feedback incorporated; bug fixes

--- engdahl/extractArrivals.py
+++ engdahl/extractArrivals.py
@@ -9,7 +9,27 @@ from obspy import UTCDateTime
 from obspy.core.event.header import PickOnset, PickPolarity
 from obspy.core.event import Amplitude, Event, Magnitude, Origin, Pick,\
    OriginQuality, CreationInfo, WaveformStreamID, ResourceIdentifier, Arrival
+
+'''
+# This code is for associating stations with networks
+# Commenting it out for now.
 from obspy.clients.fdsn import Client
+
+      client = Client(base_url='https://service.iris.edu/fdsnws/', debug=True)
+      if not client.services.has_key('station') or not client.services.has_key('station'):
+         print('The fdsn client of obspy was not initiated successfully. The network code will not be populated.')
+      else:
+         inv = client.get_stations(station=arrParser.station, level='network')
+         networkCodes = [cod for cod in set([net.code for net in inv.networks]) if cod != 'SY']
+         if len(networkCodes) > 1:
+            print('Multiple network codes returned for the station: ' + arrParser.station +
+                  '. Continuing with this arrival without network code assignment. ' + arrParser.__dict__)
+         elif len(networkCodes) == 0:
+            print('No network found for this station: ' + arrParser.station +
+                  '. Continuing with this arrival without network code assignment. ' + arrParser.__dict__)
+         else:
+            arrParser.network = networkCodes[0]
+'''
 
 global eventCount
 eventCount = 1
@@ -26,6 +46,13 @@ def setEventData(eventParser, arrivals, count):
                                 creation_time=UTCDateTime(),
                                 agency_uri=ResourceIdentifier(id='smi:engdahl.ga.gov.au/ga-engdahl'),
                                 agency_id='ga-engdahl')
+
+#   magnitudeSurface = Magnitude(resource_id=ResourceIdentifier(id='smi:engdahl.ga.gov.au/origin/'+str(originCount)+'#netMag.Ms'),
+#                         mag=eventParser.ms,
+#                         magnitude_type='Ms',
+#                         origin_id=ResourceIdentifier(id='smi:engdahl.ga.gov.au/origin/'+str(originCount)),
+#                         azimuthal_gap=eventParser.openaz2,
+#                         creation_info=creation_info)
    origin = Origin(resource_id=ResourceIdentifier(id='smi:engdahl.ga.gov.au/origin/'+str(originCount)),
                    time=UTCDateTime(int(str(2000 + int(eventParser.iyr))), int(eventParser.mon), int(eventParser.iday),
                                     int(eventParser.ihr), int(eventParser.min), int(eventParser.sec.split('.')[0]),
@@ -42,6 +69,14 @@ def setEventData(eventParser, arrivals, count):
                             azimuthal_gap=eventParser.openaz2),
                    evaluation_mode='automatic',
                    creation_info=creation_info)
+
+   magnitude = Magnitude(resource_id=ResourceIdentifier(id='smi:engdahl.ga.gov.au/origin/'+str(originCount)+'#netMag.Mb'),
+                         mag=eventParser.mb,
+                         magnitude_type='Mb',
+                         origin_id=ResourceIdentifier(id='smi:engdahl.ga.gov.au/origin/'+str(originCount)),
+                         azimuthal_gap=eventParser.openaz1,
+                         creation_info=creation_info)
+
    originCount += 1
 
    pickList = []
@@ -50,6 +85,7 @@ def setEventData(eventParser, arrivals, count):
    for arrParser in arrivals:
       pickOnset = None
       pol = None
+
       if arrParser.phase and arrParser.phase.lower().startswith('p') and arrParser.year and arrParser.month and arrParser.day:
          pPhaseArrival = arrParser
       else:
@@ -57,8 +93,15 @@ def setEventData(eventParser, arrivals, count):
          arrParser.day = pPhaseArrival.day
          arrParser.month = pPhaseArrival.month
          arrParser.station = pPhaseArrival.station
+         arrParser.delta = pPhaseArrival.delta
+         arrParser.dtdd = pPhaseArrival.dtdd
+         arrParser.backaz = pPhaseArrival.backaz
+         arrParser.focalDip = pPhaseArrival.focalDip
+         arrParser.angleAzimuth = pPhaseArrival.angleAzimuth
+
       if arrParser.phase1 == 'LR' or arrParser.phase2 == 'LR':
          continue
+
       if arrParser.phase1.startswith('i'):
          pickOnset = PickOnset.impulsive
          if arrParser.fm == '+':
@@ -67,20 +110,6 @@ def setEventData(eventParser, arrivals, count):
             pol = PickPolarity.negative
       elif arrParser.phase1.startswith('e'):
          pickOnset = PickOnset.emergent
-      client = Client(base_url='https://service.iris.edu/fdsnws/', debug=True)
-      if not client.services.has_key('station') or not client.services.has_key('station'):
-         print('The fdsn client of obspy was not initiated successfully. The network code will not be populated.')
-      else:
-         inv = client.get_stations(station=arrParser.station, level='network')
-         networkCodes = [cod for cod in set([net.code for net in inv.networks]) if cod != 'SY']
-         if len(networkCodes) > 1:
-            print('Multiple network codes returned for the station: ' + arrParser.station +
-                  '. Continuing with this arrival without network code assignment. ' + arrParser.__dict__)
-         elif len(networkCodes) == 0:
-            print('No network found for this station: ' + arrParser.station +
-                  '. Continuing with this arrival without network code assignment. ' + arrParser.__dict__)
-         else:
-            arrParser.network = networkCodes[0]
 
       pick = Pick(resource_id=ResourceIdentifier(id='smi:engdahl.ga.gov.au/pick/'+str(pickCount)),
                   time=UTCDateTime(int(str(2000 + int(arrParser.year))), int(arrParser.month), int(arrParser.day),
@@ -103,7 +132,7 @@ def setEventData(eventParser, arrivals, count):
       pickList.append(pick)
 
       arrival = Arrival(pick_id=ResourceIdentifier(id='smi:engdahl.ga.gov.au/pick/'+str(pickCount-1)),
-                        phase=arrParser.phase[0],
+                        phase=arrParser.phase if arrParser.phase else None,
                         azimuth=arrParser.backaz if arrParser.backaz else None,
                         distance=arrParser.delta if arrParser.delta else None,
                         # if the * has some significance, it should be accounted for. ignoring for now.
@@ -124,7 +153,9 @@ def setEventData(eventParser, arrivals, count):
 
    event.picks = pickList
    event.origins = [origin, ]
+   event.magnitudes = [magnitude, ]
    event.preferred_origin_id = origin.resource_id
+   event.preferred_magnitude_id = magnitude.resource_id
    return event
 
 def getArrivalSections(hdfFile, outFile):
