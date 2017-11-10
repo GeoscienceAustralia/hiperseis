b574491181f044032c0b251e07077553523afef4
Sudipta Basak <basaks@gmail.com>
2017-11-10 16:50:40 +1100

    tests for cluster output

--- seismic/cluster/cluster.py
+++ seismic/cluster/cluster.py
@@ -28,7 +28,7 @@ Station = namedtuple('Station', 'station_code, latitude, longitude, '
               type=click.File(mode='w'), default='cluster_p.csv',
               help='Output P picks file')
 @click.option('-x', '--nx', type=int, default=1440,
-              help='number of segments from 0 to 360 degrees for longtitude')
+              help='number of segments from 0 to 360 degrees for longitude')
 @click.option('-y', '--ny', type=int, default=720,
               help='number of segments from 0 to 180 degrees for latitude')
 @click.option('-z', '--dz', type=float, default=25.0,
@@ -47,67 +47,64 @@ def cluster(events_dir, station_metadata, output_file, nx, ny, dz, verbosity):
     writer = csv.writer(output_file)
 
     for e in events:
+        process_event(e, stations, writer, nx, ny, dz)
 
-        # use timestamp as the event number
-        ev_number = int(e.creation_info.creation_time.timestamp*1e6)
 
-        origin = e.preferred_origin()
-        # other event parameters we need
+def process_event(event, stations, writer, nx, ny, dz):
 
-        ev_latitude = origin.latitude
-        ev_longitude = origin.longitude
-        ev_depth = origin.depth
+    # use timestamp as the event number
+    ev_number = int(event.creation_info.creation_time.timestamp * 1e6)
+    origin = event.preferred_origin()
 
-        dx = 360./nx
-        dy = 180./ny
+    # other event parameters we need
+    ev_latitude = origin.latitude
+    ev_longitude = origin.longitude
+    ev_depth = origin.depth
 
-        y = 90. - origin.latitude
+    dx = 360. / nx
+    dy = 180. / ny
+    y = 90. - origin.latitude
+    if origin.longitude > 0.:
+        x = origin.longitude
+    else:
+        x = origin.longitude + 360.
+    # endif
+    z = origin.depth
+    i = round(x / dx) + 1
+    j = round(y / dy) + 1
+    k = round(z / dz) + 1
+    event_block = (k - 1) * nx * ny + (j - 1) * nx + i
+    for arr in origin.arrivals:
+        sta_code = arr.pick_id.get_referred_object(
+        ).waveform_id.station_code
+        sta = stations[sta_code]
 
-        if origin.longitude > 0.:
-            x = origin.longitude
+        y = 90. - float(sta.latitude)
+        lon = float(sta.longitude)
+        if lon > 0:
+            x = lon
         else:
-            x = origin.longitude + 360.
+            x = lon + 360.
         # endif
 
-        z = origin.depth
+        z = 0.
 
-        i = round(x/dx) + 1
-        j = round(y/dy) + 1
-        k = round(z/dz) + 1
+        i = round(x / dx) + 1
+        j = round(y / dy) + 1
+        k = round(z / dz) + 1
+        station_block = (k - 1) * nx * ny + (j - 1) * nx + i
+        # phase_type == 1 if P else S
+        phase_type = 1 if arr.phase == 'P' else 2
+        if phase_type == 2:
+            assert arr.phase == 'S'
 
-        event_block = (k-1) * nx * ny + (j-1) * nx + i
-
-        for arr in origin.arrivals:
-            sta_code = arr.pick_id.get_referred_object(
-            ).waveform_id.station_code
-            sta = stations[sta_code]
-
-            y = 90. - float(sta.latitude)
-            lon = float(sta.longitude)
-            if lon > 0:
-                x = lon
-            else:
-                x = lon + 360.
-            # endif
-
-            z = 0.
-
-            i = round(x/dx) + 1
-            j = round(y/dy) + 1
-            k = round(z/dz) + 1
-            station_block = (k-1) * nx * ny + (j-1) * nx + i
-            # phase_type == 1 if P else S
-            phase_type = 1 if arr.phase == 'P' else 2
-            if phase_type == 2:
-                assert arr.phase == 'S'
-
-            writer.writerow([
-                event_block, station_block, arr.time_residual,
-                ev_number, ev_longitude, ev_latitude, ev_depth,
-                sta.longitude, sta.latitude,
-                (arr.pick_id.get_referred_object().time.timestamp -
-                 origin.time.timestamp),
-                phase_type])
+        writer.writerow([
+            event_block, station_block, arr.time_residual,
+            ev_number, ev_longitude, ev_latitude, ev_depth,
+            sta.longitude, sta.latitude,
+            (arr.pick_id.get_referred_object().time.timestamp -
+             origin.time.timestamp),
+            phase_type])
 
 
 def _read_stations(csv_file):
---
+++ tests/test_cluster.py
@@ -0,0 +1,24 @@
+import os
+import csv
+import numpy
+from obspy import read_events
+from seismic.cluster.cluster import process_event, _read_stations
+
+TESTS = os.path.dirname(__file__)
+stations_file = os.path.join(TESTS, 'mocks', 'inventory', 'stations.csv')
+saved_out = os.path.join(TESTS, 'mocks', 'events', 'ga2017qxlpiu.csv')
+
+
+def test_single_event_output(xml, random_filename):
+    output_file = random_filename(ext='.csv')
+    print(output_file)
+    with open(stations_file, 'r') as sta_f:
+        with open(output_file, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            process_event(read_events(xml)[0],
+                          stations=_read_stations(sta_f),
+                          writer=writer,
+                          nx=1440, ny=720, dz=25.0)
+    inputs = numpy.genfromtxt(saved_out, delimiter=',')
+    outputs = numpy.genfromtxt(output_file, delimiter=',')
+    numpy.testing.assert_array_almost_equal_nulp(inputs, outputs)
