569b5c7a5933861ca125f8bccbffb0b0d1a45c71
Sudipta Basak <basaks@gmail.com>
2017-11-13 16:48:32 +1100

    tests adapted due to change in function signature
    [ci skip]

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -10,14 +10,19 @@ saved_out = os.path.join(TESTS, 'mocks', 'events', 'ga2017qxlpiu.csv')
 
 
 def test_single_event_output(xml, random_filename):
-    output_file = random_filename(ext='.csv')
+    p_file = random_filename(ext='_p.csv')
+    s_file = random_filename(ext='_s.csv')
     with open(stations_file, 'r') as sta_f:
-        with open(output_file, 'w') as csvfile:
-            writer = csv.writer(csvfile)
-            process_event(read_events(xml)[0],
-                          stations=_read_stations(sta_f),
-                          writer=writer,
-                          nx=1440, ny=720, dz=25.0)
+        with open(p_file, 'w') as p_writer:
+            with open(s_file, 'w') as s_writer:
+                p_writer = csv.writer(p_writer)
+                process_event(read_events(xml)[0],
+                              stations=_read_stations(sta_f),
+                              p_writer=p_writer,
+                              s_writer=s_writer,
+                              nx=1440, ny=720, dz=25.0,
+                              wave_type='P S')
     inputs = numpy.genfromtxt(saved_out, delimiter=',')
-    outputs = numpy.genfromtxt(output_file, delimiter=',')
+    outputs = numpy.genfromtxt(p_file, delimiter=',')
     numpy.testing.assert_array_almost_equal(inputs, outputs)
+    assert os.path.exists(s_file)
