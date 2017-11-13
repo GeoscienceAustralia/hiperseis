053a0bee082b4f0c6dfd0e76ac5c8467b9875b61
Sudipta Basak <basaks@gmail.com>
2017-11-13 17:38:27 +1100

    number of arrivals are tested to be same as output file

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -5,6 +5,7 @@ from obspy import read_events
 from seismic.cluster.cluster import process_event, _read_stations
 
 TESTS = os.path.dirname(__file__)
+xmls = os.path.join(TESTS, 'mocks', 'events', '*.xml')
 stations_file = os.path.join(TESTS, 'mocks', 'inventory', 'stations.csv')
 saved_out = os.path.join(TESTS, 'mocks', 'events', 'ga2017qxlpiu.csv')
 
@@ -12,6 +13,8 @@ saved_out = os.path.join(TESTS, 'mocks', 'events', 'ga2017qxlpiu.csv')
 def test_single_event_output(xml, random_filename):
     p_file = random_filename(ext='_p.csv')
     s_file = random_filename(ext='_s.csv')
+    event = read_events(xml).events[0]
+    origin = event.preferred_origin()
     with open(stations_file, 'r') as sta_f:
         with open(p_file, 'w') as p_writer:
             with open(s_file, 'w') as s_writer:
@@ -25,4 +28,14 @@ def test_single_event_output(xml, random_filename):
     inputs = numpy.genfromtxt(saved_out, delimiter=',')
     outputs = numpy.genfromtxt(p_file, delimiter=',')
     numpy.testing.assert_array_almost_equal(inputs, outputs)
+
+    # s_file is created
     assert os.path.exists(s_file)
+
+    # make sure number of arrivals match that of output lines
+    assert len(origin.arrivals) == len(outputs)
+
+
+# def test_multiple_event_output(random_filename):
+#     p_file = random_filename(ext='_p.csv')
+#     s_file = random_filename(ext='_s.csv')
