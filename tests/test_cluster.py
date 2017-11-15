963b9150aa4e1585cf8d34c4f9446eb6db90f844
Sudipta Basak <basaks@gmail.com>
2017-11-15 20:27:51 +1100

    some refactoring and pep8

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -5,7 +5,7 @@ import numpy
 import pytest
 from obspy import read_events
 from seismic.cluster.cluster import (process_event,
-                                     _read_stations,
+                                     read_stations,
                                      process_many_events)
 
 TESTS = os.path.dirname(__file__)
@@ -21,6 +21,11 @@ def event_xml(request):
     return request.param
 
 
+@pytest.fixture(params=['P S'])
+def arr_type(request):
+    return request.param
+
+
 def test_single_event_output(xml, random_filename):
     p_file = random_filename(ext='_p.csv')
     s_file = random_filename(ext='_s.csv')
@@ -31,7 +36,7 @@ def test_single_event_output(xml, random_filename):
             with open(s_file, 'w') as s_writer:
                 p_writer = csv.writer(p_writer)
                 process_event(read_events(xml)[0],
-                              stations=_read_stations(sta_f),
+                              stations=read_stations(sta_f),
                               p_writer=p_writer,
                               s_writer=s_writer,
                               nx=1440, ny=720, dz=25.0,
@@ -44,7 +49,44 @@ def test_single_event_output(xml, random_filename):
     assert os.path.exists(s_file)
 
     # make sure number of arrivals match that of output lines
-    assert len(origin.arrivals) == len(outputs)
+    assert len(origin.arrivals) == len(outputs)  # no s arrivals for this event
+
+
+def test_single_event_arrivals(event_xml, random_filename, arr_type):
+    p_file = random_filename(ext='_p.csv')
+    s_file = random_filename(ext='_s.csv')
+    event = read_events(event_xml).events[0]
+    origin = event.preferred_origin()
+    with open(stations_file, 'r') as sta_f:
+        with open(p_file, 'w') as p_writer:
+            with open(s_file, 'w') as s_writer:
+                p_writer = csv.writer(p_writer)
+                process_event(read_events(event_xml)[0],
+                              stations=read_stations(sta_f),
+                              p_writer=p_writer,
+                              s_writer=s_writer,
+                              nx=1440, ny=720, dz=25.0,
+                              wave_type=arr_type)
+    outputs_p = numpy.genfromtxt(p_file, delimiter=',')
+    outputs_s = numpy.genfromtxt(s_file, delimiter=',')
+
+    stations = read_stations(open(stations_file, 'r'))
+
+    p_arrivals = []
+    s_arrivals = []
+    p, s = arr_type.split()
+    for arr in origin.arrivals:
+        sta_code = arr.pick_id.get_referred_object(
+            ).waveform_id.station_code
+        if sta_code in stations:
+            if arr.phase == p:
+                p_arrivals.append(arr)
+            if arr.phase == s:
+                s_arrivals.append(arr)
+
+    # make sure number of arrivals match that of output lines
+    assert len(p_arrivals) == len(outputs_p)
+    assert len(s_arrivals) == len(outputs_s)
 
 
 def test_sorted():
@@ -62,7 +104,7 @@ def test_multiple_event_output(random_filename):
 
     with open(stations_file, 'r') as sta_f:
         process_many_events(events,
-                            stations=_read_stations(sta_f),
+                            stations=read_stations(sta_f),
                             nx=1440, ny=720, dz=25.0,
                             wave_type='P S',
                             output_file=outfile)
