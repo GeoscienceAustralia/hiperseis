5b435ea5a48cf38a4d95fef0a0add9e61f77b42d
Sudipta Basak <basaks@gmail.com>
2017-11-18 10:02:04 +1100

    exception handling for 5 char station names

--- isc-inventory/parse_inventory.py
+++ isc-inventory/parse_inventory.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import
 import struct
 import os
+from obspy import UTCDateTime
 isc_format1 = '4s12s10s8s20s20s'
 isc_format2 = '4s12s10s8s20s'
 isc_format3 = '5s12s10s8s20s20s'
 isc_format4 = '5s12s10s8s20s'
-alt_isc_format2 = '5s11s10s8s20s'
+five_char_isc_format1 = '5s11s10s8s20s20s'
+five_char_isc_format2 = '5s11s10s8s20s'
 
 """
 The first section is the data from the (ISC-)EHB station list showning station 
@@ -20,7 +22,7 @@ the sampling rates.
 isc_file_1 = os.path.join('isc-inventory', 'ehb.stn')
 isc_file_2 = os.path.join('isc-inventory', 'iscehb.stn')
 
-with open(isc_file_1, 'r') as sta:
+with open(isc_file_2, 'r') as sta:
     stations = 0
     for l, line in enumerate(sta):
         line = line.rstrip()
@@ -35,19 +37,40 @@ with open(isc_file_1, 'r') as sta:
             else:  # line_length == 55
                 isc_format = isc_format4
             sta_tuple = struct.unpack(isc_format, line)
+
+            # exception handling due to 5 char station names
             if sta_tuple[0].strip():
                 stations += 1
                 print('found station {}'.format(stations),
                       'station_code: ', sta_tuple[0],
                       'sta details: ', sta_tuple)
                 try:
-                    _ = float(sta_tuple[1])
+                    if len(sta_tuple) == 5:
+                        lat, lon, ele, start_date = sta_tuple[1:5]
+                        end_date = False
+                    elif len(sta_tuple) == 6:
+                        lat, lon, ele, start_date, end_date = sta_tuple[1:6]
+
+                    _ = float(lat)
+                    _ = float(lon)
+                    _ = float(ele)
+                    start_date = UTCDateTime(start_date)
+                    print('This station start date: ', start_date)
+                    if end_date:
+                        end_date = UTCDateTime(end_date)
+                        print('This station end date: ', end_date)
                 except ValueError:
                     print(sta_tuple)
                     if line_length == 54:
-                        sta_tuple = struct.unpack(alt_isc_format2, line)
+                        sta_tuple = struct.unpack(five_char_isc_format2, line)
                         print(sta_tuple)
                         # raise
+                    elif line_length == 74:
+                        sta_tuple = struct.unpack(five_char_isc_format1, line)
+                        print(sta_tuple)
+                    else:
+                        raise
         else:
             # make sure we know all other formats before we ignore them
             assert len(line) == 80 or len(line) == 86 or len(line) == 92
+
