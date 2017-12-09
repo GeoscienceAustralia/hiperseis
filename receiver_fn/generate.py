b11d2981239ea808a6496c0b93c5d14a2916198e
Niket Chhajed <niket.chhajed@ga.gov.au>
2017-12-09 17:56:44 +1100

    moving to standalone get_waveforms function instead of class method

--- receiver_fn/generate.py
+++ receiver_fn/generate.py
@@ -10,7 +10,22 @@ from rf.imaging import plot_profile_map
 from rf.profile import profile
 from tqdm import tqdm
 
-from asdf_util import ASDFUtil
+import pyasdf
+from pyasdf import ASDFDataSet
+from obspy.core import Stream
+
+asdfDataSet = pyasdf.ASDFDataSet('/g/data/ha3/Passive/_ANU/7D(2012-2013)/ASDF/7D(2012-2013).h5', mode='r')
+
+def custom_get_waveforms(network, station, location, channel, starttime,
+                  endtime, quality=None, minimumlength=None,
+                  longestonly=None, filename=None, attach_response=False,
+                  **kwargs):
+    st = Stream()
+    #ignoring channel for now as all the 7D network waveforms have only BH? channels
+    filteredList = [i for i in asdfDataSet.waveforms[network+'.'+station].list() if 'raw_recording' in i and UTC(i.split("__")[1]) < starttime and UTC(i.split("__")[2]) > endtime]
+    for t in filteredList:
+        st += asdfDataSet.waveforms[network+'.'+station][t]
+    return st
 
 data = os.path.join('data', '')
 invfile = data + '7D-inventory.xml'
@@ -23,9 +38,9 @@ inventory = read_inventory(invfile)
 catalog = read_events(catfile)
 
 stream = RFStream()
-asdfUtil = ASDFUtil('/g/data/ha3/Passive/_ANU/7D(2012-2013)/ASDF/7D(2012-2013).h5')
 with tqdm() as pbar:
-    for s in iter_event_data(catalog, inventory, asdfUtil.get_waveforms, pbar=pbar):
+    for s in iter_event_data(catalog, inventory, custom_get_waveforms, pbar=pbar):
         stream.extend(s)
 
 stream.write(datafile, 'H5')
+
