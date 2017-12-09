90b4353752bac98639f0748350ce92bdaefe5822
Niket Chhajed <niket.chhajed@ga.gov.au>
2017-12-09 20:44:42 +1100

    using ASDFDataSet as Context Manager

--- receiver_fn/generate.py
+++ receiver_fn/generate.py
@@ -14,18 +14,20 @@ import pyasdf
 from pyasdf import ASDFDataSet
 from obspy.core import Stream
 
-asdfDataSet = pyasdf.ASDFDataSet('/g/data/ha3/Passive/_ANU/7D(2012-2013)/ASDF/7D(2012-2013).h5', mode='r')
-
 def custom_get_waveforms(network, station, location, channel, starttime,
                   endtime, quality=None, minimumlength=None,
                   longestonly=None, filename=None, attach_response=False,
                   **kwargs):
-    st = Stream()
-    #ignoring channel for now as all the 7D network waveforms have only BH? channels
-    filteredList = [i for i in asdfDataSet.waveforms[network+'.'+station].list() if 'raw_recording' in i and UTC(i.split("__")[1]) < starttime and UTC(i.split("__")[2]) > endtime]
-    for t in filteredList:
-        st += asdfDataSet.waveforms[network+'.'+station][t]
-    return st
+    with pyasdf.ASDFDataSet('/g/data/ha3/Passive/_ANU/7D(2012-2013)/ASDF/7D(2012-2013).h5', mode='r') as asdfDataSet:
+        st = Stream()
+        #ignoring channel for now as all the 7D network waveforms have only BH? channels
+        filteredList = [i for i in asdfDataSet.waveforms[network+'.'+station].list() if
+                        'raw_recording' in i and
+                        UTC(i.split("__")[1]) < starttime and
+                        UTC(i.split("__")[2]) > endtime]
+        for t in filteredList:
+            st += asdfDataSet.waveforms[network+'.'+station][t]
+        return st
 
 data = os.path.join('data', '')
 invfile = data + '7D-inventory.xml'
