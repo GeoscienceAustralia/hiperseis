4482a937b06cffebcda64f4d661fc9e3a8920d0e
Sudipta Basak <basaks@gmail.com>
2018-01-05 08:46:18 +1100

    use numpy.distutils `setup`

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-from setuptools import setup
-from setuptools.command.test import test as TestCommand
 import sys
-
+from setuptools.command.test import test as TestCommand
+from numpy.distutils.core import Extension, setup
 
 python_version = sys.version_info
 __version__ = "0.0.1"
@@ -39,121 +38,117 @@ The full documentation is at http://geoscienceaustralia.github.io/passive
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 
-if __name__ == '__main__':
-    setup(
-        name='Passive-Seismic',
-        version=__version__,
-        description='Repository for development of software and '
-                    'metadata for passive seismic project',
-        long_description=readme + '\n\n' + doclink + '\n\n' + history,
-        author='Geoscience Australia Passive Seismic Team',
-        author_email='',
-        url='https://github.com/GeoscienceAustralia/Passive-Seismic',
-        packages=['seismic', 'convert_logs', 'seismic.scripts', 'seismic.cluster',
-                  'inventory', 'PhasePApy'],
-        package_dir={'passive-seismic': 'seismic'},
-        include_package_data=True,
-        dependency_links=[
-            'https://github.com/matplotlib/basemap/archive/'
-            'v1.1.0.zip#egg=basemap-1.1.0',
-            # 'https://github.com/GeoscienceAustralia/PhasePApy/'
-            # 'archive/master.zip#egg=phasepapy-1.1.1',
-            # git+https://github.com/GeoscienceAustralia/PhasePApy.git@v1.1.1
-            ],
-        entry_points={
-            'console_scripts': [
-                'anulog = convert_logs.decode_datfile:anulog',
-                'seismic = seismic.scripts.event:cli',
-                'cluster = seismic.cluster.cluster:cli',
-            ]
-        },
+setup(
+    name='Passive-Seismic',
+    version=__version__,
+    description='Repository for development of software and '
+                'metadata for passive seismic project',
+    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    author='Geoscience Australia Passive Seismic Team',
+    author_email='',
+    url='https://github.com/GeoscienceAustralia/Passive-Seismic',
+    packages=['seismic', 'convert_logs', 'seismic.scripts', 'seismic.cluster',
+              'inventory', 'PhasePApy'],
+    package_dir={'passive-seismic': 'seismic'},
+    include_package_data=True,
+    dependency_links=[
+        'https://github.com/matplotlib/basemap/archive/'
+        'v1.1.0.zip#egg=basemap-1.1.0',
+        # 'https://github.com/GeoscienceAustralia/PhasePApy/'
+        # 'archive/master.zip#egg=phasepapy-1.1.1',
+        # git+https://github.com/GeoscienceAustralia/PhasePApy.git@v1.1.1
+        ],
+    entry_points={
+        'console_scripts': [
+            'anulog = convert_logs.decode_datfile:anulog',
+            'seismic = seismic.scripts.event:cli',
+            'cluster = seismic.cluster.cluster:cli',
+        ]
+    },
 
-        # numpy preinstall required due to obspy
-        # mpi4py  preinstall required due to h5py
-        setup_requires=[
-            NUMPY_VERSION,
-            'mpi4py==3.0.0',
-            'decorator>=4.1.0',
-            'setuptools>=36.2.1'
-        ],
-        install_requires=[
-            'Click >= 6.0',
-            NUMPY_VERSION,
-            'Cython >= 0.22.1',
-            'mpi4py == 3.0.0',
-            'scipy >= 0.15.1',
-            'PyYAML >= 3.11',
-            'matplotlib >= 1.4.3',  # nci version with python=3.4
-            'joblib',
-            'obspy >= 1.1.0',  # 1.0.3 does not have sc3ml read functionality
-            'h5py >= 2.6.0',
-            'pyasdf',
-            'pyqtgraph',
-            'pandas',
-            # 'phasepapy == 1.1.1',
-            'basemap == 1.1.0',
-            'netCDF4 >= 1.3.0',
-            'chardet == 3.0.4',
-        ],
-        extras_require={
-            'dev': [
-                'sphinx',
-                'ghp-import',
-                'sphinxcontrib-programoutput',
-                'pytest-cov',
-                'coverage == 4.4.1',
-                'codecov == 2.0.9',
-                'tox',
-                'pytest >= 3.1.0',
-                'pytest-flake8 >= 0.8.1',
-                'pytest-mock >= 1.6.0',
-                'pytest-cov == 2.5.1',
-                'pytest-regtest >= 0.15.1',
-                'flake8-docstrings >= 1.1.0',
-            ]
-        },
-        license="Apache Software License 2.0",
-        zip_safe=False,
-        keywords='Passive Seismic',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            "Operating System :: POSIX",
-            "License :: OSI Approved :: Apache Software License",
-            "Natural Language :: English",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 2.7",
-            # "Programming Language :: Python :: 3",
-            # "Programming Language :: Python :: 3.3",
-            "Programming Language :: Python :: 3.4",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-            # "Programming Language :: Python :: 3.7",
-            # add additional supported python versions
-            "Intended Audience :: Science/Research",
-            "Intended Audience :: Developers",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-            "Topic :: Scientific/Engineering :: Information Analysis"
-            # add more topics
-        ],
-        cmdclass={
-            'test': PyTest,
-        }
+    # numpy preinstall required due to obspy
+    # mpi4py  preinstall required due to h5py
+    setup_requires=[
+        NUMPY_VERSION,
+        'mpi4py==3.0.0',
+        'decorator>=4.1.0',
+        'setuptools>=36.2.1'
+    ],
+    install_requires=[
+        'Click >= 6.0',
+        NUMPY_VERSION,
+        'Cython >= 0.22.1',
+        'mpi4py == 3.0.0',
+        'scipy >= 0.15.1',
+        'PyYAML >= 3.11',
+        'matplotlib >= 1.4.3',  # nci version with python=3.4
+        'joblib',
+        'obspy >= 1.1.0',  # 1.0.3 does not have sc3ml read functionality
+        'h5py >= 2.6.0',
+        'pyasdf',
+        'pyqtgraph',
+        'pandas',
+        # 'phasepapy == 1.1.1',
+        'basemap == 1.1.0',
+        'netCDF4 >= 1.3.0',
+        'chardet == 3.0.4',
+    ],
+    extras_require={
+        'dev': [
+            'sphinx',
+            'ghp-import',
+            'sphinxcontrib-programoutput',
+            'pytest-cov',
+            'coverage == 4.4.1',
+            'codecov == 2.0.9',
+            'tox',
+            'pytest >= 3.1.0',
+            'pytest-flake8 >= 0.8.1',
+            'pytest-mock >= 1.6.0',
+            'pytest-cov == 2.5.1',
+            'pytest-regtest >= 0.15.1',
+            'flake8-docstrings >= 1.1.0',
+        ]
+    },
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords='Passive Seismic',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        # "Programming Language :: Python :: 3",
+        # "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        # "Programming Language :: Python :: 3.7",
+        # add additional supported python versions
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Information Analysis"
+        # add more topics
+    ],
+    cmdclass={
+        'test': PyTest,
+    }
+)
+
+# Build the f2py fortran extension
+# --------------------------------
+
+ellipcorr = Extension(
+    name='ellipcorr',
+    # add several modules files under the same extension
+    sources=['ellip-corr/ellip/ellipcorr.f']
+)
+
+setup(
+    name='ellipcorr',
+    ext_modules=[ellipcorr]
     )
-
-    # Build the f2py fortran extension
-    # --------------------------------
-    from numpy.distutils.core import Extension
-    from numpy.distutils.core import setup as np_setup
-
-
-    ellipcorr = Extension(
-        name='ellipcorr',
-        # add several modules files under the same extension
-        sources=['ellip-corr/ellip/ellipcorr.f']
-    )
-
-    np_setup(
-        name='ellipcorr',
-        ext_modules=[ellipcorr]
-        )
