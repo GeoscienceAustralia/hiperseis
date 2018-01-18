c502c792792ac82cc8eb4300b7c152bf14a3599c
Sudipta Basak <basaks@gmail.com>
2018-01-18 11:28:49 +1100

    reject stations already associated

--- iloc_rstt/config.py
+++ iloc_rstt/config.py
@@ -1,5 +1,7 @@
 from os.path import join
 
-event_dir = '/home/sudipta/Alexi/analyst_events'
+event_dir = join('iloc_rstt', 'mocks')
 
 event_file = join(event_dir, '772009.xml')
+
+max_station_dist = 120.0   # percentage of distance to scan for extra stations
