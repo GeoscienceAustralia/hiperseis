fb1ab312c6cfc628ebb2bc63837b232935602bae
Sudipta Basak <basaks@gmail.com>
2018-01-19 15:55:23 +1100

    change `station` list to a `{sta:phase}` dict

--- tests/test_iloc_event.py
+++ tests/test_iloc_event.py
@@ -8,7 +8,7 @@ from iloc_rstt.iloc_event import ILocCatalog, DELTA, stations, stations_dict
 @pytest.fixture(params=[
     pytest.lazy_fixture('xml'),
     pytest.lazy_fixture('analyst_event')
-], scope='module')
+    ], scope='module')
 def one_event(request):
     return request.param
 
@@ -20,15 +20,15 @@ def test_stations_in_range(one_event):
     assert len(catalog.events) == 1  # should create only one iloc event
 
     for e_old, e_new in zip(catalog.orig_events, catalog.events):
-        max_dist, orig_stas = e_new._farthest_station_dist()
+        max_dist, sta_phs_dict = e_new._farthest_station_dist()
         assert all(e_new.new_stations[DELTA] < 1.2*max_dist)  # 120% is default
-        orig_stations = stations[stations['station_code'].isin(orig_stas)]
+        orig_stations = stations[stations['station_code'].isin(sta_phs_dict)]
         assert all(orig_stations[DELTA] <= 1.0*max_dist)
 
         origin = e_old.preferred_origin()
         for a in origin.arrivals:
             sta = a.pick_id.get_referred_object().waveform_id.station_code
-            assert sta in orig_stas
+            assert sta in sta_phs_dict
             assert sta not in e_new.new_stations['station_code']
             station = stations_dict[sta]
             dis = locations2degrees(station.latitude, station.longitude,
