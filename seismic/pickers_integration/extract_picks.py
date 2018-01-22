d9f48f5f75a06914951b7cbd5eccc5c7d7e1a440
Niket Chhajed <chhajed@gmail.com>
2018-01-22 19:28:11 +1100

    #6 added extracting of phases from new stations; refactoring

--- seismic/pickers_integration/extract_picks.py
+++ seismic/pickers_integration/extract_picks.py
@@ -2,12 +2,19 @@ from obspy import UTCDateTime, read_events
 from obspy.clients.fdsn import Client
 from obspy.taup import TauPyModel
 from obspy.signal.trigger import ar_pick, classic_sta_lta, trigger_onset
+from obspy.core.event import Pick, CreationInfo, WaveformStreamID, ResourceIdentifier
 import glob
 
 sc3_host_ip='54.79.27.220'
 trigOnThreshold=1.5
 trigOffThreshold=0.5
 client = Client('http://'+sc3_host_ip+':8081')
+model = TauPyModel(model='iasp91')
+
+global p_pickCount
+p_pickCount = 1
+global s_pickCount
+s_pickCount = 1
 
 def s_phase_exists(evt, pick):
     exists=False
@@ -24,52 +31,103 @@ def clean_trace(tr, t1, t2):
     tr.taper(0.05)
     tr.filter('bandpass', freqmin=2.0, freqmax=4.9)
 
-def pick_sphase(event):
-    model = TauPyModel(model='iasp91')
+def createPickObject(net, sta, cha, time, backaz, phasehint):
+    global p_pickCount
+    global s_pickCount
+    if phasehint == 'P':
+        count = p_pickCount
+        p_pickCount += 1
+    else:
+        count = s_pickCount
+        s_pickCount += 1
+    return Pick(resource_id=ResourceIdentifier(id='smi:niket.picker.ga.gov.au/pick/'+str(count)),
+                time=time,
+                waveform_id=WaveformStreamID(network_code=net, station_code=sta, channel_code=cha),
+                methodID=ResourceIdentifier('obspy/stalta/arpicker'),
+                backazimuth=backaz,
+                phase_hint=phasehint,
+                evaluation_mode='automatic',
+                creation_info=CreationInfo(author='niket',
+                                creation_time=UTCDateTime(),
+                                agency_id='niket-ga-picker'))
 
+def pick_p_s_phase(network, station, prefor, pickP=False):
+    arrivals = model.get_travel_times_geo(prefor.depth/1000, prefor.latitude,
+                 prefor.longitude, stations.latitude, stations.longitude, phase_list=("ttbasic",))
+    parrivals = [ar for ar in arrivals if ar.phase.name.lower() == 'p']
+    sarrivals = [ar for ar in arrivals if ar.phase.name.lower() == 's']
+    arrivals.sort(key=lambda x: x.time)
+    if len(sarrivals) > 0:
+        try:
+            stz = client.get_waveforms(network.code, station.code, '*', 'BHZ,SHZ,HHZ', prefor.time+arrivals[0].time-100, prefor.time+arrivals[-1].time+150)
+            stn = client.get_waveforms(network.code, station.code, '*', 'BHN,SHN,HHN,BH1,SH1', prefor.time+arrivals[0].time-100, prefor.time+arrivals[-1].time+150)
+            ste = client.get_waveforms(network.code, station.code, '*', 'BHE,SHE,HHZ,BH2,SH2', prefor.time+arrivals[0].time-100, prefor.time+arrivals[-1].time+150)
+        except Exception, e:
+            print ('FDSN client could not retrieve waveform for net='+network.code+', sta='+station.code+' around time='+str(prefor.time+arrivals[0].time))
+            print(str(e))
+            continue
+        if stz and stn and ste and stz[0].stats.sampling_rate == stn[0].stats.sampling_rate and \
+                                   stn[0].stats.sampling_rate == ste[0].stats.sampling_rate:
+            samp_rate, f1, f2, lta_p, sta_p, lta_s, sta_s, m_p, m_s, l_p, l_s = \
+                stz[0].stats.sampling_rate, 2.0, 4.9, 1.0, 0.1, 4.0, 1.0, 2, 8, 0.1, 0.2
+            for st in [stz, stn, ste]:
+                clean_trace(st[0], prefor.time+arrivals[0].time-100, prefor.time+arrivals[-1].time+150)
+            ptime, stime = ar_pick(stz[0].data, stn[0].data, ste[0].data, samp_rate, f1, f2,
+                                   lta_p, sta_p, lta_s, sta_s, m_p, m_s, l_p, l_s)
+            if ptime > 0 and stime > 0 and stime > ptime:
+                az = client.distaz(stalat=station.latitude, stalon=station.longitude, evtlat=prefor.latitude, evtlon=prefor.longitude)
+                ppick = createPickObject(network.code, station.code, stz[0].stats.channel, prefor.time+arrivals[0].time-100+ptime, az['backazimuth'] if az else None, 'P')
+                spick = createPickObject(network.code, station.code, stn[0].stats.channel, prefor.time+arrivals[0].time-100+stime, az['backazimuth'] if az else None, 'S')
+            elif ptime > 0:
+                ppick = createPickObject(network.code, station.code, stz[0].stats.channel, prefor.time+arrivals[0].time-100+ptime, az['backazimuth'] if az else None, 'P')
+                trigs= []
+                for tr in [stn[0], ste[0]]:
+                    cft = classic_sta_lta(tr.data, int(5*samp_rate), int(10*samp_rate))
+                    trigs.extend(trigger_onset(cft, 1.5, 0.75))
+                trigs = [trig for trig in trigs if abs(sarrivals[0].time*samp_rate-trig[0]) < (samp_rate*20)]
+                mintrigdiff = abs(sarrivals[0].time*samp_rate-trigs[0][0])
+                besttrig = trigs[0][0]
+                for trig in trigs:
+                    if abs(sarrivals[0].time*samp_rate-trig[0]) < mintrigdiff:
+                        mintrigdiff = abs(sarrivals[0].time*samp_rate-trig[0])
+                        besttrig = trig[0]
+                spick = createPickObject(network.code, station.code, stn[0].stats.channel, prefor.time+arrivals[0].time-100+(besttrig/samp_rate), az['backazimuth'] if az else None, 'S')
+            if pickP:
+                return ppick, spick
+            else:
+                return spick
+
+def pick_new_phases(event):
+    prefor = event.preferred_origin() or event.origins[0]
+    inv = client.get_stations(latitude=prefor.latitude, longitude=prefor.longitude, minradius=0, maxradius=90)
+    for n in inv.networks():
+        for s in n.stations():
+            if s.code not in [p.waveform_id.station_code for p in event.picks]:
+                p_pick, s_pick = pick_p_s_phases(n, s, prefor, pickP=True)
+
+def pick_sphase(event):
     if not event:
         return
     if len(event.picks) < 30:
         return
+    s_phases = []
     for p in event.picks:
         net = p.waveform_id.network_code
         sta = p.waveform_id.station_code
         cha = p.waveform_id.channel_code
         loc = p.waveform_id.location_code
         t = p.time
-        phasehint = p.phase_hint
-        if phasehint=='P':
+        if p.phase_hint=='P':
             try:
                 sts = client.get_stations(network=net, station=sta)
-                if not s_phase_exists(event, p):
-                    prefor = event.preferred_origin() or event.origins[0]
-                    arrivals = model.get_travel_times_geo(prefor.depth/1000, prefor.latitude,
-                                 prefor.longitude, sts.networks[0].stations[0].latitude,
-                                 sts.networks[0].stations[0].longitude, phase_list=("ttbasic",))
-                    sarrivals = [ar for ar in arrivals if ar.phase.name.lower() == 's']
-                    arrivals.sort(key=lambda x: x.time)
-                    if len(sarrivals) > 0:
-                        stz = client.get_waveforms(net, sta, '--', 'BHZ', prefor.time+arrivals[0].time-100, prefor.time+arrivals[-1].time+150)
-                        stn = client.get_waveforms(net, sta, '--', 'BHN', prefor.time+arrivals[0].time-100, prefor.time+arrivals[-1].time+150)
-                        ste = client.get_waveforms(net, sta, '--', 'BHE', prefor.time+arrivals[0].time-100, prefor.time+arrivals[-1].time+150)
-                        if stz and stn and ste and stz[0].stats.sampling_rate == stn[0].stats.sampling_rate and \
-                                                   stn[0].stats.sampling_rate == ste[0].stats.sampling_rate:
-                            samp_rate, f1, f2, lta_p, sta_p, lta_s, sta_s, m_p, m_s, l_p, l_s = \
-                                stz[0].stats.sampling_rate, 2.0, 4.9, 1.0, 0.1, 4.0, 1.0, 2, 8, 0.1, 0.2
-                            for st in [stz, stn, ste]:
-                                clean_trace(st[0], prefor.time+arrivals[0].time-100, prefor.time+arrivals[-1].time+150)
-                            ptime, stime = ar_pick(stz[0].data, stn[0].data, ste[0].data, samp_rate, f1, f2,
-                                                   lta_p, sta_p, lta_s, sta_s, m_p, m_s, l_p, l_s)
-                            if stime > ptime:
-                                print ('ar_picker discovered ptime =>'+ptime+' and stime =>'+stime)
-                            elif ptime > 95:
-                                for tr in [stn[0], ste[0]]:
-                                    cft = classic_sta_lta(tr.data, int(5*samp_rate), int(10*samp_rate))
-                                    trigs = trigger_onset(cft, 1.5, 0.75)
-                                    print [(tr.stats.channel, tr.stats.sampling_rate, abs(sarrivals[0].time*samp_rate-trig[0])) for trig in trigs if abs(sarrivals[0].time*samp_rate-trig[0]) < 1000]
-            except:
+            except Exception, e:
+                print ('FDSN client could not retrieve station details for net='+net+', sta='+sta)
+                print(str(e))
                 continue
-
+            if not s_phase_exists(event, p):
+                prefor = event.preferred_origin() or event.origins[0]
+                s_phases.append(pick_p_s_phase(sts.networks[0], sts.networks[0].stations[0], prefor))
+    return s_phases
 
 evtfiles = glob.glob('event_sc3mls/*.xml')
 for f in evtfiles:
@@ -77,4 +135,7 @@ for f in evtfiles:
     if evts and evts[0]:
         prefor = evts[0].preferred_origin() or evts[0].origins[0]
         if prefor.time > UTCDateTime('2015-03-01T00:00:00.000000Z') and prefor.time < UTCDateTime('2015-04-01T00:00:00.000000Z'):
-            pick_sphase(evts[0])
+            s_picks = pick_sphase(evts[0])
+            p_picks, s_picks = pick_new_phases(evts[0])
+            
+
