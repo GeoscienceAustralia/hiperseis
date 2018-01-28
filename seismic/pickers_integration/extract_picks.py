7dabd83c9ec7ca9f2f83535c70995e61cc916107
Niket Chhajed <niket.chhajed@ga.gov.au>
2018-01-28 17:53:53 +1100

    #6 better results

--- seismic/pickers_integration/extract_picks.py
+++ seismic/pickers_integration/extract_picks.py
@@ -4,19 +4,22 @@ from obspy.clients.fdsn import Client
 from obspy.clients.iris import Client as IrisClient
 from obspy.taup import TauPyModel
 from obspy.taup.taup_geo import calc_dist
-from obspy.signal.trigger import ar_pick, classic_sta_lta, trigger_onset
+from obspy.signal.trigger import ar_pick, recursive_sta_lta, trigger_onset
 from obspy.core.event import Pick, CreationInfo, WaveformStreamID, ResourceIdentifier
+from multiprocessing import Pool
 import glob
 
-#sc3_host_ip='54.79.27.220'
-sc3_host_ip='test-elb-521962885.ap-southeast-2.elb.amazonaws.com'
-#client = Client('http://'+sc3_host_ip+':8081')
-client = Client('http://'+sc3_host_ip)
+sc3_host_ip='54.79.27.220'
+#sc3_host_ip='test-elb-521962885.ap-southeast-2.elb.amazonaws.com'
+client = Client('http://'+sc3_host_ip+':8081')
+#client = Client('http://'+sc3_host_ip)
 model = TauPyModel(model='iasp91')
-plookback=100
+plookback=50
 plookahead=100
 slookback=100
-slookahead=100
+slookahead=200
+pphase_search_margin=10
+sphase_search_margin=25
 global p_pickCount
 p_pickCount = 1
 global s_pickCount
@@ -35,7 +38,7 @@ def clean_trace(tr, t1, t2, freqmin=1.0, freqmax=4.9):
     # add bandpass filtering sensitive to the distance d between source and receiver
     tr.trim(t1, t2)
     tr.detrend('linear')
-    tr.taper(0.05)
+    tr.taper(0.01)
     tr.filter('bandpass', freqmin=freqmin, freqmax=freqmax)
 
 def createPickObject(net, sta, cha, time, backaz, phasehint):
@@ -65,15 +68,17 @@ def get_backazimuth(stalat, stalon, evtlat, evtlon):
 def calc_distance(rec, src):
     return calc_dist(src.latitude, src.longitude, rec.latitude, rec.longitude, 6371, 0)
 
-def find_best_bounds(cft):
+def find_best_bounds(cft, samp_rate):
     bestupper = 1.5
     bestlower = 0.75
-    leasttrigs = len(trigger_onset(cft, bestupper, bestlower))
+    max_margin = bestupper - bestlower
+    leasttrigs = len(trigger_onset(cft, 1.5, 0.75, max_len=(60*samp_rate), max_len_delete=True))
     for upper in np.linspace(1.5, 2.5, 5):
-        for lower in [1.0, 0.75, 0.5]:
-            t = trigger_onset(cft, upper, lower)
-            if len(t) < leasttrigs and len(t) > 0:
+        for lower in [0.875, 0.75, 0.625, 0.5, 0.375]:
+            t = trigger_onset(cft, upper, lower, max_len=(60*samp_rate), max_len_delete=True)
+            if len(t) > 0 and (upper - lower) > max_margin and len(t) <= leasttrigs:
                 leasttrigs = len(t)
+                max_margin = upper - lower
                 bestupper = upper
                 bestlower = lower
     return bestupper, bestlower
@@ -115,38 +120,43 @@ def pick_p_s_phases(network, station, prefor, pickP=False, available_ptime=None)
                 clean_trace(tr, trim_starttime, trim_endtime)
             ptime, stime = ar_pick(stz[0].data, stn[0].data, ste[0].data, samp_rate, f1, f2,
                                    lta_p, sta_p, lta_s, sta_s, m_p, m_s, l_p, l_s)
-            if ptime > 0 and stime > 0 and stime > ptime and abs(trim_starttime + ptime - theoretical_ptime) < 10 and \
-                abs(prefor.time + mean_sarrival - trim_starttime - stime) < 20:
+            if ptime > 0 and stime > 0 and stime > ptime and abs(trim_starttime + ptime - theoretical_ptime) < pphase_search_margin and \
+                abs(prefor.time + mean_sarrival - trim_starttime - stime) < sphase_search_margin:
                 az = get_backazimuth(stalat=station.latitude, stalon=station.longitude, evtlat=prefor.latitude, evtlon=prefor.longitude)
                 ppick = createPickObject(network.code, station.code, stz[0].stats.channel, trim_starttime+ptime, az['backazimuth'] if az else None, 'P')
                 spick = createPickObject(network.code, station.code, stn[0].stats.channel, trim_starttime+stime, az['backazimuth'] if az else None, 'S')
-            elif (pickP and ptime > 0 and abs(trim_starttime + ptime - available_ptime) < 10) or \
-                 (not pickP and available_ptime):
+            elif (not pickP and available_ptime) or \
+                 (pickP and ptime > 0 and abs(trim_starttime + ptime - available_ptime) < pphase_search_margin):
                 if not pickP and available_ptime:
                     ptime = available_ptime - trim_starttime
                 az = get_backazimuth(stalat=station.latitude, stalon=station.longitude, evtlat=prefor.latitude, evtlon=prefor.longitude)
                 ppick = createPickObject(network.code, station.code, stz[0].stats.channel, trim_starttime+ptime, az['backazimuth'] if az else None, 'P')
                 trim_starttime = prefor.time+sarrivals[0].time-slookback
                 trim_endtime = prefor.time+sarrivals[-1].time+slookahead
+                stz_for_s = client.get_waveforms(network.code, station.code, '*', 'BHZ,SHZ,HHZ', trim_starttime, trim_endtime)
                 stn_for_s = client.get_waveforms(network.code, station.code, '*', 'BHN,SHN,HHN,BH1,SH1', trim_starttime, trim_endtime)
                 ste_for_s = client.get_waveforms(network.code, station.code, '*', 'BHE,SHE,HHZ,BH2,SH2', trim_starttime, trim_endtime)
                 trigs= []
                 if trim_starttime < available_ptime:
-                    trim_starttime = available_ptime
-                for tr in [stn_for_s[0], ste_for_s[0]]:
+                    trim_starttime = available_ptime + 10
+                for tr in [stz_for_s[0], stn_for_s[0], ste_for_s[0]]:
+                    # replace steps in this loop with a function that runs on 4 cores in parallel
+                    # with diffferent filter windows and returns best trigger for this trace
                     clean_trace(tr, trim_starttime, trim_endtime, 0.5, 3)
-                    cft = classic_sta_lta(tr.data, int(5*samp_rate), int(20*samp_rate))
-                    upper, lower = find_best_bounds(cft)
-                    trigs.extend(trigger_onset(cft, upper, lower))
-                trigs = [trig for trig in trigs if abs(prefor.time + mean_sarrival - trim_starttime - (trig[0]/samp_rate)) < 20]
+                    cft = recursive_sta_lta(tr.data, int(5*samp_rate), int(20*samp_rate))
+                    upper, lower = find_best_bounds(cft, tr.stats.sampling_rate)
+                    trigs.extend([(onset, tr.stats.channel) for onset in trigger_onset(cft, upper, lower, max_len=(60*tr.stats.sampling_rate), max_len_delete=True)])
+                trigs = [trig for trig in trigs if abs(prefor.time + mean_sarrival - trim_starttime - (trig[0][0]/samp_rate)) < sphase_search_margin]
                 if len(trigs) > 0:
-                    mintrigdiff = abs(prefor.time + mean_sarrival - trim_starttime - trigs[0][0])
-                    besttrig = trigs[0][0]
+                    mintrigdiff = abs(prefor.time + mean_sarrival - trim_starttime - trigs[0][0][0])
+                    besttrig = trigs[0][0][0]
+                    best_cha = trigs[0][1]
                     for trig in trigs:
-                        if abs(prefor.time + mean_sarrival - trim_starttime - trig[0]) < mintrigdiff:
-                            mintrigdiff = abs(prefor.time + mean_sarrival - trim_starttime - trig[0])
-                            besttrig = trig[0]
-                    spick = createPickObject(network.code, station.code, stn[0].stats.channel, trim_starttime+(besttrig/samp_rate), az['backazimuth'] if az else None, 'S')
+                        if abs(prefor.time + mean_sarrival - trim_starttime - trig[0][0]) < mintrigdiff:
+                            mintrigdiff = abs(prefor.time + mean_sarrival - trim_starttime - trig[0][0])
+                            besttrig = trig[0][0]
+                            best_cha = trig[1]
+                    spick = createPickObject(network.code, station.code, best_cha, trim_starttime+(besttrig/samp_rate), az['backazimuth'] if az else None, 'S')
             elif pickP and ptime <= 0:
                 # this needs to be thought through and implemented, dummy behavior for now
                 ppick = None
@@ -156,6 +166,9 @@ def pick_p_s_phases(network, station, prefor, pickP=False, available_ptime=None)
             else:
                 return None, spick
     return None
+
+def find_trigger_parallel(trace, starttime, endtime):
+    pass
 
 def pick_new_phases(event):
     prefor = event.preferred_origin() or event.origins[0]
