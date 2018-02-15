a1cb3f9104002d4c82ddc7c231f8a5b650cdfb63
Sudipta Basak <basaks@gmail.com>
2018-02-16 00:03:25 +1100

    CI config and createenv script updated to match new dockerfile
    [ci skip]

--- sc3/createenv.sh
+++ sc3/createenv.sh
@@ -7,66 +7,6 @@ if [ -z ${ILOCROOT+x} ]; then
     env
 fi
 
-sudo yum update -y
-sudo yum groupinstall 'Development Tools' -y
-
-sudo yum install -y wget \
-                    tar \
-                    bzip2-devel \
-                    m4 \
-                    openmpi openmpi-devel \
-                    git \
-                    vim \
-                    screen \
-                    libpng \
-                    libpng-devel \
-                    epel-release \
-                    blas blas-devel \
-                    lapack lapack-devel \
-                    libtiff-devel \
-                    libjpeg-devel \
-                    zlib-devel \
-                    freetype-devel \
-                    lcms2-devel \
-                    libwebp-devel \
-                    tkinter \
-                    tcl-devel \
-                    tk-devel \
-                    libffi-devel \
-                    libxml2 \
-                    libxml2-devel \
-                    libxslt \
-                    libxslt-devel \
-                    python-devel \
-                    netcdf-devel.x86_64 \
-                    cpan \
-                    evince \
-                    evince-nautilus \
-                    java-1.7.0-openjdk  # required due to cwbquery
-
-# perl stuff
-sudo perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit'
-sudo cpan App::cpanminus && cpanm PDL
-
-
-# Install GMP with mapproject from source, yum install does not bring
-# mapproject, required by Istvan's scripts
-cd $HOME && \
-    svn checkout svn://gmtserver.soest.hawaii.edu/gmt/trunk gmt-dev && \
-    wget ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/gshhg-gmt-2.3.7.tar.gz && \
-    wget ftp://ftp.star.nesdis.noaa.gov/pub/sod/lsa/gmt/dcw-gmt-1.1.3.tar.gz && \
-    tar -xzf gshhg-gmt-2.3.7.tar.gz && \
-    tar -xzf dcw-gmt-1.1.3.tar.gz && \
-    rm -rf $HOME/dcw-gmt-1.1.3.tar.gz $HOME/gshhg-gmt-2.3.7.tar.gz
-
-cd $HOME/gmt-dev && \
-    cp cmake/ConfigUserTemplate.cmake cmake/ConfigUser.cmake && \
-    echo "set (CMAKE_INSTALL_PREFIX /usr/local/)" >> cmake/ConfigUser.cmake && \
-    echo "set (GSHHG_ROOT $HOME/gshhg-gmt-2.3.7)" >> cmake/ConfigUser.cmake && \
-    echo "set (DCW_ROOT $HOME/dcw-gmt-1.1.3)" >> cmake/ConfigUser.cmake &&
-    mkdir build && cd build && \
-    cmake .. && sudo make install
-
 # clone passive-seismic in home directory after git install
 if [ ! -d "$HOME/passive-seismic" ]; then
 	CWD=`pwd`
@@ -74,34 +14,6 @@ if [ ! -d "$HOME/passive-seismic" ]; then
 	git clone https://github.com/GeoscienceAustralia/passive-seismic.git
 	cd $CWD
 fi
-
-sudo yum install -y geos-devel python-pip openjpeg2-devel
-sudo pip install -U pip setuptools virtualenv virtualenvwrapper numpy
-
-# install mpi4py
-sudo env MPICC=/usr/lib64/openmpi/bin/mpicc pip install mpi4py==3.0.0
-
-
-echo "Installing parallel hdf5...."
-#Build parallel HDF5
-wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.14/src/hdf5-1.8.14.tar.gz
-tar -xzf hdf5-1.8.14.tar.gz
-cd hdf5-1.8.14 && \
-    CC=/usr/lib64/openmpi/bin/mpicc ./configure --enable-parallel --enable-shared --prefix=/usr/local/hdf5 && \
-    make && \
-    sudo make install && \
-    cd .. && sudo rm -rf hdf5-1.8.14 hdf5-1.8.14.tar.gz
-
-# build parallel h5py
-# /usr/include/openmpi-x86_64/mpi.h
-echo "Installing parallel h5py...."
-git clone https://github.com/h5py/h5py.git && \
-    cd h5py && git checkout tags/2.7.0  && \
-    sed -i "52i \ \ \ \ COMPILER_SETTINGS['include_dirs'].extend(['/usr/include/openmpi-x86_64'])" setup_build.py && \
-    export CC=/usr/lib64/openmpi/bin/mpicc && \
-    python setup.py configure --mpi --hdf5=/usr/local/hdf5/ && \
-    sudo python setup.py install && \
-    cd .. && sudo rm -rf h5py
 
 # Setup virtualenv
 echo '' >> $HOME/.bashrc
