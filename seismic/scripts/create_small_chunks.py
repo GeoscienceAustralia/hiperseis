35182e14238fb78cbc95c592f86a4ea2c5caeea2
Niket Chhajed <chhajed@gmail.com>
2018-05-01 05:29:34 +1000

    #82 create smaller day miniseed files for skippy

---
+++ seismic/scripts/create_small_chunks.py
@@ -0,0 +1,34 @@
+import os
+from obspy import read_inventory, Stream
+from obspy import UTCDateTime as UTC
+from pyasdf import ASDFDataSet
+import datetime
+
+base_dir = '/g/data/ha3/Passive/_ANU/7B(1993-1995)'
+asdf_file = os.path.join(base_dir, 'ASDF', '7B(1993-1995).h5')
+out_dir = os.path.join(base_dir, 'small_mseed_DATA')
+
+inv = read_inventory(os.path.join(base_dir, '7B.xml'))
+asdf = ASDFDataSet(asdf_file, mode='r')
+
+def create_chunk(trace, st_time, end_time, sta):
+    trace.trim(starttime=st_time, endtime=end_time)
+    st_out = Stream(traces=[trace, ])
+    dest_dir = os.path.join(out_dir, str(trace.stats.starttime.timetuple().tm_year), str(tr.stats.starttime.timetuple().tm_yday))
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    st_out.write(os.path.join(dest_dir, sta.code+'_'+str(st_time)+'_'+str(end_time)+'.ms'), format='MSEED')
+
+for sta in inv.networks[0].stations:
+    if asdf.waveforms.__contains__(inv.networks[0].code+'.'+sta.code):
+        for i in asdf.waveforms[inv.networks[0].code+'.'+sta.code].list():
+            start_time = UTC(i.split("__")[1])
+            st = asdf.waveforms[inv.networks[0].code+'.'+sta.code][i]
+            while (start_time+86400<UTC(i.split("__")[2])):
+                tr = st[0].copy()
+                create_chunk(tr, start_time, start_time+86400, sta)
+                start_time += 86400
+            if start_time < UTC(i.split("__")[2]):
+                tr=st[0].copy()
+                create_chunk(tr, start_time, UTC(i.split("__")[2]), sta)
+
