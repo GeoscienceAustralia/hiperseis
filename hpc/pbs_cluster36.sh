545cd78ad163df84be0b3e13033014edefa4da07
Fei Zhang <fei.zhang@ga.gov.au>
2018-05-22 16:15:42 +1000

    commented out cluster match subcommand [ci skip]

--- hpc/pbs_cluster36.sh
+++ hpc/pbs_cluster36.sh
@@ -36,15 +36,16 @@ cluster sort outfile_S.csv 10. -s sorted_S.csv
 
 # match
 # matching is an optional step as we can simply do P or S wave inversion
-cluster match sorted_P.csv sorted_S.csv -p matched_P.csv -s matched_S.csv
+#Optional cluster match sorted_P.csv sorted_S.csv -p matched_P.csv -s matched_S.csv
 
 # zones
 # if matching is not performed, replace matched_P.csv by sorted_P.csv and
 # matched_S.csv by sorted_P.csv
 cluster zone sorted_S.csv -z '0 -50.0 100 190' -r sorted_region_S.csv -g sorted_global_S.csv
 cluster zone sorted_P.csv -z '0 -50.0 100 190' -r sorted_region_P.csv -g sorted_global_P.csv
-cluster zone matched_P.csv -z '0 -50.0 100 190' -r region_P.csv -g global_P.csv
-cluster zone matched_S.csv -z '0 -50.0 100 190' -r region_S.csv -g global_S.csv
+
+#cluster zone matched_P.csv -z '0 -50.0 100 190' -r region_P.csv -g global_P.csv
+#cluster zone matched_S.csv -z '0 -50.0 100 190' -r region_S.csv -g global_S.csv
 
 
 # alternative zone command using the param2x2, the parameter file used during
@@ -52,7 +53,7 @@ cluster zone matched_S.csv -z '0 -50.0 100 190' -r region_S.csv -g global_S.csv
 # cluster zone -p /path/to/param2x2 matched_S.csv -r region_S.csv -g global_S.csv
 
 # cluster diagnostics plot
-cluster plot matched_P.csv '0 -50.0 100 190'
+#cluster plot matched_P.csv '0 -50.0 100 190'
 cluster plot sorted_P.csv '0 -50.0 100 190'
 # cluster plot matched_S.csv '0 -50.0 100 190'
-# cluster plot sorted_S.csv '0 -50.0 100 190'
+cluster plot sorted_S.csv '0 -50.0 100 190'
