0737d966569bccc7657ab95a752ae5bff46a585d
Rakib Hassan <rakib.hassan@ga.gov.au>
2018-10-08 18:09:28 +1100

    S-arrivals Picker
    * Implemented combined p- and s-arrivals picker
    * When horizontal components are unavailable, s-arrivals
      are picked on the Z component

--- seismic/pick_harvester/pick.py
+++ seismic/pick_harvester/pick.py
@@ -48,10 +48,15 @@ from PhasePApy.phasepapy.phasepicker import fbpicker
 from PhasePApy.phasepapy.phasepicker import ktpicker
 from PhasePApy.phasepapy.phasepicker import aicdpicker
 
-from utils import EventParser, DistAz, recursive_glob, split_list
+from utils import EventParser, DistAz, Catalog
 
 def extract_p(taupy_model, picker, event, station_longitude, station_latitude,
-              st, win_start=-50, win_end=50, resample_hz=20):
+              st, win_start=-50, win_end=50, resample_hz=20,
+              bp_freqmins = [0.5, 2., 5.],
+              bp_freqmaxs = [5, 9.9, 9.95],
+              margin=10,
+              max_amplitude=1e8):
+
     po = event.preferred_origin
     if(not po): return None
 
@@ -79,12 +84,8 @@ def extract_p(taupy_model, picker, event, station_longitude, station_latitude,
 
     tr = st[0]
     if(type(tr.data) == np.ndarray):
-        if(np.max(tr.data) > 1e8): return None
+        if(np.max(tr.data) > max_amplitude): return None
 
-        bp_freqmins = [0.5, 2., 5.]
-        bp_freqmaxs = [5, 9.9, 9.95]
-
-        margin = 10
         pickslist = []
         snrlist = []
         residuallist = []
@@ -123,16 +124,126 @@ def extract_p(taupy_model, picker, event, station_longitude, station_latitude,
         if (len(pickslist)):
             optimal_pick_idx = np.argmax(np.array(snrlist))
 
-            da = DistAz(po.lat, po.lon, station_latitude, station_longitude)
-
-            return event.public_id, po.lon, po.lat, da.getBaz(), \
-                    da.degreesToKilometers(da.getDelta()), \
-                    pickslist[optimal_pick_idx], residuallist[optimal_pick_idx], \
-                    snrlist[optimal_pick_idx]
+            return pickslist[optimal_pick_idx], residuallist[optimal_pick_idx], \
+                   snrlist[optimal_pick_idx], optimal_pick_idx
         # end if
     # end if
 
     return None
+# end func
+
+def extract_s(taupy_model, picker, event, station_longitude, station_latitude,
+              stn, ste, ba, win_start=-50, win_end=50, resample_hz=20,
+              bp_freqmins= [0.05, 2],
+              bp_freqmaxs = [0.5, 5],
+              margin=20,
+              max_amplitude=1e8):
+
+    po = event.preferred_origin
+    if(not po): return None
+
+    atimes = []
+    try:
+        atimes = taupy_model.get_travel_times_geo(po.depthkm, po.lat,
+                                                  po.lon, station_latitude,
+                                                  station_longitude,
+                                                  phase_list=('S',))
+    except:
+        return None
+    # end try
+
+    if (len(atimes) == 0): return None
+    tat = atimes[0].time # theoretical arrival time
+
+    tr = None
+    try:
+        stn = stn.slice(po.utctime + tat + win_start, po.utctime + tat + win_end)
+        stn.resample(resample_hz)
+
+        if(ste):
+            ste = ste.slice(po.utctime + tat + win_start, po.utctime + tat + win_end)
+            ste.resample(resample_hz)
+        # end if
+
+        if(ste):
+            if(type(stn[0].data) == np.ndarray and type(ste[0].data) == np.ndarray):
+                rc, tc = rotate_ne_rt(stn[0].data, ste[0].data, ba)
+                tr = Trace(data=tc, header=stn[0].stats)
+                #tr = Trace(data=np.sqrt(np.power(rc,2) + np.power(tc,2)), header=stn[0].stats)
+            # end if
+        else:
+            if(type(stn[0].data) == np.ndarray):
+                tr = stn[0]
+            # end if
+        # end if
+    except Exception as e:
+        return None
+    # end try
+
+    if(tr):
+        if(np.max(tr.data) > max_amplitude): return None
+
+        pickslist = []
+        snrlist = []
+        residuallist = []
+
+        tr.detrend('linear')
+        for i in range(len(bp_freqmins)):
+            trc = tr.copy()
+            trc.filter('bandpass', freqmin=bp_freqmins[i],
+                       freqmax=bp_freqmaxs[i], corners=4,
+                       zerophase=True)
+
+            try:
+                scnl, picks, polarity, snr, uncert = picker.picks(trc)
+
+                for ipick, pick in enumerate(picks):
+                    actualArrival = pick - po.utctime
+                    residual = tat - actualArrival
+
+                    if (np.fabs(residual) < margin):
+                        pickslist.append(pick)
+                        snrlist.append(snr[ipick])
+                        residuallist.append(residual)
+
+                        #summary = fbpicker.FBSummary(picker, trc)
+                        #summary = aicdpicker.AICDSummary(picker, trc)
+                        #outputPath = '/home/rakib/work/pst/picking/sarr'
+                        #ofn = '%s/%s.%s_%f_%d.s.png' % (outputPath, scnl, str(po.utctime), snr[0], i)
+                        #summary.plot_picks(show=False, savefn=ofn)
+                    # end if
+                # end for
+            except:
+                continue
+            # end try
+        # end for
+
+        if (len(pickslist)):
+            optimal_pick_idx = np.argmax(np.array(snrlist))
+
+            return pickslist[optimal_pick_idx], residuallist[optimal_pick_idx], \
+                   snrlist[optimal_pick_idx], optimal_pick_idx
+        # end if
+    # end if
+
+    return None
+# end func
+
+def getWorkloadEstimate(fds, originTimestamps):
+    totalTraceCount = 0
+    for nc, sc, start_time, end_time in fds.local_net_sta_list():
+
+        day = 24 * 3600
+        curr = start_time
+        while (curr < end_time):
+            eventIndices = (np.where((originTimestamps >= curr.timestamp) & \
+                                     (originTimestamps <= (curr + day).timestamp)))[0]
+
+            if(eventIndices.shape[0]>0): totalTraceCount += 1
+            curr += day
+        # wend
+    # end for
+    return totalTraceCount
 # end func
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -171,58 +282,11 @@ def process(asdf_source, event_folder, output_path):
         # end func
 
         outputConfigParameters()
-        # retrieve list of all event xml files
-        xml_files = recursive_glob(event_folder, '*.xml')
-        shuffle(xml_files) # shuffle to avoid concentration of large files
-        proc_workload = split_list(xml_files, nproc)
     # end if
 
-    # broadcast workload to all procs
-    proc_workload = comm.bcast(proc_workload, root=0)
-
-    print 'Rank %d: processing %d files'%(rank, len(proc_workload[rank]))
-
-    eventList = []
-    poTimestamps =[]
-    for ifn, fn in enumerate(proc_workload[rank]):
-        es = EventParser(fn).getEvents()
-        for i, (eid, e) in enumerate(es.iteritems()):
-            po = e.preferred_origin
-            if(not (po.depthkm >= 0)): continue
-
-            eventList.append(e)
-            poTimestamps.append(po.utctime.timestamp)
-        # end for
-    # end for
-
-    eventList = comm.allgather(eventList)
-    poTimestamps = comm.allgather(poTimestamps)
-
-    allEventList = []
-    allPOTimestamps = []
-
-    for iproc in np.arange(nproc):
-        for i, e in enumerate(eventList[iproc]):
-            allEventList.append(e)
-            allPOTimestamps.append(poTimestamps[iproc][i])
-        # end for
-    # end for
-    allPOTimestamps = np.array(allPOTimestamps)
-
-    if(rank==0):
-        print 'Collected %d event origins'%(len(allEventList))
-
-        hasPM = 0
-        hasMultipleMags = 0
-        for e in allEventList:
-            o = e.preferred_origin
-            if(e.preferred_magnitude): hasPM += 1
-            if(len(o.magnitude_list)): hasMultipleMags += 1
-        # end for
-
-        print '%d preferred origins have a preferred magnitude'%(hasPM)
-        print '%d preferred origins have at least one magnitude'%(hasMultipleMags)
-    # end if
+    cat = Catalog(event_folder)
+    events = cat.get_events()
+    originTimestamps = cat.get_preferred_origin_timestamps()
 
     # ==========================================
     #picker = fbpicker.FBPicker(t_long=5, freqmin=0.1, mode='std', t_ma=20, nsigma=8, \
@@ -232,31 +296,40 @@ def process(asdf_source, event_folder, output_path):
 
     taupyModel = TauPyModel(model='iasp91')
     fds = FederatedASDFDataSet(asdf_source, use_json_db=False, logger=None)
+    workload = getWorkloadEstimate(fds, originTimestamps)
 
     totalTraceCount = 0
     for nc, sc, start_time, end_time in fds.local_net_sta_list():
-        ofn = os.path.join(output_path, '%s.%s.%d.txt'%(nc, sc, rank))
-        of = open(ofn, 'w+')
+        header = '#eventID originTimestamp originLon originLat net sta cha pickTimestamp stationLon stationLat baz distKm ttResidual snr bandIndex\n'
+        ofnp = os.path.join(output_path, '%s.%s.p.%d.txt'%(nc, sc, rank))
+        ofp = open(ofnp, 'w+')
+        ofns = os.path.join(output_path, '%s.%s.s.%d.txt'%(nc, sc, rank))
+        ofs = open(ofns, 'w+')
+
+        ofp.write(header)
+        ofs.write(header)
 
         day = 24 * 3600
         dayCount = 0
         curr = start_time
+        traceCountP = 0
+        pickCountP = 0
+        traceCountS = 0
+        pickCountS = 0
         sw_start = datetime.now()
-        traceCount = 0
-        pickCount = 0
         while (curr < end_time):
-            eventIndices = (np.where((allPOTimestamps >= curr.timestamp) & \
-                                     (allPOTimestamps <= (curr + day).timestamp)))[0]
 
-            if(eventIndices.shape[0]>0): totalTraceCount += 1
-            if(eventIndices.shape[0]>0 and 0): ##########################
+            eventIndices = (np.where((originTimestamps >= curr.timestamp) & \
+                                     (originTimestamps <= (curr + day).timestamp)))[0]
+
+            if(eventIndices.shape[0]>0):
+                totalTraceCount += 1
                 stations = fds.get_stations(curr, curr + day, network=nc, station=sc)
                 stations_zch = [s for s in stations if 'Z' in s[3]]  # only Z channels
+                stations_nch = [s for s in stations if 'N' in s[3] or '1' in s[3]]  # only N channels
+                stations_ech = [s for s in stations if 'E' in s[3] or '2' in s[3]]  # only E channels
 
                 for codes in stations_zch:
-                    #if (codes[0] != 'AU'): continue
-                    #if (codes[1] != 'PTPS' or codes[1] != 'AS07'): continue
-
                     st = fds.get_waveforms(codes[0], codes[1], codes[2], codes[3],
                                            curr,
                                            curr+day, tag='raw_recording', automerge=True)
@@ -266,28 +339,86 @@ def process(asdf_source, event_folder, output_path):
 
                     slon, slat = codes[4], codes[5]
                     for ei in eventIndices:
-                        event = allEventList[ei]
+                        event = events[ei]
+                        po = event.preferred_origin
+                        da = DistAz(po.lat, po.lon, slat, slon)
 
                         result = extract_p(taupyModel, picker, event, slon, slat, st)
                         if(result):
-                            po = event.preferred_origin
-                            da = DistAz(po.lat, po.lon, slat, slon)
+                            pick, residual, snr, bi = result
 
-                            epid, polon, polat, baz, distkm, pick, residual, snr = result
-                            line = '%s %f %f ' \
-                                   '%s %s %s %f %f ' \
+                            line = '%s %f, %f %f ' \
+                                   '%s %s %s %f, %f %f ' \
                                    '%f %f ' \
-                                   '%s %f %f\n' % (epid, polon, polat,
-                                                   codes[0], codes[1], codes[3], slon, slat,
-                                                   baz, distkm,
-                                                   str(pick), residual, snr)
-                            of.write(line)
-                            pickCount += 1
+                                   '%f %f %d\n' % (event.public_id, po.utctime.timestamp, po.lon, po.lat,
+                                                   codes[0], codes[1], codes[3], pick.timestamp, slon, slat,
+                                                   da.getBaz(), da.degreesToKilometers(da.getDelta()),
+                                                   residual, snr, bi)
+                            ofp.write(line)
+                            pickCountP += 1
+                        # end if
+
+                        if (len(stations_nch) == 0 and len(stations_ech) == 0):
+                            result = extract_s(taupyModel, picker, event, slon, slat, st, None, da.getBaz())
+                            if (result):
+                                pick, residual, snr, bi = result
+
+                                line = '%s %f, %f %f ' \
+                                       '%s %s %s %f, %f %f ' \
+                                       '%f %f ' \
+                                       '%f %f %d\n' % (event.public_id, po.utctime.timestamp, po.lon, po.lat,
+                                                       codes[0], codes[1], codes[3], pick.timestamp, slon, slat,
+                                                       da.getBaz(), da.degreesToKilometers(da.getDelta()),
+                                                       residual, snr, bi)
+                                ofs.write(line)
+                                pickCountS += 1
+                            # end if
                         # end if
                     # end for
 
-                    traceCount += len(st)
+                    traceCountP += len(st)
                 # end for
+
+                if(len(stations_nch) != len(stations_ech)): continue
+                for codesn, codese in zip(stations_nch, stations_ech):
+                    stn = fds.get_waveforms(codesn[0], codesn[1], codesn[2], codesn[3],
+                                           curr,
+                                           curr + day, tag='raw_recording', automerge=True)
+                    ste = fds.get_waveforms(codese[0], codese[1], codese[2], codese[3],
+                                           curr,
+                                           curr + day, tag='raw_recording', automerge=True)
+
+                    if (len(stn) == 0): continue  # no data found or likely merge failed
+                    if (len(ste) == 0): continue  # no data found or likely merge failed
+                    if (stn[0].stats.sampling_rate < 1): continue  # likely corrupt data
+                    if (ste[0].stats.sampling_rate < 1): continue  # likely corrupt data
+
+                    slon, slat = codesn[4], codesn[5]
+
+                    for ei in eventIndices:
+                        event = events[ei]
+                        po = event.preferred_origin
+                        da = DistAz(po.lat, po.lon, slat, slon)
+
+                        result = extract_s(taupyModel, picker, event, slon, slat, stn, ste, da.getBaz())
+                        if (result):
+                            pick, residual, snr, bi = result
+
+                            line = '%s %f, %f %f ' \
+                                   '%s %s %s %f, %f %f ' \
+                                   '%f %f ' \
+                                   '%f %f %d\n' % (event.public_id, po.utctime.timestamp, po.lon, po.lat,
+                                                   codesn[0], codesn[1], '..T', pick.timestamp, slon, slat,
+                                                   da.getBaz(), da.degreesToKilometers(da.getDelta()),
+                                                   residual, snr, bi)
+                            ofs.write(line)
+                            pickCountS += 1
+                        # end if
+                    # end for
+
+                    traceCountS += (len(stn) + len(ste))
+                # end for
+
             # end if
             curr += day
             dayCount += 1
@@ -295,12 +426,14 @@ def process(asdf_source, event_folder, output_path):
         sw_stop = datetime.now()
         totalTime = (sw_stop - sw_start).total_seconds()
 
-        print 'Read %d traces and found %d picks on rank %d for network %s station %s in %f seconds'%\
-              (traceCount, pickCount, rank, nc, sc, totalTime), start_time, end_time
-        of.close()
-    # end for
+        print '(%5.2f%%) Read %d traces and found %d p-arrivals and %d s-arrivals on rank %d for ' \
+              'network %s station %s in %f s'%\
+              ((float(totalTraceCount)/float(workload)*100) if workload>0 else 100,
+               traceCountP+traceCountS, pickCountP, pickCountS, rank, nc, sc, totalTime)
 
-    print 'Need to process %d traces on rank %d'%(totalTraceCount, rank)
+        ofp.close()
+        ofs.close()
+    # end for
 
     del fds
 # end func
