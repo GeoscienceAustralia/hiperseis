2d349b64a8693fdb1fae19d6d2f8bf3a594323b8
Andrew Medlin <Andrew.Medlin@ga.gov.au>
2018-12-13 11:58:15 +1100

    Added pandas'ified data representation of station files with common index

--- inventory/engd2stxml.py
+++ inventory/engd2stxml.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
-import numpy as np
+'''Creates database of stations from .STN files which are not in IRIS database,
+   curates the data using heuristic rules, and exports new stations to station.xml
+'''
+import os
 import sys
-from obspy.core import utcdatetime
-from obspy.geodetics.base import locations2degrees
-from obspy.clients.fdsn.client import Client
-#client = Client("IRIS")
+
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.width', 240)
+import cStringIO as sio
+
+try:
+    import tqdm
+    show_progress = True
+except:
+    show_progress = False
+
+USE_PICKLE = True
+
 from obspy import read_inventory
 from obspy.core.inventory import Inventory, Network, Station, Channel, Site
-from obspy.clients.nrl import NRL
+from collections import defaultdict
+
+station_columns = ['Latitude', 'Longitude', 'Elevation', 'StationStart', 'StationEnd', 'ChannelCode', 'ChannelStart', 'ChannelEnd']
 
 def read_eng(fname):
-    ''' We read Engdahl file in this subroutine
+    ''' We read Engdahl file in this function which parses the following format of fixed width formatted columns:
 
      AAI   Ambon             BMG, Indonesia, IA-Ne              -3.6870  128.1945      0.0   2005001  2286324  I
      AAII                                                       -3.6871  128.1940      0.0   2005001  2286324  I
@@ -20,22 +37,33 @@ def read_eng(fname):
      AS01  Alice Springs Arra                                  -23.6647  133.9508      0.0   2005001  2286324  I
     0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
               10        20        30        40        50        60        70        80
+
+    Each Station Code (first column) might NOT be unique, and network codes are missing here, so duplicates will
+    simply be noted and handled later.
     '''
-    eng_stn=[]
-    with open(fname) as stations:
-        for string in stations:
-            columns = ((0,6),(59,67),(68,77),(78,86))
-            dataline = [ string[c[0]:c[1]] for c in columns ]
-            clean=[x.strip(' ') for x in dataline]
-            eng_stn.append(clean)
-    eng_stn=np.array(eng_stn)
-    stations.close()
-    return eng_stn
+    colspec_0 = ((0, 6), (59, 67), (68, 77), (78, 86))
+    col_names = ['StationCode', 'Latitude', 'Longitude', 'Elevation']
+    data_frame = pd.read_fwf(fname, colspecs=colspec_0, names=col_names, index_col=0)
+    # Populate missing data
+    # data_frame[['Latitude', 'Longitude', 'Elevation']] = np.nan
+    data_frame['StationStart'] = pd.NaT
+    data_frame['StationEnd'] = pd.NaT
+    data_frame['ChannelStart'] = pd.NaT
+    data_frame['ChannelEnd'] = pd.NaT
+    data_frame['ChannelCode'] = ''
+    data_frame = data_frame[station_columns]
+    # Assumed network code for files of this format.
+    data_frame = pd.concat([data_frame], keys=['GE'], names=['NetworkCode'])
+    # Compute number of duplicates
+    num_dupes = len(data_frame) - len(data_frame.index.unique())
+    print("{0}: {1} stations found with {2} duplicates".format(fname, len(data_frame), num_dupes))
+    return data_frame
 
 def read_isc(fname):
     # Engdahl file is imported and stored, now lets find appropriate network codes based on coordinates
     # first we read catalogue supplied by ISC that inherited Engdahl work
-    '''
+    '''Read ISC station inventory supplied by ISC, having such format:
+
     109C     32.8892 -117.1100     0.0 2006-06-01 04:11:18 2008-01-04 01:26:30
     109C     32.8882 -117.1050   150.0 2008-01-04 01:26:30
                  FDSN 109C   TA -- BHZ 2004-05-04 23:00:00 2005-03-03 23:59:59
@@ -45,162 +73,124 @@ def read_isc(fname):
     0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
               10        20        30        40        50        60        70        80
 
+    The following policies are applied in the merging of duplicate data within a given file
+    when read by this function:
+    * The first level of the index is the Network Code.
+    * For given station matched by station code and lat/long, a non-zero elevation, if present,
+      overwrites a 0.0 elevation.
+    * Where multiple Network Codes are present for a given station header, the network codes
+      are preserved as the first level of the table index. Within each network code for a 
+      given station, we tabulate the channel stages.
+    * Where Station Code on a channel data row does not match header Station Code, the mismatch
+      is logged, assumed to be an error, and ignored since the ISC data format in this file 
+      implies the station code should be the same as the header row.
+
+    Useful reference for adding level of multiindex: https://stackoverflow.com/questions/14744068/prepend-a-level-to-a-pandas-multiindex
     '''
-    isc_inv=[]
-    with open(fname) as stations:
-        for string in stations:
-            if len(string[0:5].strip())>2:
-                header=True
-                columns=((0,5),(7,16),(17,26),(27,34),(35,54,),(55,74))
-                dataline = [ string[c[0]:c[1]] for c in columns ]
-                lon=(dataline[2])
-                lat=(dataline[1])
-                ele=(dataline[3])
-                stn=dataline[0].strip()
-                dataline.insert(1,'IR')
-                dataline.insert(5,'Z')
-                clean=[x.strip(' ') for x in dataline]
-                isc_inv.append(clean)
-            else:
-                columns=((17,22),(23,27),(30,34),(35,54),(55,74))
-                dataline = [ string[c[0]:c[1]] for c in columns ]
-                if header:
-#                   print "replaced"
-                    isc_inv[-1][1]=dataline[1].strip()
-                    isc_inv[-1][5]=dataline[2].strip()
-                    header=False
-#               if dataline[0].strip() != stn:
-#                   print dataline, " Station header mismatch"
-                dataline.insert(2,ele)
-                dataline.insert(2,lon)
-                dataline.insert(2,lat)
-                clean=[x.strip(' ') for x in dataline]
-                isc_inv.append(clean)
 
+    def reportStationCount(df):
+        num_unique_stations = len(df.index.get_level_values('StationCode').unique())
+        print("{0}: {1} unique stations codes found".format(fname, num_unique_stations))
 
-    isc_inv=np.array(isc_inv)
-    stations.close()
-    return isc_inv
+    if USE_PICKLE:
+        pkl_name = fname + ".pkl"
+        if os.path.exists(pkl_name): 
+            print("Reading cached " + fname)
+            with open(pkl_name, 'rb') as f:
+                import cPickle as pkl
+                df_all = pkl.load(f)
+                reportStationCount(df_all)
+                return df_all
+
+    print("Parsing " + fname)
+    header_colspec = ((0, 5), (7, 16), (17, 26), (27, 34), (35, 54,), (55, 74))
+    header_cols = ['StationCode', 'Latitude', 'Longitude', 'Elevation', 'StationStart', 'StationEnd']
+    channel_colspec = ((13, 17), (17, 22), (23, 27), (30, 34), (35, 54), (55, 74))
+    channel_cols = ['FDSN', 'StationCode', 'NetworkCode', 'ChannelCode', 'ChannelStart', 'ChannelEnd']
+    df_list = []
+    # Due to irregular data format, read one row at a time from file. Consider replacing with a pre-processing pass 
+    # so that more than one row can be read at a time.
+    if show_progress:
+        pbar = tqdm.tqdm(total=os.path.getsize(fname), ascii=True)
+    with open(fname, "r", buffering=64*1024) as f:
+        line = f.readline()
+        if show_progress:
+            pbar.update(len(line))
+        hdr = None
+        while line.strip():
+            channels = []
+            while 'FDSN' in line:
+                # Read channel rows
+                line_input = sio.StringIO(line)
+                ch_data = pd.read_fwf(line_input, colspecs=channel_colspec, names=channel_cols, index_col=1, nrows=1, parse_dates=[4, 5])
+                assert ch_data.iloc[0]['FDSN'] == 'FDSN'
+                channels.append(ch_data)
+                line = f.readline()
+                if show_progress:
+                    pbar.update(len(line))
+            if hdr is not None:
+                # Combine header and channel data. 
+                # ch_all = pd.concat(channels, ignore_index=True)
+                if channels:
+                    ch_all = pd.concat(channels)
+                    netcode = ch_all.iloc[0]['NetworkCode']
+                    ch_all.drop('FDSN', axis=1, inplace=True)
+                    # TODO: Add warning here if there is more than one network code. Only the first will be respected.
+                    ch_all.drop('NetworkCode', axis=1, inplace=True)
+                    network_df = hdr.join(ch_all)
+                    # Make sure column ordering is consistent
+                    network_df = network_df[station_columns]
+                    # Add a first level index of network code
+                    network_df = pd.concat([network_df], keys=[netcode], names=['NetworkCode'])
+                else:
+                    netcode = 'IR'
+                    hdr['ChannelStart'] = pd.NaT
+                    hdr['ChannelEnd'] = pd.NaT
+                    hdr['ChannelCode'] = ''
+                    hdr = hdr[station_columns]
+                    network_df = pd.concat([hdr], keys=[netcode], names=['NetworkCode'])
+                df_list.append(network_df)
+                hdr = None
+            # Read header row
+            line_input = sio.StringIO(line)
+            hdr = pd.read_fwf(line_input, colspecs=header_colspec, names=header_cols, index_col=0, nrows=1, parse_dates=[4, 5])
+            line = f.readline()
+            if show_progress:
+                pbar.update(len(line))
+    if show_progress:
+        pbar.close()
+    print("Concatenating records...")
+    df_all = pd.concat(df_list, sort=False)
+    if USE_PICKLE:
+        with open(fname + ".pkl", "wb") as f:
+            import cPickle as pkl
+            pkl.dump(df_all, f, pkl.HIGHEST_PROTOCOL)
+    reportStationCount(df_all)
+    return df_all
 
 def main(argv):
-    inv = read_inventory("IRIS-ALL.xml")
+    # Read IRIS station database. Stations from STN files which exist here will 
+    if USE_PICKLE:
+        if os.path.exists("IRIS-ALL.pkl"): # doesn't work reliably on EC2 CentOS instance for some reason
+            with open('IRIS-ALL.pkl', 'rb') as f:
+                import cPickle as pkl
+                inv = pkl.load(f)
+        else:
+            with open("IRIS-ALL.xml", 'r', buffering=1024*1024) as f:
+                inv = read_inventory(f)
+            with open('IRIS-ALL.pkl', 'wb') as f:
+                import cPickle as pkl
+                pkl.dump(inv, f, pkl.HIGHEST_PROTOCOL)
+    else:
+        with open("IRIS-ALL.xml", 'r', buffering=1024*1024) as f:
+            inv = read_inventory(f)
 
-# unknown stations in Indonesia are usually installed by Potsdam and we assume they have network name GE
-    default_net='GE'
-    ehb1=read_eng('BMG.STN')
-    ehb2=read_eng('ISC.STN')
-    ehb=np.unique(np.vstack((ehb1,ehb2)),axis=0)
+    ehb_data_bmg = read_eng('BMG.STN')
+    ehb_data_isc = read_eng('ISC.STN')
 
-    isc1=read_isc('ehb.stn')
-    isc2=read_isc('iscehb.stn')
-    isc=np.unique(np.vstack((isc1,isc2)),axis=0)
-
-    catalogue=[]
-    our_xml=Inventory(networks=[],source='EHB')
-
-    for i in xrange(ehb.shape[0]):
-                 filed=False
-                 xml=False
-                 stn_found=isc[isc[:,0]==ehb[i,0],:]
-                 min_dist=10e10
-                 if stn_found.shape[0]>0:
-                       if stn_found.shape[0]>1:
-                           for j in xrange(stn_found.shape[0]):
-                              dist=locations2degrees(np.float(stn_found[j,2]),np.float(stn_found[j,3]),np.float(ehb[i,1]),np.float(ehb[i,2]))
-                              if dist < min_dist:
-                                 min_dist=dist
-                                 record=stn_found[j,:]
-                       else:
-                              min_dist=locations2degrees(np.float(stn_found[0,2]),np.float(stn_found[0,3]),np.float(ehb[i,1]),np.float(ehb[i,2]))
-                              record=stn_found[0,:]
-
-#                Now we try to find the same station in XML file
-#                if min_dist > 1. or stn_found.shape[0]==0:
-
-                 xstn_found=inv.select(station=ehb[i,0],channel="*HZ")
-
-                 if len(stn_found)==0 and len(xstn_found)==0:
-                     # we filed to find station anywhere and assign dummy values
-                     record=[ehb[i,0],default_net,ehb[i,1],ehb[i,2],ehb[i,3],'Z','1964-1-1 00:00:00','2599-12-31 23:59:59']
-                     min_dist=0.
-                     filed=True
-                 else:
-                     # if station is found somehwere we try to iterate and see if XML has data giving it preference through adding extra value to min_dist found in ISC
-                     if len(xstn_found)>0:
-#                        print "----------",len(xstn_found)
-#                        print xstn_found[0][0].latitude
-                         min_dist=min_dist+0.1
-                         for j in xrange(len(xstn_found)):
-                             dist=locations2degrees(xstn_found[j][0].latitude,xstn_found[j][0].longitude,np.float(ehb[i,1]),np.float(ehb[i,2]))
-                             if min_dist > dist:
-                                 min_dist=dist
-                                 record=xstn_found[j]
-#                                print record
-                                 xml=True
-
-# last defence if stations have been done but distance between declared and found locations are more than 1 degree
-                 if min_dist > 1:
-                       record=[ehb[i,0],default_net,ehb[i,1],ehb[i,2],ehb[i,3],'Z','1964-1-1 00:00:00','2599-12-31 23:59:59']
-                       filed=True
-                 if xml:
-                       #our_xml.networks.append(record)
-                       xml=False
-
-                 else:
-                    if filed:
-
-                        if len(record[7]) < 5:
-                             record[7]='2599-12-31 23:59:59'
-                        catalogue.append(record)
-
-                    else:
-
-                        stn_found=isc[(isc[:,0]==record[0]) & (isc[:,1]==record[1]),:]
-
-                        for k in xrange(stn_found.shape[0]):
-                                    net=Network(code=stn_found[k,1],stations=[],description=' ')
-                                    if len(stn_found[k,7]) < 5:
-                                           stn_found[k,7]='2599-12-31 23:59:59'
-                                    catalogue.append(stn_found[k,:])
-
-
-
-
-    stn_found=np.unique(np.array(catalogue),axis=0)
-    if len(stn_found[stn_found==''])>0 or len(stn_found[stn_found==' '])>0:
-              print "Some elements are empty, check the list"
-    for k in xrange(stn_found.shape[0]):
-
-              net=Network(code=stn_found[k,1],stations=[],description=' ')
-              if len(stn_found[k,7]) < 5:
-                     stn_found[k,7]='2599-12-31 23:59:59'
-              catalogue.append(stn_found[k,:])
-              sta=Station(code=stn_found[k,0],creation_date=utcdatetime.UTCDateTime(stn_found[k,6]), \
-              termination_date=utcdatetime.UTCDateTime(stn_found[k,7]), \
-              site=Site(name=' '), \
-              latitude=np.float(stn_found[k,2]), \
-              longitude=np.float(stn_found[k,3]), \
-              elevation=np.float(stn_found[k,4]))
-
-              cha=Channel(code=stn_found[k,5], \
-              depth=0., \
-              azimuth=0., \
-              dip=-90., \
-              location_code='', \
-              latitude=np.float(stn_found[k,2]), \
-              longitude=np.float(stn_found[k,3]), \
-              elevation=np.float(stn_found[k,4]))
-
-              sta.channels.append(cha)
-              net.stations.append(sta)
-              our_xml.networks.append(net)
-#             print 'np',stn_found[k,:]
-
-
-    our_xml.write("station.xml",format="stationxml",validate=True)
-    our_xml.write("station.txt",format="stationtxt")
-
-
+    isc1 = read_isc('ehb.stn')
+    isc2 = read_isc('iscehb.stn')
+    pass
 
 if __name__ == "__main__":
- main(sys.argv)
+    main(sys.argv)
