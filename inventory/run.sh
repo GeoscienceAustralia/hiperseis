862d54ef2306b887394aedd7a81f44037a1df5d2
Andrew Medlin <Andrew.Medlin@ga.gov.au>
2019-01-15 16:25:44 +1100

    Added qsub job params to script.

--- inventory/run.sh
+++ inventory/run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=1:mem=8gb:cput=7200s
+## To run, enter 'qsub run.sh'
+#PBS -P ha3
+#PBS -q normal
+#PBS -l mem=8gb
+#PBS -l jobfs=2gb
+#PBS -l ncpus=1
+#PBS -l cput=7200s
+#PBS -l software=python
+## Tell it gdata1a filesystem needs to be available
+#PBS -lother=gdata1a
+## The job will be executed from current working directory instead of home.
+#PBS -l wd
 #PBS -N StationMetadataCleanup
-time python ./engd2stxml.py
+python ./engd2stxml.py
