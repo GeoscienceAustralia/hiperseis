ccec142bfc95c729b987bd858901f0d04e0c2932
medlin01GA <Andrew.Medlin@ga.gov.au>
2019-02-13 16:31:06 +1100

    Getting notebooks working and some cleanup to address usability and maintainability

---
+++ seismic/xcorqc/notebooks/analytic_plot_utils.py
@@ -0,0 +1,31 @@
+import math
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+from shapely.geometry import Polygon
+from descartes import PolygonPatch
+
+
+def distance(origin, destination):
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+    radius = 6371  # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
+
+
+def drawBBox(minLon, minLat, maxLon, maxLat, bm, **kwargs):
+    bblons = np.array([minLon, maxLon, maxLon, minLon, minLon])
+    bblats = np.array([minLat, minLat, maxLat, maxLat, minLat])
+    
+    x, y = bm( bblons, bblats )
+    xy = zip(x,y)
+    poly = Polygon(xy)
+    bm.ax.add_patch(PolygonPatch(poly, **kwargs))
+
