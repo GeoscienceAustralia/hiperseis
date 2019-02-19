bee67397287437b381cb41feba2520d58aa98521
Rakib Hassan <rakib.hassan@ga.gov.au>
2019-02-20 10:19:23 +1100

    Moving Unused Files to Legacy

--- iloc_rstt/config.py
+++
@@ -1,7 +0,0 @@
-from os.path import join
-
-event_dir = join('tests', 'mocks', 'events', 'analyst_event_samples')
-
-event_file = join(event_dir, '772009.xml')
-
-max_station_dist = 120.0   # percentage of distance to scan for extra stations
