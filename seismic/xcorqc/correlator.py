0608eaa457a89662f0e0499bca83a02d4b8439ac
Rakib Hassan <rakib.hassan@ga.gov.au>
2019-03-04 12:33:11 +1100

    Adaptations for Ambient Noise Tomography
    * Streamlined cross-correlation data from station-pairs
    * Adapted Dataset class in correlator.py to work with FederatedASDFDataset class
    * Made numerous changes to facilitate cross-correlations for ambient noise tomography

--- seismic/xcorqc/correlator.py
+++ seismic/xcorqc/correlator.py
@@ -22,7 +22,7 @@ from scipy.spatial import cKDTree
 
 from mpi4py import MPI
 from obspy import UTCDateTime
-import pyasdf
+from seismic.ASDFdatabase.FederatedASDFDataSet import FederatedASDFDataSet
 
 import click
 
@@ -53,71 +53,29 @@ def xyz2rtp(x, y, z):
 
 
 class Dataset:
-    def __init__(self, asdf_file_name, station_names='*', ignore_json_db=False, zchan=None, nchan=None, echan=None):
+    def __init__(self, asdf_file_name, netsta_list='*'):
 
         self._data_path = asdf_file_name
-        self.ds = None
-        self.has_jason_db = False
-        self.ds_jason_db = None
         self._earth_radius = 6371  # km
 
-        try:
-            self.ds = pyasdf.ASDFDataSet(self._data_path, mode='r')
-        except:
-            raise NameError('Error reading file : %s' % (self._data_path))
-        # end try
-
-        if(not ignore_json_db):
-            # look for json db
-            files = glob.glob(os.path.dirname(self._data_path) + '/*.json')
-            for f in files:
-                if(os.path.splitext(os.path.basename(self._data_path))[0] in f):
-                    try:
-                        self.ds_jason_db = SeisDB(f)
-                        self.has_jason_db = True
-                    except:
-                        raise RuntimeError('Failed to load json file:%s' % (f))
-                    # end try
-                    break
-                # end if
-            # end for
-        # end if
-
+        self.fds = FederatedASDFDataSet(asdf_file_name)
         # Gather station metadata
-        station_subset = set(station_names.split(' ')) if station_names != '*' else station_names
-        self.stations = []
-        self.stations_metadata = defaultdict(list)
-        zchannels = set()
-        nchannels = set()
-        echannels = set()
+        netsta_list_subset = set(netsta_list.split(' ')) if netsta_list != '*' else netsta_list
+        self.netsta_list = []
+        self.metadata = defaultdict(list)
 
         rtps = []
-        for station in self.ds.waveforms:
-            sn = station._station_name.split('.')[1]
-
-            if(station_subset != '*'):
-                if sn not in station_subset:
+        for netsta in self.fds.unique_coordinates.keys():
+            if(netsta_list_subset != '*'):
+                if netsta not in netsta_list_subset:
                     continue
 
-            self.stations.append(sn)
-            self.stations_metadata[sn] = station
+            self.netsta_list.append(netsta)
+            self.metadata[netsta] = self.fds.unique_coordinates[netsta]
 
             rtps.append([self._earth_radius,
-                         np.radians(90 - station.coordinates['latitude']),
-                         np.radians(station.coordinates['longitude'])])
-
-            for nw in station.StationXML:
-                for st in nw:
-                    for ch in st:
-                        if 'Z' in ch.code or 'z' in ch.code:
-                            zchannels.add(ch.code)
-                        if 'N' in ch.code or 'n' in ch.code:
-                            nchannels.add(ch.code)
-                        if 'E' in ch.code or 'e' in ch.code:
-                            echannels.add(ch.code)
-                    # end for
-                # end for
-            # end for
+                         np.radians(90 - self.metadata[netsta][1]),
+                         np.radians(self.metadata[netsta][0])])
         # end for
 
         rtps = np.array(rtps)
@@ -125,90 +83,61 @@ class Dataset:
 
         self._tree = cKDTree(xyzs)
         self._cart_location = defaultdict(list)
-        for i, s in enumerate(self.stations):
-            self._cart_location[s] = xyzs[i, :]
+        for i, ns in enumerate(self.netsta_list):
+            self._cart_location[ns] = xyzs[i, :]
         # end for
-
-        if len(zchannels):
-            if(zchan is None):
-                assert len(zchannels) == 1, 'Multiple z-channels found %s' % (zchannels)
-            else:
-                zchannels = set([zchan]) if zchan in zchannels else None
-            if(zchannels is None):
-                assert 0, 'Invalid z-channel name'
-
-        if len(nchannels):
-            if(nchan is None):
-                assert len(nchannels) == 1, 'Multiple n-channels found %s' % (nchannels)
-            else:
-                nchannels = set([nchan]) if nchan in nchannels else None
-            if(nchannels is None):
-                assert 0, 'Invalid n-channel name'
-
-        if len(echannels):
-            if(echan is None):
-                assert len(echannels) == 1, 'Multiple e-channels found %s' % (echannels)
-            else:
-                echannels = set([echan]) if echan in echannels else None
-            if(echannels is None):
-                assert 0, 'Invalid e-channel name'
-
-        self.zchannel = zchannels.pop() if len(zchannels) else None
-        self.nchannel = nchannels.pop() if len(nchannels) else None
-        self.echannel = echannels.pop() if len(echannels) else None
     # end func
 
-    def get_closest_stations(self, station_name, other_dataset, nn=1):
-        assert isinstance(station_name, basestring), 'station_name must be a string'
+    def get_closest_stations(self, netsta, other_dataset, nn=1):
+        assert isinstance(netsta, basestring), 'station_name must be a string'
         assert isinstance(other_dataset, Dataset), 'other_dataset must be an instance of Dataset'
-        station_name = station_name.upper()
+        netsta = netsta.upper()
 
-        assert station_name in self.stations, 'station %s not found'%(station_name)
+        assert netsta in self.netsta_list, '%s not found'%(netsta)
 
-        d, l = other_dataset._tree.query(self._cart_location[station_name], nn)
+        d, l = other_dataset._tree.query(self._cart_location[netsta], nn)
 
         if isinstance(l, int):
             l = [l]
 
-        l = l[l<len(other_dataset.stations)]
+        l = l[l<len(other_dataset.netsta_list)]
 
         if isinstance(l, int):
             l = [l]
 
         assert len(l), 'No stations found..'
 
-        return list(np.array(other_dataset.stations)[l])
+        return list(np.array(other_dataset.netsta_list)[l])
     # end func
 # end class
 
 def process(data_source1, data_source2, output_path,
             interval_seconds, window_seconds,
-            ds1_dec_factor=1, ds2_dec_factor=1, nearest_neighbours=1,
-            fmin=0.3, fmax=1., station_names1='*', station_names2='*',
+            resample_rate=None, nearest_neighbours=1,
+            fmin=0.3, fmax=1., netsta_list1='*', netsta_list2='*',
             start_time='1970-01-01T00:00:00', end_time='2100-01-01T00:00:00',
-            clip_to_2std=False, one_bit_normalize=False, read_buffer_size=10,
+            clip_to_2std=False, whitening=False, one_bit_normalize=False, read_buffer_size=10,
             ds1_zchan=None, ds1_nchan=None, ds1_echan=None,
-            ds2_zchan=None, ds2_nchan=None, ds2_echan=None,
-            ignore_ds1_json_db=True, ignore_ds2_json_db=True):
+            ds2_zchan=None, ds2_nchan=None, ds2_echan=None):
     """
-    DATA_SOURCE1: Path to ASDF file \n
-    DATA_SOURCE2: Path to ASDF file \n
+    DATA_SOURCE1: Text file containing paths to ASDF files \n
+    DATA_SOURCE2: Text file containing paths to ASDF files \n
     OUTPUT_PATH: Output folder \n
     INTERVAL_SECONDS: Length of time window (s) over which to compute cross-correlations; e.g. 86400 for 1 day \n
     WINDOW_SECONDS: Length of stacking window (s); e.g 3600 for an hour. INTERVAL_SECONDS must be a multiple of
                     WINDOW_SECONDS; no stacking is performed if they are of the same size.
     """
     read_buffer_size *= interval_seconds
-    station_names1 = str(station_names1)
-    station_names2 = str(station_names2)
+    netsta_list1 = str(netsta_list1)
+    netsta_list2 = str(netsta_list2)
 
     comm = MPI.COMM_WORLD
     nproc = comm.Get_size()
     rank = comm.Get_rank()
     proc_stations = defaultdict(list)
 
-    ds1 = Dataset(data_source1, station_names1, ignore_ds1_json_db, ds1_zchan, ds1_nchan, ds1_echan)
-    ds2 = Dataset(data_source2, station_names2, ignore_ds2_json_db, ds2_zchan, ds2_nchan, ds2_echan)
+    ds1 = Dataset(data_source1, netsta_list1)
+    ds2 = Dataset(data_source2, netsta_list2)
 
     if (rank == 0):
         def outputConfigParameters():
@@ -224,13 +153,12 @@ def process(data_source1, data_source2, output_path,
             f.write('%25s\t\t: %s\n' % ('INTERVAL_SECONDS', interval_seconds))
             f.write('%25s\t\t: %s\n\n' % ('WINDOW_SECONDS', window_seconds))
 
-            f.write('%25s\t\t: %s\n' % ('--ds1-dec-factor', ds1_dec_factor))
-            f.write('%25s\t\t: %s\n' % ('--ds2-dec-factor', ds2_dec_factor))
+            f.write('%25s\t\t: %s\n' % ('--resample-rate', resample_rate))
             f.write('%25s\t\t: %s\n' % ('--nearest-neighbours', nearest_neighbours))
             f.write('%25s\t\t: %s\n' % ('--fmin', fmin))
             f.write('%25s\t\t: %s\n' % ('--fmax', fmax))
-            f.write('%25s\t\t: %s\n' % ('--station-names1', station_names1))
-            f.write('%25s\t\t: %s\n' % ('--station-names2', station_names2))
+            f.write('%25s\t\t: %s\n' % ('--station-names1', netsta_list1))
+            f.write('%25s\t\t: %s\n' % ('--station-names2', netsta_list2))
             f.write('%25s\t\t: %s\n' % ('--start-time', start_time))
             f.write('%25s\t\t: %s\n' % ('--end-time', end_time))
             f.write('%25s\t\t: %s\n' % ('--clip-to-2std', clip_to_2std))
@@ -245,12 +173,12 @@ def process(data_source1, data_source2, output_path,
         # split work over stations in ds1 for the time being
         count = 0
         for iproc in np.arange(nproc):
-            for istation in np.arange(np.divide(len(ds1.stations), nproc)):
-                proc_stations[iproc].append(ds1.stations[count])
+            for istation in np.arange(np.divide(len(ds1.netsta_list), nproc)):
+                proc_stations[iproc].append(ds1.netsta_list[count])
                 count += 1
         # end for
-        for iproc in np.arange(np.mod(len(ds1.stations), nproc)):
-            proc_stations[iproc].append(ds1.stations[count])
+        for iproc in np.arange(np.mod(len(ds1.netsta_list), nproc)):
+            proc_stations[iproc].append(ds1.netsta_list[count])
             count += 1
         # end for
     # end if
@@ -269,18 +197,19 @@ def process(data_source1, data_source2, output_path,
             else:
                 st2list = ds1.get_closest_stations(st1, ds2, nn=nearest_neighbours)
         else:
-            st2list = ds2.stations
+            st2list = ds2.netsta_list
 
         for st2 in st2list:
             startTime = UTCDateTime(start_time)
             endTime = UTCDateTime(end_time)
 
-            x, xCorrResDict, wcResDict = IntervalStackXCorr(ds1.ds, ds1.ds_jason_db, ds2.ds, ds2.ds_jason_db, startTime,
-                                                            endTime, [st1], [st2], ds1.zchannel, ds2.zchannel,
-                                                            ds1_dec_factor,
-                                                            ds2_dec_factor, read_buffer_size, interval_seconds,
-                                                            window_seconds, fmin, fmax, clip_to_2std, one_bit_normalize,
-                                                            output_path, 2)
+            if (st1 == st2): continue
+
+            x, xCorrResDict, wcResDict = IntervalStackXCorr(ds1.fds, ds2.fds, startTime,
+                                                            endTime, st1, st2, ds1_zchan, ds2_zchan,
+                                                            resample_rate, read_buffer_size, interval_seconds,
+                                                            window_seconds, fmin, fmax, clip_to_2std, whitening,
+                                                            one_bit_normalize, output_path, 2)
         # end for
     # end for
 # end func
@@ -299,8 +228,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
                 type=int)
 @click.argument('window-seconds', required=True,
                 type=int)
-@click.option('--ds1-dec-factor', default=1, help="Decimation factor for data-source-1")
-@click.option('--ds2-dec-factor', default=1, help="Decimation factor for data-source-2")
+@click.option('--resample-rate', default=None, help="Resampling rate (Hz); applies to both datasets")
 @click.option('--nearest-neighbours', default=-1, help="Number of nearest neighbouring stations in data-source-2"
                                                        " to correlate against a given station in data-source-1. If"
                                                        " set to -1, correlations for a cross-product of all stations"
@@ -321,6 +249,9 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option('--clip-to-2std', default=False,
               type=bool,
               help="Clip data in each window to +/- 2 standard deviations")
+@click.option('--whitening', default=False,
+              type=bool,
+              help="Apply spectral whitening")
 @click.option('--one-bit-normalize', default=False,
               type=bool,
               help="Apply one-bit normalization to data in each window")
@@ -328,34 +259,29 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               type=int,
               help="Data read buffer size; default is 10 x 'interval_seconds'. This parameter allows fetching data in bulk,"
                    " which can improve efficiency, but has no effect on the results produced")
-@click.option('--ds1-zchan', default=None,
+@click.option('--ds1-zchan', default='BHZ',
               type=str,
               help="Name of z-channel for data-source-1. This parameter and the ones following are only required when "
                    "channel names are ambiguous, e.g. ['HHZ', u'HYZ', u'HNZ', u'MFZ']")
-@click.option('--ds1-nchan', default=None,
+@click.option('--ds1-nchan', default='BHN',
               type=str,
               help="Name of n-channel for data-source-1")
-@click.option('--ds1-echan', default=None,
+@click.option('--ds1-echan', default='BHE',
               type=str,
               help="Name of e-channel for data-source-1")
-@click.option('--ds2-zchan', default=None,
+@click.option('--ds2-zchan', default='BHZ',
               type=str,
               help="Name of z-channel for data-source-2")
-@click.option('--ds2-nchan', default=None,
+@click.option('--ds2-nchan', default='BHN',
               type=str,
               help="Name of n-channel for data-source-2")
-@click.option('--ds2-echan', default=None,
+@click.option('--ds2-echan', default='BHE',
               type=str,
               help="Name of e-channel for data-source-2")
-@click.option('--ignore-ds1-json-db', is_flag=True,
-              help="Ignore json db for data-source-1, if found")
-@click.option('--ignore-ds2-json-db', is_flag=True,
-              help="Ignore json db for data-source-2, if found")
-def main(data_source1, data_source2, output_path, interval_seconds, window_seconds, ds1_dec_factor,
-            ds2_dec_factor, nearest_neighbours, fmin, fmax, station_names1, station_names2, start_time,
-            end_time, clip_to_2std, one_bit_normalize, read_buffer_size,
-            ds1_zchan, ds1_nchan, ds1_echan, ds2_zchan, ds2_nchan, ds2_echan,
-            ignore_ds1_json_db, ignore_ds2_json_db):
+def main(data_source1, data_source2, output_path, interval_seconds, window_seconds, resample_rate,
+            nearest_neighbours, fmin, fmax, station_names1, station_names2, start_time,
+            end_time, clip_to_2std, whitening, one_bit_normalize, read_buffer_size,
+            ds1_zchan, ds1_nchan, ds1_echan, ds2_zchan, ds2_nchan, ds2_echan):
     """
     DATA_SOURCE1: Path to ASDF file \n
     DATA_SOURCE2: Path to ASDF file \n
@@ -365,11 +291,12 @@ def main(data_source1, data_source2, output_path, interval_seconds, window_secon
                     WINDOW_SECONDS; no stacking is performed if they are of the same size.
     """
 
-    process(data_source1, data_source2, output_path, interval_seconds, window_seconds, ds1_dec_factor,
-            ds2_dec_factor, nearest_neighbours, fmin, fmax, station_names1, station_names2, start_time,
-            end_time, clip_to_2std, one_bit_normalize, read_buffer_size,
-            ds1_zchan, ds1_nchan, ds1_echan, ds2_zchan, ds2_nchan, ds2_echan,
-            ignore_ds1_json_db, ignore_ds2_json_db)
+    if(resample_rate): resample_rate = float(resample_rate)
+
+    process(data_source1, data_source2, output_path, interval_seconds, window_seconds, resample_rate,
+            nearest_neighbours, fmin, fmax, station_names1, station_names2, start_time,
+            end_time, clip_to_2std, whitening, one_bit_normalize, read_buffer_size,
+            ds1_zchan, ds1_nchan, ds1_echan, ds2_zchan, ds2_nchan, ds2_echan)
 # end func
 
 if __name__ == '__main__':
