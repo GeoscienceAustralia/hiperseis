6691e7f48c283f2fdf6425cb30827d742861baf7
medlin01GA <Andrew.Medlin@ga.gov.au>
2019-03-07 17:56:52 +1100

    Added scripts and files to test Raijin setup

---
+++ seismic/xcorqc/test/generate_test_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# Simple helper script to generate small data files for validation of Python setup on Raijin
+
+import sys
+
+from obspy import UTCDateTime, Stream
+from obspy.clients.fdsn.client import Client
+
+import pyasdf
+
+NETWORK = 'AU'
+CHANNEL = 'BHZ'
+TAG = 'data'
+TIME_RANGE = ("2011-03-11T00:00:00Z", "2011-03-11T01:00:00Z")
+
+def generateStationTestData(sta):
+
+    time_range = (UTCDateTime(TIME_RANGE[0]),
+                  UTCDateTime(TIME_RANGE[1]))
+
+    client = Client("IRIS")
+    inv = client.get_stations(network=NETWORK, station=sta, channel=CHANNEL, 
+                              starttime=time_range[0], endtime=time_range[1], level='channel')
+    print(inv)
+
+    traces = client.get_waveforms(network=NETWORK, station=sta, channel=CHANNEL, location='*', 
+                                  starttime=time_range[0], endtime=time_range[1])
+    print(traces)
+
+    outfile = 'test_data_' + sta + '.h5'
+    asdf_out = pyasdf.ASDFDataSet(outfile, mode='w')
+    asdf_out.add_stationxml(inv)
+    asdf_out.add_waveforms(traces, TAG)
+
+    print("Saved data to " + outfile)
+
+
+if __name__ == "__main__":
+    generateStationTestData('ARMA')
+    generateStationTestData('CMSA')
+
