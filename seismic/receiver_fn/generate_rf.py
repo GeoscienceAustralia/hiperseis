ff4c2248159d2894b8019801b7585841d4c2e4eb
Andrew Medlin <Andrew.Medlin@ga.gov.au>
2019-04-18 17:00:17 +1000

    Merged parallel and serial implementations into a single version of generate_rf.py

--- seismic/receiver_fn/generate_rf.py
+++ seismic/receiver_fn/generate_rf.py
@@ -11,6 +11,12 @@ from rf import IterMultipleComponents
 # from rf.profile import profile
 from tqdm import tqdm
 
+try:
+    from joblib import Parallel, delayed
+    parallel_available = True
+except:
+    parallel_available = False
+
 # pylint: disable=invalid-name
 
 RESAMPLE_RATE_HZ = 100
@@ -20,20 +26,14 @@ TRIM_START_TIME_SEC = -25.0
 TRIM_END_TIME_SEC = 75.0
 EXCLUDE_STATION_CODE = ['MIJ2', 'MIL2']
 
-data = read_rf('/g/data/ha3/am7399/shared/OA_event_waveforms_for_rf_20171001T120000-20171015T120000.h5', 'H5')
+RUN_PARALLEL = True
 
-# exclude bad stations
-inc_set = list(set([tr.stats.inclination for tr in data]))
-data_filtered = RFStream([tr for tr in data if tr.stats.inclination in inc_set and tr.stats.station
-                          not in EXCLUDE_STATION_CODE])
-
-stream = RFStream()
-for i, stream3c in enumerate(tqdm(IterMultipleComponents(data, 'onset', 3))):
+def generate_rf(i, stream3c):
     stream3c.detrend('linear').interpolate(RESAMPLE_RATE_HZ)
     stream3c.taper(TAPER_LIMIT)
     stream3c.filter('bandpass', freqmin=FILTER_BAND_HZ[0], freqmax=FILTER_BAND_HZ[1], corners=2, zerophase=True)
     if len(stream3c) != 3:
-        continue
+        return RFStream()
 
     try:
         # LQT receiver functions are default
@@ -42,10 +42,10 @@ for i, stream3c in enumerate(tqdm(IterMultipleComponents(data, 'onset', 3))):
         stream3c.rf(rotate='NE->RT')
         # stream3c.rf(rotate='NE->RT',deconvolve='freq',gauss=2.0)
     except ValueError as e:
-        print("ERROR: Failed on stream:\n{}".format(stream3c))
+        print("ERROR: Failed on stream {}:\n{}".format(i, stream3c))
         print(e)
         print("(continuing from error...)")
-        continue
+        return RFStream()
 
     # Note the parameters of gaussian pulse and its width where
     # Value of "a" | Frequency (hz) at which G(f) = 0.1 |  Approximate Pulse Width (s)
@@ -80,7 +80,39 @@ for i, stream3c in enumerate(tqdm(IterMultipleComponents(data, 'onset', 3))):
 #   stream3c[2].data = stream3c[0].data*(amax['amax']/np.amax(stream3c[0].data))  # Is this supposed to be zero index here?
 
     stream3c.trim2(TRIM_START_TIME_SEC, TRIM_END_TIME_SEC, 'onset')
-    stream.extend(stream3c)
+
+    return stream3c
 
 
-stream.write('/g/data/ha3/am7399/shared/OA_event_waveforms_for_rf_20171001T120000-20171015T120000_ZRT.h5', 'H5')
+if RUN_PARALLEL:
+    assert parallel_available, "Cannot run parallel as joblib import failed"
+
+# Read source data
+print("Reading source file...")
+data = read_rf('/local/p25/am7399/tmp/OA_event_waveforms_for_rf_20170913T235913-20181128T011114_snapshot.h5', 'H5')
+#data = read_rf('/local/p25/am7399/tmp/OA_event_waveforms_for_rf_20171001T120000-20171015T120000.h5', 'H5')
+
+# exclude bad stations
+print("Filtering input data...")
+inc_set = list(set([tr.stats.inclination for tr in data]))
+data_filtered = RFStream([tr for tr in data if tr.stats.inclination in inc_set and tr.stats.station
+                          not in EXCLUDE_STATION_CODE])
+
+if RUN_PARALLEL:
+    # Process in parallel
+    print("Parallel processing...")
+    rf_streams = Parallel(n_jobs=-1, verbose=5, max_nbytes='8M')\
+        (delayed(generate_rf)(i, s) for i, s in enumerate(IterMultipleComponents(data_filtered, 'onset', 3)))
+else:
+    # Process in serial
+    print("Processing...")
+    rf_streams = list((generate_rf(i, s) for i, s in enumerate(IterMultipleComponents(data_filtered, 'onset', 3))))
+
+# Write to output file
+print("Generating output file...")
+stream = RFStream()
+for rf in rf_streams:
+    stream.extend(rf)
+#stream.write('/local/p25/am7399/tmp/test_rf_ZRT.h5', 'H5')
+stream.write('/local/p25/am7399/tmp/OA_event_waveforms_for_rf_20170913T235913-20181128T011114_snapshot_ZRT.h5', 'H5')
+print("SUCCESS!")
