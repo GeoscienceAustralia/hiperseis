f0661ae12d2968b7a266e4105f01e8e6fc7c1519
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2019-05-16 16:59:53 +1000

    updated scripts to use previously harvested autopicks from ASDF database

--- seismic/ml_classifier/autopick-classifier.py
+++ seismic/ml_classifier/autopick-classifier.py
@@ -1,37 +1,22 @@
-import sys
-sys.path.append('/g/data1a/ha3/rakib/seismic/pst/passive-seismic')
-from ASDFdatabase.FederatedASDFDataSet import FederatedASDFDataSet
 from data_harvester.autopicks import pickLoader
-
-from obspy.clients.fdsn.client import Client
 
 import model
 import numpy as np
-
-ic=Client("IRIS")
-fds=FederatedASDFDataSet('/g/data/ha3/Passive/SHARED_DATA/Index/asdf_files.txt', variant='db', use_json_db=True, logger=None)
-
-pickIt=pickLoader(fds,ic)
+import autopick_data
 
 network=model.shakenet(pretrained_weights='shakenet-model.hdf5')
 
-outfile='/g/data/ha3/rlt118/curatedpicks/ensemble.s.txt'
-rawoutfile='/g/data/ha3/rlt118/curatedpicks/ensemble.s.avail.txt'
-poutfile='/g/data/ha3/rlt118/curatedpicks/ensemble.s.badp.txt'
-with open(outfile,'w') as f, open(rawoutfile,'w') as g, open(poutfile,'w') as e:
+datagen=autopick_data.dataGenerator(128)
+
+outfile='/g/data/ha3/rlt118/autopicks/ensemble.s.verified.txt'
+
+
+
+with open(outfile,'w') as f:
     #initialise the file with the same format as the input    
     f.write("#eventID originTimestamp mag originLon originLat originDepthKm net sta cha pickTimestamp phase stationLon stationLat az baz distance ttResidual snr qualityMeasureCWT domFreq qualityMeasureSlope bandIndex nSigma\n")
-    g.write("#eventID originTimestamp mag originLon originLat originDepthKm net sta cha pickTimestamp phase stationLon stationLat az baz distance ttResidual snr qualityMeasureCWT domFreq qualityMeasureSlope bandIndex nSigma\n")
-    for pick,data in pickIt:
-        print pick[15]+" degrees"
-        inArr=np.resize(data,(1,6002,1))
-        result= network.predict(inArr,batch_size=1)
-        g.write(' '.join(pick)+'\n')
-        if np.argmax(result[0])==0:
-            f.write(' '.join(pick)+'\n')
-            print "Is an S wave"
-        elif np.argmax(result[0])==1:
-            print 'false P wave'
-            e.write(' '.join(pick)+'\n')
-        else:
-            print "Possibly not an S wave"
+
+    results=network.predict_generator(datagen)
+
+print len(results)
+
--- seismic/ml_classifier/scoreClassifier.py
+++ seismic/ml_classifier/scoreClassifier.py
@@ -2,48 +2,31 @@ from model import *
 from data import *
 
 sGen=SGenerator(16)
-pGen=PGenerator(16)
 nGen=NGenerator(16)
 
 model=shakenet(pretrained_weights='shakenet-model.hdf5')
 
 sRes= model.predict_generator(sGen,steps=len(sGen))
-pRes= model.predict_generator(pGen,steps=len(pGen))
 nRes= model.predict_generator(nGen,steps=len(nGen))
 
 tS=0
 fS=0
-tP=0
-fP=0
 tN=0
 fN=0
 
 for result in sRes:
     if np.argmax(result)==0:
         tS+=1
-    elif np.argmax(result)==1:
-        fP+=1
-    else:
-        fN+=1
-
-for result in pRes:
-    if np.argmax(result)==0:
-        fS+=1
-    elif np.argmax(result)==1:
-        tP+=1
     else:
         fN+=1
 
 for result in nRes:
     if np.argmax(result)==0:
         fS+=1
-    elif np.argmax(result)==1:
-        fP+=1
     else:
         tN+=1
 
 print "S recall = "+str(float(tS)/len(sRes))+", S precision = "+str(float(tS)/(tS+fS))
-print "P recall = "+str(float(tP)/len(pRes))+", P precision = "+str(float(tP)/(tP+fP))
 print "N recall = "+str(float(tN)/len(nRes))+", N precision = "+str(float(tN)/(tN+fN))
 
 tGen=testGenerator(16)
