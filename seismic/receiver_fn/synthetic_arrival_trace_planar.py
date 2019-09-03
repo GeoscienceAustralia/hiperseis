9ed0720b4696e28851c24a68f66131021885e835
Andrew Medlin <Andrew.Medlin@ga.gov.au>
2019-09-03 12:13:09 +1000

    Added realistic teleseismic distances, range of inclinations, and numerically found maximum H-k solution

--- seismic/receiver_fn/synthetic_arrival_trace_planar.py
+++ seismic/receiver_fn/synthetic_arrival_trace_planar.py
@@ -32,7 +32,7 @@ def generate_synth_rf(arrival_times, arrival_amplitudes, fs_hz=100.0, window_sec
 
 
 def main():
-    H = 40  # km
+    H = 50  # km
 
     V_p = 6.4  # km/s, top (crust) layer
     V_s = 4.0  # km/s, top (crust) layer
@@ -41,45 +41,79 @@ def main():
     print("H = {:3g}".format(H))
     print("k = {:3g}".format(k))
 
-    inc_deg = 15.0
-    theta_p = np.deg2rad(inc_deg)
-    p = np.sin(theta_p)/V_p
+    # Generate function mapping ray parameter to teleseismic distance
+    ts_distance = np.linspace(25, 95, 71)
+    inc = np.zeros_like(ts_distance)
+    model = obspy.taup.TauPyModel(model="iasp91")
+    source_depth_km = 10.0
+    for i, d in enumerate(ts_distance):
+        ray = model.get_ray_paths(source_depth_km, d, phase_list=['P'])
+        inc[i] = ray[0].incident_angle  # pylint: disable=unsupported-assignment-operation
+    # end for
+    plt.plot(inc, ts_distance, '-+')
+    plt.xlabel('P-arrival inclination (deg)')
+    plt.ylabel('Teleseismic distance (deg)')
+    plt.title("Relationship between incident angle and teleseismic distance\n(source depth = {:2g} km)".format(
+        source_depth_km))
+    plt.grid("#80808080", linestyle=":")
+    plt.savefig("c:\\temp\\inclination_dist.png", dpi=300)
 
-    t1 = H*(np.sqrt((k*k/V_p/V_p) - p*p) - np.sqrt(1.0/V_p/V_p - p*p))
-    t2 = H*(np.sqrt((k*k/V_p/V_p) - p*p) + np.sqrt(1.0/V_p/V_p - p*p))
-    print("Arrival times: {}".format([t1, t2]))
+    # Create interpolator to convert inclination to teleseismic distance
+    interp_dist = scipy.interpolate.interp1d(inc, ts_distance)
 
-    arrivals = [0, t1, t2]
-    amplitudes = [1, 0.5, 0.4]
-    window = (-10.0, 50.0)  # sec
-    fs = 100.0
-    _, signal = generate_synth_rf(arrivals, amplitudes, fs_hz=fs, window_sec=window)
+    # Loop over valid range of inclinations and generate synthetic RFs
+    inclinations = np.linspace(np.min(inc), np.max(inc), 10)
+    traces = []
+    for inc_deg in inclinations:
+        theta_p = np.deg2rad(inc_deg)
+        p = np.sin(theta_p)/V_p
 
-    now = obspy.UTCDateTime.now()
-    dt = float(window[1] - window[0])
-    end = now + dt
-    onset = now - window[0]
-    header = {'network': 'OA', 'station': 'TST', 'location': '0M', 'channel': 'HHR', 'sampling_rate': fs,
-              'starttime': now, 'endtime': end, 'onset': onset,
-              'station_latitude': -19.0, 'station_longitude': 137.0,
-              'slowness': p*rf_util.KM_PER_DEG, 'inclination': inc_deg,
-              'back_azimuth': 0, 'distance': 45}
-    tr = rf.rfstream.RFTrace(data=signal, header=header)
-    tr = tr.decimate(10, no_filter=True)
-    tr_list = [tr, tr, tr]
-    # stream = rf.RFStream(tr_list)
-    # _ = rf_plot_utils.plot_rf_stack(stream)
-    # plt.show()
+        t1 = H*(np.sqrt((k*k/V_p/V_p) - p*p) - np.sqrt(1.0/V_p/V_p - p*p))
+        t2 = H*(np.sqrt((k*k/V_p/V_p) - p*p) + np.sqrt(1.0/V_p/V_p - p*p))
+        print("Inclination {:3g} arrival times: {}".format(inc_deg, [t1, t2]))
 
-    station_db = {'HHR': tr_list}
+        arrivals = [0, t1, t2]
+        amplitudes = [1, 0.5, 0.4]
+        window = (-10.0, 50.0)  # sec
+        fs = 100.0
+        _, signal = generate_synth_rf(arrivals, amplitudes, fs_hz=fs, window_sec=window)
 
-    k, h, hk = rf_stacking.compute_hk_stack(station_db, 'HHR', include_t3=False)
-    rf_plot_utils.plot_hk_stack(k, h, hk[0], save_file="c:\\temp\\Ps.png", show=False)
-    rf_plot_utils.plot_hk_stack(k, h, hk[1], save_file="c:\\temp\\Ppps.png", show=False)
-    stack = rf_stacking.compute_weighted_stack(hk, weighting=(0.5, 0.5))
-    rf_plot_utils.plot_hk_stack(k, h, stack, save_file="c:\\temp\\synth_stack_0.5_0.5.png", show=False)
+        now = obspy.UTCDateTime.now()
+        dt = float(window[1] - window[0])
+        end = now + dt
+        onset = now - window[0]
+        header = {'network': 'SY', 'station': 'TST', 'location': 'GA', 'channel': 'HHR', 'sampling_rate': fs,
+                  'starttime': now, 'endtime': end, 'onset': onset,
+                  'station_latitude': -19.0, 'station_longitude': 137.0,
+                  'slowness': p*rf_util.KM_PER_DEG, 'inclination': inc_deg,
+                  'back_azimuth': 0, 'distance': float(interp_dist(inc_deg))}
+        tr = rf.rfstream.RFTrace(data=signal, header=header)
+        tr = tr.decimate(10, no_filter=True)
+        traces.append(tr)
+    # end for
+
+    stream = rf.RFStream(traces)
+    _ = rf_plot_utils.plot_rf_stack(stream, time_window=(-10, 30), save_file="c:\\temp\\synth_rf.png", dpi=300)
+
+    station_db = {'HHR': traces}
+
+    k, h, hk = rf_stacking.compute_hk_stack(station_db, 'HHR', h_range=np.linspace(20.0, 70.0, 1001), include_t3=False)
+    rf_plot_utils.plot_hk_stack(k, h, hk[0], title="Synthetic Ps component",
+                                save_file="c:\\temp\\Ps.png", show=False)
+    rf_plot_utils.plot_hk_stack(k, h, hk[1], title="Synthetic Ppps component",
+                                save_file="c:\\temp\\Ppps.png", show=False)
+    w = (0.5, 0.5)
+    w_str = "w={:2g},{:2g}".format(*w)
+    stack = rf_stacking.compute_weighted_stack(hk, weighting=w)
+    rf_plot_utils.plot_hk_stack(k, h, stack, num=len(traces), title="Synthetic H-k stack ({})".format(w_str),
+                                save_file="c:\\temp\\synth_stack_{}.png".format(w_str), show=False)
     # stack = rf_stacking.compute_weighted_stack(hk, weighting=(0.8, 0.2))
     # rf_plot_utils.plot_hk_stack(k, h, stack, save_file="c:\\temp\\synth_stack_0.8_0.2.png", show=False)
+
+    max_loc = np.unravel_index(np.argmax(stack), stack.shape)
+    h_max = h[max_loc[0], 0]
+    k_max = k[0, max_loc[1]]
+    print("Numerical solution (H, k) = ({:3g}, {:3g})".format(h_max, k_max))
 
 if __name__ == "__main__":
     main()
