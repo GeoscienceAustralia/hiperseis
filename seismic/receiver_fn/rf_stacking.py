Found and fix problem with ray param computation - extra sin_i factor since 'slowness' from rf is actually a ray parameter with the sin_i factor already implicit. Generalized H-k stacking code to infer V_p when possible.

seismic/receiver_fn/rf_stacking.py
@@ -12,35 +12,45 @@ import seismic.receiver_fn.rf_util as rf_util
 logging.basicConfig()
 
 
-def compute_hk_stack(db_station, cha, h_range=np.linspace(20.0, 70.0, 251), k_range=np.linspace(1.4, 2.0, 301),
-                     V_p=6.4, root_order=1, include_t3=True):
+def compute_hk_stack(db_station, cha, V_p=None, h_range=np.linspace(20.0, 70.0, 251), k_range=np.linspace(1.4, 2.0, 301),
+                     root_order=1, include_t3=True):
     """This function subject to further validation - documentation deferred.
 
-    :param db_station: [description]
-    :type db_station: [type]
-    :param cha: [description]
-    :type cha: [type]
-    :param h_range: [description], defaults to np.linspace(20.0, 70.0, 251)
-    :type h_range: [type], optional
-    :param k_range: [description], defaults to np.linspace(1.4, 2.0, 301)
-    :type k_range: [type], optional
-    :param V_p: [description], defaults to 6.4
-    :type V_p: float, optional
-    :param root_order: [description], defaults to 1
-    :type root_order: int, optional
-    :param include_t3: [description], defaults to True
-    :type include_t3: bool, optional
-    :return: [description]
-    :rtype: [type]
+    It is *critical* for the correctness of the output of this function that the V_p passed in is the same as the
+    V_p of the shallowest layer of the velocity model (from tau-py model) that was used to compute the arrival
+    inclination of each trace. Leave as None to have this function infer V_p from the trace metadata.
+
     """
+    log = logging.getLogger(__name__)
+    cha_data = db_station[cha]
+
+    infer_Vp = (V_p is None)
+    if infer_Vp:
+        # Determine the internal V_p consistent with the trace ray parameters and inclinations.
+        V_p_values = []
+        for tr in cha_data:
+            p = tr.stats.slowness/rf_util.KM_PER_DEG
+            incl_deg = tr.stats.inclination
+            incl_rad = np.deg2rad(incl_deg)
+            V_p_value = np.sin(incl_rad)/p
+            V_p_values.append(V_p_value)
+        # end for
+        V_p_values = np.array(V_p_values)
+        if not np.allclose(V_p_values, V_p_values, rtol=1e-3, atol=1e-4):
+            log.error("Inconsistent V_p values inferred from traces, H-k stacking results may be unreliable!")
+        # end if
+        V_p = np.mean(V_p_values)
+    # end if
+
     # Pre-compute grid quantities
     k_grid, h_grid = np.meshgrid(k_range, h_range)
     hk_stack = np.zeros_like(k_grid)
     H_on_V_p = h_grid/V_p
     k2 = k_grid*k_grid
 
+    # Whether to use RF slowness as the ray parameter (after unit conversion) and as source of V_p,
+    # or else use specific V_p given by user.
     stream_stack = []
-    cha_data = db_station[cha]
     # Loop over traces, compute times, and stack interpolated values at those times
     channel_id = None
     for tr in cha_data:
@@ -50,12 +60,19 @@ def compute_hk_stack(db_station, cha, h_range=np.linspace(20.0, 70.0, 251), k_ra
             assert tr.stats.channel == channel_id, \
                 "Stacking mismatching channel data: expected {}, found {}".format(channel_id, tr.stats.channel)
         # end if
-        incl = tr.stats.inclination
-        incl_rad = incl*np.pi/180.0
-        sin_i = np.sin(incl_rad)
-        slowness_sec_per_km = tr.stats.slowness/rf_util.KM_PER_DEG
-        # p is the ray parameter
-        p = sin_i*slowness_sec_per_km
+        # p is the ray parameter in seconds-per-km.
+        if infer_Vp:
+            # The "slowness" from rf.Trace.stats is actually the ray parameter, computed using the tau-py model
+            # that is also used to compute the inclination, then converted to seconds-per-degree. Therefore the
+            # "slowness" from rf.Trace.stats already contains the sin_i factor.
+            p = tr.stats.slowness/rf_util.KM_PER_DEG
+        else:
+            # User-provided velocity. It should be the same as that used during computation of tr.stats.inclination.
+            incl_deg = tr.stats.inclination
+            incl_rad = np.deg2rad(incl_deg)
+            sin_i = np.sin(incl_rad)
+            p = sin_i/V_p
+        # end if
         p2_Vp2 = p*p*V_p*V_p
         term1 = H_on_V_p*np.sqrt(k2 - p2_Vp2)
         term2 = H_on_V_p*np.sqrt(1 - p2_Vp2)

seismic/receiver_fn/validate_hk_stacking.py
@@ -22,7 +22,7 @@ logging.basicConfig()
 
 
 def main():
-    H = 50  # km
+    H = 50.0  # km.
 
     V_p = 6.4  # km/s, top (crust) layer
     V_s = 4.0  # km/s, top (crust) layer
@@ -46,42 +46,43 @@ def main():
     plt.title("Relationship between incident angle and teleseismic distance\n(source depth = {:2g} km)".format(
         source_depth_km))
     plt.grid("#80808080", linestyle=":")
-    plt.savefig("c:\\temp\\inclination_dist.png", dpi=300)
+    plt.savefig("inclination_dist.png", dpi=300)
 
     # Create interpolator to convert inclination to teleseismic distance
     interp_dist = interp1d(inc, ts_distance, bounds_error=False, fill_value=(np.max(ts_distance), np.min(ts_distance)))
 
     # Loop over valid range of inclinations and generate synthetic RFs
     inclinations = np.linspace(np.min(inc), np.max(inc), 10)
+    # inclinations = np.array([20.0]*10)
     distances = interp_dist(inclinations)
     final_sample_rate_hz = 10.0
     stream = synthesize_rf_dataset(H, V_p, V_s, inclinations, distances, final_sample_rate_hz, log)
-    stream.write("c:\\temp\\synth_rf_data.h5", format='h5')
-    _ = rf_plot_utils.plot_rf_stack(stream, time_window=(-10, 30), save_file="c:\\temp\\synth_rf.png", dpi=300)
+    stream.write("synth_rf_data.h5", format='h5')
+    _ = rf_plot_utils.plot_rf_stack(stream, time_window=(-10, 30), save_file="synth_rf.png", dpi=300)
 
     # Run H-k stacking on synthetic data
     station_db = {'HHR': stream}
 
     k, h, hk = rf_stacking.compute_hk_stack(station_db, 'HHR',
-                                            h_range=np.linspace(40.0, 60.0, 101),
-                                            k_range=np.linspace(1.5, 1.8, 151),
+                                            h_range=np.linspace(40.0, 60.0, 201),
+                                            k_range=np.linspace(1.5, 1.8, 301),
                                             include_t3=False)
     rf_plot_utils.plot_hk_stack(k, h, hk[0], title="Synthetic Ps component",
-                                save_file="c:\\temp\\Ps.png", show=False)
+                                save_file="Ps.png", show=False)
     rf_plot_utils.plot_hk_stack(k, h, hk[1], title="Synthetic Ppps component",
-                                save_file="c:\\temp\\Ppps.png", show=False)
+                                save_file="Ppps.png", show=False)
     w = (0.5, 0.5)
     w_str = "w={:2g},{:2g}".format(*w)
     stack = rf_stacking.compute_weighted_stack(hk, weighting=w)
     rf_plot_utils.plot_hk_stack(k, h, stack, num=len(stream), title="Synthetic H-k stack ({})".format(w_str),
-                                save_file="c:\\temp\\synth_stack_{}.png".format(w_str), show=False)
+                                save_file="synth_stack_{}.png".format(w_str), show=False)
     # stack = rf_stacking.compute_weighted_stack(hk, weighting=(0.8, 0.2))
     # rf_plot_utils.plot_hk_stack(k, h, stack, save_file="c:\\temp\\synth_stack_0.8_0.2.png", show=False)
 
     max_loc = np.unravel_index(np.argmax(stack), stack.shape)
     h_max = h[max_loc[0], 0]
     k_max = k[0, max_loc[1]]
-    print("Numerical solution (H, k) = ({:3g}, {:3g})".format(h_max, k_max))
+    print("Numerical solution (H, k) = ({:4g}, {:3g})".format(h_max, k_max))
 
 
 if __name__ == "__main__":