e3e372494ddebaa9f8bbf00aa24f3d204f35d5d9
Andrew Medlin <Andrew.Medlin@ga.gov.au>
2019-09-13 12:14:42 +1000

    Got plot selection callbacks and blitting working cleanly.

--- seismic/receiver_fn/rf_handpick_tool.py
+++ seismic/receiver_fn/rf_handpick_tool.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
 import logging
+import tkinter as tk
+from tkinter import filedialog
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 from matplotlib.widgets import RectangleSelector
 import matplotlib
-
-import tkinter as tk
-from tkinter import filedialog
 
 import rf
 
 import seismic.receiver_fn.rf_util as rf_util
 import seismic.receiver_fn.rf_plot_utils as rf_plot_utils
 
+# pylint: disable=invalid-name, logging-format-interpolation
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
@@ -39,16 +38,62 @@ stations = ['BS24']
 rf_type = data_all[0].stats.rotation
 
 def on_select(e_down, e_up, select_mask):
-    print("on_select ({}, {}) to ({}, {})".format(e_down.xdata, e_down.ydata, e_up.xdata, e_up.ydata))
+    """Event handler for RectangleSelector
+
+    :param e_down: [description]
+    :type e_down: [type]
+    :param e_up: [description]
+    :type e_up: [type]
+    :param select_mask: [description]
+    :type select_mask: [type]
+    """
+    log.debug("on_select ({}, {}) to ({}, {})".format(e_down.xdata, e_down.ydata, e_up.xdata, e_up.ydata))
     min_y = np.round(np.min([e_down.ydata, e_up.ydata])).astype(int)
     max_y = np.round(np.max([e_down.ydata, e_up.ydata])).astype(int)
     toggle_range = (max(0, min_y - 1), min(len(select_mask), max_y))
     select_mask[toggle_range[0]:toggle_range[1]] = ~select_mask[toggle_range[0]:toggle_range[1]]
-    print("Selection:")
+
+    # Echo selection
+    log.info("Selection:")
     if np.any(select_mask):
-        print(np.nonzero(select_mask)[0].tolist())
+        log.info(np.nonzero(select_mask)[0].tolist())
     else:
-        print("none")
+        log.info("none")
+    # end if
+
+# end func
+
+def on_release(event, select_mask, background, rect_selector):
+    """Event handler when mouse button released.
+
+    :param event: [description]
+    :type event: [type]
+    :param select_mask: [description]
+    :type select_mask: [type]
+    :param background: [description]
+    :type background: [type]
+    :param rect_selector: [description]
+    :type rect_selector: [type]
+    """
+
+    # Display selection
+    event.canvas.restore_region(background)
+    lines = [c for c in event.inaxes.get_children() if isinstance(c, matplotlib.lines.Line2D)]
+    for i, selected in enumerate(select_mask):
+        line = lines[i]
+        if selected:
+            line.set_color('red')
+            event.inaxes.draw_artist(line)
+        else:
+            line.set_color('black')
+        # end if
+    # end for
+    event.canvas.blit(event.inaxes.bbox)
+    # Make sure rect_selector widget now updates it's background to the new colors
+    rect_selector.update_background(event)
+
+# end func
+
 
 for st in stations:
     station_db = data_dict[st]
@@ -69,11 +114,19 @@ for st in stations:
     fig.set_size_inches(8, 9)
     fig.suptitle("Channel {}".format(rf_stream[0].stats.channel))
     ax0 = fig.axes[0]
+    # Make sure we draw once first before capturing blit background
+    fig.canvas.draw()
+    # Disallow resizing to avoid having to handle blitting with resized window.
+    win = fig.canvas.window()
+    win.setFixedSize(win.size())
+    blit_background = fig.canvas.copy_from_bbox(ax0.bbox)
 
     mask = np.array([False]*len(rf_stream))
-    rect_select = RectangleSelector(ax0, lambda e0, e1: on_select(e0, e1, mask), useblit=True)
-    # cid = fig.canvas.mpl_connect('pick_event', onpick)
+    rect_select = RectangleSelector(ax0, lambda e0, e1: on_select(e0, e1, mask), useblit=True,
+                                    rectprops=dict(fill=False, edgecolor='red'))
+    cid = fig.canvas.mpl_connect('button_release_event', lambda e: on_release(e, mask, blit_background, rect_select))
     plt.show()
 
-    # fig.canvas.mpl_disconnect(cid)
+    fig.canvas.mpl_disconnect(cid)
+    rect_select = None
 # end for
