- modified/fixed pulse generation to only produce causal RF estimates - cleaned up redundant code - completed initial unit testing - reran iterative deconv notebooks with updated algorithm - deleted redundant notebook file

seismic/receiver_fn/rf_deconvolution.py
@@ -66,27 +66,27 @@ def gauss_filter(x, gwidth_factor, dt):
     return x_filt
 
 
-def phase_shift(x, time_shift, dt):
-    """
-    Phase shifts a signal using frequency domain technique for smooth arbitrary shifting.
-    """
-    n = len(x)
-    two_pi = 2 * np.pi
+# def phase_shift(x, time_shift, dt):
+#     """
+#     Phase shifts a signal using frequency domain technique for smooth arbitrary shifting.
+#     """
+#     n = len(x)
+#     two_pi = 2 * np.pi
 
-    fft_x = np.fft.rfft(x)  # complex array
-    n2 = len(fft_x)
+#     fft_x = np.fft.rfft(x)  # complex array
+#     n2 = len(fft_x)
 
-    df = 1 / (float(n) * dt)
-    d_omega = two_pi * df
+#     df = 1 / (float(n) * dt)
+#     d_omega = two_pi * df
 
-    omega = np.arange(n2) * d_omega
-    # The phase shift is omega (angular velocity) * delta_t (the time shift).
-    spectral_shift = np.exp(omega*time_shift*1j)
-    fft_x = fft_x * spectral_shift
+#     omega = np.arange(n2) * d_omega
+#     # The phase shift is omega (angular velocity) * delta_t (the time shift).
+#     spectral_shift = np.exp(omega*time_shift*1j)
+#     fft_x = fft_x * spectral_shift
 
-    x_shifted = np.fft.irfft(fft_x, n)  # real_array
+#     x_shifted = np.fft.irfft(fft_x, n)  # real_array
 
-    return x_shifted
+#     return x_shifted
 
 
 def build_decon(amps, shifts, n, gwidth, dt):
@@ -102,15 +102,15 @@ def build_decon(amps, shifts, n, gwidth, dt):
     return p_filt, p
 
 
-def _convolve(x, y):
+def _convolve(x, y, leadin):
     """
     Helper function for convolving approximate receiver function with source signal
     to get estimated observation.
     """
     n = len(x)
     assert len(y) == n
-    z = scipy.signal.convolve(x, y)
-    z_sync = z[:n]
+    z = scipy.signal.convolve(x, y, mode='full')
+    z_sync = z[leadin:leadin + n]
     return z_sync
 
 
@@ -202,20 +202,21 @@ def iter_deconv_pulsetrain(denominator, numerator, max_pulses, tol=1.0e-3, gwidt
     max_causal_idx = npts - non_causal_leadin
     assert max_causal_idx > 0
     if only_positive:
-        shifts.append(np.argmax(xc[:max_causal_idx]))
+        shift_index = np.argmax(xc[:max_causal_idx])
     else:
         xc_abs = np.abs(xc)
-        shifts.append(np.argmax(xc_abs[:max_causal_idx]))
+        shift_index = np.argmax(xc_abs[:max_causal_idx])
     # end if
-    amps.append(xc[shifts[-1]])
+    shifts.append(shift_index + non_causal_leadin)
+    amps.append(xc[shift_index])
 
-    nshifts = 0
+    num_pulses = 1
 
     # compute the predicted deconvolution result
     p, pulses = build_decon(amps, shifts, npts, gwidth, dt)
 
     # convolve the prediction with the denominator signal
-    f_hat_predicted = _convolve(p, g)
+    f_hat_predicted = _convolve(p, g, non_causal_leadin)
 
     # compute the residual (initial error is 1.0)
     resid, sumsq_ip1 = get_residual(f_hat, f_hat_predicted)
@@ -225,34 +226,35 @@ def iter_deconv_pulsetrain(denominator, numerator, max_pulses, tol=1.0e-3, gwidt
     d_error = 100 * (sumsq_i - sumsq_ip1)
 
     log.info('%11s %s', 'Iteration', '  Spike amplitude  Spike delay   Misfit   Improvement')
-    log.info('%10d  %16.9e  %10.3f   %7.2f%%   %9.4f%%', nshifts, dt * amps[-1], (shifts[-1] - 1) * dt,
+    log.info('%10d  %16.9e  %10.3f   %7.2f%%   %9.4f%%', num_pulses, dt * amps[-1], (shifts[-1] - 1) * dt,
              100 * sumsq_ip1, d_error)
 
     # *************************************************************************
-    while (np.abs(d_error) > tol) and (nshifts < max_pulses):
+    while (np.abs(d_error) > tol) and (num_pulses < max_pulses):
 
-        nshifts = nshifts + 1
+        num_pulses += 1
         sumsq_i = sumsq_ip1
 
         xc = _xcorrelate(resid, g_hat)
 
         if only_positive:
-            shifts.append(np.argmax(xc[:max_causal_idx]))
+            shift_index = np.argmax(xc[:max_causal_idx])
         else:
             xc_abs = np.abs(xc)
-            shifts.append(np.argmax(xc_abs[:max_causal_idx]))
+            shift_index = np.argmax(xc_abs[:max_causal_idx])
         # end if
-        amps.append(xc[shifts[-1]])
+        shifts.append(shift_index + non_causal_leadin)
+        amps.append(xc[shift_index])
 
         p, pulses = build_decon(amps, shifts, npts, gwidth, dt)
 
-        f_hat_predicted = _convolve(p, g)
+        f_hat_predicted = _convolve(p, g, non_causal_leadin)
 
         resid, sumsq_ip1 = get_residual(f_hat, f_hat_predicted)
         sumsq_ip1 = sumsq_ip1 / power
         d_error = 100 * (sumsq_i - sumsq_ip1)
 
-        log.info('%10d  %16.9e  %10.3f   %7.2f%%   %9.4f%%', nshifts, dt * amps[-1], shifts[-1] * dt,
+        log.info('%10d  %16.9e  %10.3f   %7.2f%%   %9.4f%%', num_pulses, dt * amps[-1], shifts[-1] * dt,
                  100 * sumsq_ip1, d_error)
     # end while
 
@@ -260,20 +262,16 @@ def iter_deconv_pulsetrain(denominator, numerator, max_pulses, tol=1.0e-3, gwidt
 
     log.info('Last Error Change = %9.4f%%', d_error)
 
-    # if the last change made no difference, drop it
+    # Compute final fit
     fit = 100 - 100 * sumsq_ip1
-    if d_error <= tol:
-        nshifts = nshifts - 1
-        fit = 100 - 100 * sumsq_i
-        log.info('Hit the min improvement tolerance - halting.')
+
+    if (num_pulses >= max_pulses) and (np.abs(d_error) > tol):
+        log.warning('Hit the max number of pulses - not halting due to convergence.')
     # end if
 
-    if nshifts >= max_pulses:
-        log.warning('Hit the max number of pulses - halting.')
-    # end if
-
-    log.info('Number of pulses in final result: %d', nshifts)
-    log.info('The final deconvolution reproduces %6.1f%% of the signal.', fit)
+    num_unique_pulses = len(set(shifts))
+    log.info('Number of pulses in final result: %d', num_unique_pulses)
+    log.info('The final deconvolution reproduces %5.1f%% of the signal.', fit)
 
     # *************************************************************************
 
@@ -281,14 +279,11 @@ def iter_deconv_pulsetrain(denominator, numerator, max_pulses, tol=1.0e-3, gwidt
     p, pulses = build_decon(amps, shifts, npts, gwidth, dt)
 
     response_trace = denominator.copy()  # clone input trace
-    f_hat_predicted = _convolve(p, g)
+    f_hat_predicted = _convolve(p, g, non_causal_leadin)
     response_trace.data = f_hat_predicted
 
-    # Shift RF estimate to synchronize with time line of inputs. If this causes RF to wrap around
-    # in the time domain, try increasing the time extent of the input data.
-    p_shifted = phase_shift(p, -time_shift, dt)
     rf_trace = numerator.copy()  # clone input trace
-    rf_trace.data = p_shifted
+    rf_trace.data = p
 
     return rf_trace, pulses, f_hat, response_trace, fit
 

seismic/receiver_fn/rf_synthetic.py
@@ -36,15 +36,16 @@ def generate_synth_rf(arrival_times, arrival_amplitudes, fs_hz=100.0, window_sec
     arrivals_index = np.round((np.array(arrival_times) - times[0])*fs_hz).astype(int)
 
     # Generate kernel of delta functions at specified arrival times
-    kern = np.zeros_like(times)
-    kern[arrivals_index] = np.array(arrival_amplitudes)  # pylint: disable=unsupported-assignment-operation
+    kernel = np.zeros_like(times)
+    kernel[arrivals_index] = np.array(arrival_amplitudes)  # pylint: disable=unsupported-assignment-operation
 
     # Filter to pass low frequencies
-    waveform = signal.butter(4, f_cutoff_hz/fs_hz)
-    signal_filt = signal.filtfilt(waveform[0], waveform[1], kern)
-
-    # Normalize signal so max positive amplitude is 1.
-    signal_filt = signal_filt/np.max(signal_filt)
+    if f_cutoff_hz:
+        waveform = signal.butter(4, f_cutoff_hz/fs_hz)
+        signal_filt = signal.filtfilt(waveform[0], waveform[1], kernel)
+    else:
+        signal_filt = kernel
+    # end if
 
     return times, signal_filt
 # end func
@@ -126,7 +127,7 @@ def synthesize_rf_dataset(H, V_p, V_s, inclinations, distances, ds, log=None, in
 
     stream = rf.RFStream(traces)
 
-    return stream
+    return stream, arrivals
 # end func
 
 

tests/seismic/receiver_fn/test_iterative_deconv.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
-import os
-import pytest
-
 import numpy as np
 from scipy import signal
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from seismic.receiver_fn.rf_synthetic import synthesize_rf_dataset
 from seismic.receiver_fn.rf_deconvolution import iter_deconv_pulsetrain
@@ -14,10 +11,9 @@ from seismic.receiver_fn.rf_deconvolution import iter_deconv_pulsetrain
 # pylint: disable=invalid-name, missing-docstring
 
 
-def test_ammon_iter_deconv(tmp_path):
-    assert True
+def test_ammon_iter_deconv():
     # Synthesize known RF as a series of delta functions and verify that iterative
-    # deconvolution recovers original RF.
+    # deconvolution recovers original RF exactly, to within numerical tolerance.
     inclinations = np.array([20.0])
     distances = np.array([60.0])
     amplitudes = [1, 0.4, 0.2]
@@ -25,17 +21,22 @@ def test_ammon_iter_deconv(tmp_path):
     V_p = 6.4  # km/s
     V_s = 3.8  # km/s
     F_s = 10.0  # Hz
-    rf_radial = synthesize_rf_dataset(H, V_p, V_s, inclinations, distances, F_s, amplitudes=amplitudes)
+    # This function call generates the container for the synthetic RF, then we overwrite it with pure delta functions
+    rf_radial, arrivals = synthesize_rf_dataset(H, V_p, V_s, inclinations, distances, F_s, amplitudes=amplitudes)
     rf_radial = rf_radial[0]
-    rf_radial.data /= F_s
-    # Generate vertical seismic trace
+    # Patch over the filtered RF data with pure delta functions for testing purposes
     times = rf_radial.times() - (rf_radial.stats.onset - rf_radial.stats.starttime)
+    rf_radial.data = np.zeros_like(rf_radial.data)
+    rf_radial.data[np.round((np.array(arrivals) - times[0])*F_s).astype(int)] = amplitudes
+    rf_radial.data /= F_s
+    # Generate synthetic vertical seismic trace as a filtered decaynig cosine. The exact form here shouldn't matter,
+    # but better that it be plausible.
     g = 1000*np.exp(-times/4)*np.cos(2*np.pi*1*times)
     g[times < 0] = 0
-    # Apply weak filter to g to smooth onset
+    # Apply weak filter to g to smooth onset, cutting off above 2.0 Hz
     bw_filt = signal.butter(2, 2.0/F_s)
     g = signal.filtfilt(bw_filt[0], bw_filt[1], g)
-    # Generate radial seismic trace
+    # Generate radial seismic trace corresponding to source signal g convolved with the RF.
     f = signal.convolve(rf_radial.data, g, mode='full')
     n_leadin = len(times[times < 0])
     f = f[n_leadin:n_leadin+len(times)]
@@ -47,8 +48,16 @@ def test_ammon_iter_deconv(tmp_path):
     numerator.data = f
     max_pulses = 100
     rf_trace, pulses, f_hat, response_trace, fit = iter_deconv_pulsetrain(denominator, numerator, max_pulses)
-    pass
+    # Check that predicted response is very close to filtered observation
+    assert np.allclose(f_hat, response_trace, rtol=2.0e-3, atol=1.0e-3)
+    # Check that we have the exact number of delta function pulses that we synthesized
+    assert np.sum(pulses > 0) == 3
+    # Check that the fit is perfect, which it should be when RF consists of perfect delta functions.
+    assert np.isclose(100.0, fit)
+    # Check that amplitude of the computed RF matches the synthesized RF.
+    assert np.isclose(np.sum(amplitudes)/F_s, np.sum(rf_trace), rtol=1.0e-3)
+# end func
+
 
 if __name__ == "__main__":
-    import tempfile
-    test_ammon_iter_deconv(tempfile.mkdtemp())
+    test_ammon_iter_deconv()