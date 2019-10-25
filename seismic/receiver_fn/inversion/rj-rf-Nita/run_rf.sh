ae94d3a47fe9437553f3d2045e5ac8f07c862071
Andrew Medlin <Andrew.Medlin@ga.gov.au>
2019-10-25 12:36:12 +1100

    Generalized pbs job submit script for handling batch submission

--- seismic/receiver_fn/inversion/rj-rf-Nita/run_rf.sh
+++ seismic/receiver_fn/inversion/rj-rf-Nita/run_rf.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+# To run:
+#   qsub -M my_email_address -N jobname -v INFILE=input_filename,OUT=output_folder ./run_rf.sh
 #PBS -P vy72
-#PBS -N rfinv
 #PBS -q normalbw
-#PBS -l walltime=10:00:00,mem=160GB,ncpus=896,jobfs=1GB
+#PBS -l walltime=24:00:00,mem=160GB,ncpus=56,jobfs=500MB
 #PBS -l other=hyperthread
 #PBS -l wd
 #PBS -j oe
-#PBS -M andrew.medlin@ga.gov.au
 #PBS -m bae
 
 module load openmpi/1.10.2-mt
-mpirun ./run
+echo $INFILE
+echo $OUT
+mpirun ./run $INFILE $OUT
 
