4a348e5ac9334fce9e50edce1dda449098172425
Andrew Medlin <Andrew.Medlin@ga.gov.au>
2019-11-01 08:44:40 +1100

    Wastefully increase memory requirement to stop last minute, single-node out of memory on mpi_reduce

--- seismic/receiver_fn/inversion/rj-rf-Nita/run_rf.sh
+++ seismic/receiver_fn/inversion/rj-rf-Nita/run_rf.sh
@@ -3,7 +3,7 @@
 #   qsub -M my_email_address -N jobname -v INFILE=input_filename,OUT=output_folder ./run_rf.sh
 #PBS -P vy72
 #PBS -q normalbw
-#PBS -l walltime=24:00:00,mem=12GB,ncpus=28,jobfs=200MB
+#PBS -l walltime=24:00:00,mem=16GB,ncpus=28,jobfs=200MB
 #PBS -l other=hyperthread
 #PBS -l wd
 #PBS -j oe
