08415aa6f65a11175ebb0011b61c8c9af725636c
Andrew Medlin <Andrew.Medlin@ga.gov.au>
2019-11-22 13:15:04 +1100

    Set up deterministic seed with 25 layers case to run in order to compare results from Intel build vs gfortran build

--- seismic/receiver_fn/inversion/rj-rf-Nita/run_rf_avx2.sh
+++ seismic/receiver_fn/inversion/rj-rf-Nita/run_rf_avx2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # To run:
-#   qsub -M my_email_address -N jobname -v INFILE=input_filename,OUT=output_folder ./run_rf.sh
+#   qsub -M my_email_address -N jobname -v INFILE=input_filename,OUT=output_folder ./run_rf_avx2.sh
 #PBS -P vy72
 #PBS -q normalbw
 #PBS -l walltime=48:00:00,mem=16GB,ncpus=28,jobfs=200MB
