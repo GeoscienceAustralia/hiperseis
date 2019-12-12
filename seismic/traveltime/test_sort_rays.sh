2937c7067f82bd4d435ebd6013d7b2dda4205a9b
Fei Zhang <fei.zhang@ga.gov.au>
2019-12-12 05:02:51 +0000

    update the scripts

--- seismic/traveltime/test_sort_rays.sh
+++ seismic/traveltime/test_sort_rays.sh
@@ -1,12 +1,25 @@
 
-export PSTHOME=~/Githubz/hiperseis/
 
+# Before running this script, make sure you are in a python enviroment where ellipcorr,  seismic and dependency packages have been installed
+# typically a virtualenv through source ...venvname/bin/activate; or conda activate venv_name
+
+export PSTHOME=~/Githubz/hiperseis/
+export PYTHONPATH=$PSTHOME:$PYTHONPATH
 export ELLIPCORR=$PSTHOME/ellip-corr/
 
-mkdir $PSTHOME/tmpdir
+wkdir="$PSTHOME/tmpdir"
 
-cd $PSTHOME/tmpdir
+
+if [[ ! -e $wkdir ]]; then
+  mkdir $wkdir
+fi
+
+cd $wkdir
+
 
 python $PSTHOME/seismic/traveltime/sort_rays.py $PSTHOME/tests/testdata/100K_ensemble.p.txt  p_arrivals_sorted1x1.csv P $PSTHOME/seismic/traveltime/param1x1 $PSTHOME/seismic/traveltime/csv_columns.json
 
 python $PSTHOME/seismic/traveltime/sort_rays.py $PSTHOME/tests/testdata/100K_ensemble.s.txt  s_arrivals_sorted1x1.csv S $PSTHOME/seismic/traveltime/param1x1 $PSTHOME/seismic/traveltime/csv_columns.json
+
+
+ls -ltr $wkdir
