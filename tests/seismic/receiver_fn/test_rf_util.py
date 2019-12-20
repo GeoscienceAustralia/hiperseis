21d2d2d879eeb8090d4f226b353214ef81819b62
Andrew Medlin <Andrew.Medlin@ga.gov.au>
2019-12-20 11:07:08 +1100

    Added placeholder file for testing rf_util module

---
+++ tests/seismic/receiver_fn/test_rf_util.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Unit testing for RF utility functions
+"""
+
+import pytest
+
+def test_phase_weights():
+    assert True
+# end func
+
+
+if __name__ == "__main__":
+    pass
