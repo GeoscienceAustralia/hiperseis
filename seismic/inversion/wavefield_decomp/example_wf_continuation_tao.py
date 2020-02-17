Fixed computation of freq bins when N is even by switching to general fft functions. Added data attribute checks. Fixed sometimes inconsistent number of samples when trimming is subject to truncation error. Fixed pruning when number of samples per channel differ. Broadened parameter search range for global minimization cases to highlight difficulty in finding most plausible minimum (might not be global minimum).

seismic/inversion/wavefield_decomp/example_wf_continuation_tao.py
@@ -35,7 +35,7 @@ def example_1():
 # end func
 
 
-def example_2():
+def example_2(station):
     # Example 2: Computing energy across a parametric space of models.
 
     def plot_Esu_space(H, Vs, Esu, network, station, save=True, show=True):
@@ -78,7 +78,7 @@ def example_2():
         Esu[i, j] = energy
     # end for
 
-    plot_Esu_space(H, Vs, Esu, 'OA', 'BT23')
+    plot_Esu_space(H, Vs, Esu, 'OA', station)
 # end func
 
 
@@ -108,12 +108,12 @@ def example_3():
 def example_4():
     # Demonstrate syntactic usage of scipy global optimizers:
     k_initial = np.mean((k_min, k_max))
-    model_initial_poor = np.array([34, Vp_c/k_initial])
+    model_initial_poor = np.array([35, Vp_c/k_initial])
 
     Vp = [Vp_c]
     rho = [rho_c]
     fixed_args = (flux_comp, mantle_props, Vp, rho, FLUX_WINDOW)
-    H_min, H_max = (25.0, 45.0)
+    H_min, H_max = (25.0, 55.0)
     bounds = optimize.Bounds([H_min, Vp_c/k_max], [H_max, Vp_c/k_min])
 
     # - Basin hopping
@@ -155,9 +155,9 @@ def example_5():
     H_initial = [1.0, 35.0]  # sediment, crust
     Vs_initial = [0.8, 3.4]  # sediment, crust
     model_initial_sed = np.array(zip(H_initial, Vs_initial))
-    H_sed_min, H_sed_max = (0, 2.5)
+    H_sed_min, H_sed_max = (0, 3.5)
     Vs_sed_min, Vs_sed_max = (0.3, 2.5)
-    H_cru_min, H_cru_max = (25.0, 55.0)
+    H_cru_min, H_cru_max = (20.0, 60.0)
     Vs_cru_min, Vs_cru_max = (Vp_c/k_max, Vp_c/k_min)
     bounds = optimize.Bounds([H_sed_min, Vs_sed_min, H_cru_min, Vs_cru_min],
                              [H_sed_max, Vs_sed_max, H_cru_max, Vs_cru_max])
@@ -200,10 +200,12 @@ if __name__ == "__main__":
                 (np.max(np.abs(stream[2].data)) <= max_amplitude))
     # end func
 
+    target_station = 'BT23'
+    # target_station = 'CI23'
     logging.info("Loading input file...")
     src_file = (r"/g/data/ha3/am7399/shared/OA_RF_analysis/" +
                 r"OA_event_waveforms_for_rf_20170911T000036-20181128T230620_rev8.h5")
-    data_all = NetworkEventDataset(src_file, network='OA', station='BT23', location='0M')
+    data_all = NetworkEventDataset(src_file, network='OA', station=target_station, location='0M')
 
     # Time window of original data to use for processing. All traces must have at least this extent
     # about the onset time.
@@ -214,7 +216,7 @@ if __name__ == "__main__":
     CUT_WINDOW = (-5, 30)
 
     # -----------------------------------------------------------------------------
-    # Apply windowinf, filtering and QC to loaded dataset before passing to Tao's algorithm.
+    # Apply windowing, filtering and QC to loaded dataset before passing to Tao's algorithm.
     logging.info("Cleaning input data...")
 
 
@@ -253,7 +255,7 @@ if __name__ == "__main__":
             if ((stream[0].stats.npts != expected_pts) or
                 (stream[1].stats.npts != expected_pts) or
                 (stream[2].stats.npts != expected_pts)):
-                discard.append(sta, evid)
+                discard.append((sta, evid))
             # end if
         # end for
     # end for
@@ -265,7 +267,7 @@ if __name__ == "__main__":
 
     # -----------------------------------------------------------------------------
     # Pass cleaned up data set for test station to flux computer class.
-    data_OA = data_all.station('BT23')
+    data_OA = data_all.station(target_station)
     fs_processing = 10.0  # Hz
     logging.info("Ingesting source data streams...")
     flux_comp = WfContinuationSuFluxComputer(data_OA.values(), fs_processing, TIME_WINDOW, CUT_WINDOW)
@@ -284,7 +286,7 @@ if __name__ == "__main__":
 
     # -----------------------------------------------------------------------------
     # Example 2: Computing energy across a parametric space of models.
-    example_2()
+    example_2(target_station)
 
     # -----------------------------------------------------------------------------
     # Example 3: Using a global energy minimization solver to find solution.

seismic/inversion/wavefield_decomp/wavefield_continuation_tao.py
@@ -40,11 +40,23 @@ class WfContinuationSuFluxComputer:
             return
         # end if
 
-        # TODO: Check input streams are all in ZRT coordinates.
+        # Check input streams are all in ZRT coordinates.
+        assert np.all([''.join([tr.stats.channel[-1] for tr in st]).upper() == 'ZRT' for st in station_event_dataset])
+
+        # Check input streams all have same sampling rate
+        traces_fs = set(tr.stats.sampling_rate for st in station_event_dataset for tr in st)
+        assert len(traces_fs) == 1, 'Inconsistent sampling rates {}!'.format(traces_fs)
 
         self._time_window = time_window
         self._cut_window = cut_window
+        # Sampling rate and time step
         self._f_s = f_s
+        self._dt = 1.0 / f_s
+        # Number of sample points. Since the last point is considered the end of a closed interval,
+        # we +1 to the numer of points to ensure f_s == 1/dt.
+        self._npts = int(np.rint((time_window[1] - time_window[0])*f_s)) + 1
+        self._time_axis = np.linspace(*self._time_window, self._npts)
+        assert np.isclose(np.mean(np.diff(self._time_axis)), self._dt)
         self._station_eventdataset_to_v0(station_event_dataset)
         self._station_event_dataset_extract_p(station_event_dataset)
         self._precompute()
@@ -88,12 +100,10 @@ class WfContinuationSuFluxComputer:
                         stream[0].stats.onset + self._time_window[1])
         # end for
 
-        # TODO: Recheck all traces have same length
-
         # Cut central data segment and resample back to original length using sinc interpolation.
+        times = self._time_axis
         for stream in data:
             for tr in stream:
-                times = tr.times() - (tr.stats.onset - tr.stats.starttime)
                 tr_cut = tr.copy().trim(tr.stats.onset + self._cut_window[0], tr.stats.onset + self._cut_window[1])
                 tr_cut.detrend('linear')
                 tr_cut.taper(0.10)
@@ -105,7 +115,9 @@ class WfContinuationSuFluxComputer:
             # end for
         # end for
 
-        # TODO: Recheck again that all traces have same length
+        # Check that all traces have same length
+        trace_lengths = set(len(st[0]) for st in data)
+        assert len(trace_lengths) == 1, 'Inconsistent trace lengths {} after sinc interpolation!'.format(trace_lengths)
 
         # Pull data arrays out into matrix format
         self._v0 = np.array([[st.select(component='R')[0].data.tolist(),
@@ -126,10 +138,7 @@ class WfContinuationSuFluxComputer:
 
     def _precompute(self):
         # Computations that need only be done once per dataset.
-        self._dt = 1.0 / self._f_s
-        self._npts = self._v0.shape[2]  # Number of sample points
         self._nevts = self._v0.shape[0]  # Number of events
-        self._time_axis = np.linspace(*self._time_window, self._npts)
 
         # *NORMALIZE v0*
         # Reshape to facilitate max_vz normalization using numpy broadcast rules.
@@ -142,16 +151,10 @@ class WfContinuationSuFluxComputer:
         self._v0 = np.moveaxis(v0, -1, 0)
 
         # Transform v0 to the spectral domain using real FFT
-        fv0 = np.fft.rfft(self._v0, axis=-1)
+        self._fv0 = np.fft.fft(self._v0, axis=-1)
 
         # Compute discrete frequencies
-        w = 2 * np.pi * np.fft.rfftfreq(self._v0.shape[-1], self._dt)
-
-        # Extend w to full spectral domain.
-        self._w = np.hstack((w, -np.flipud(w[1:])))
-
-        # To extend fv0, we need to flip left-right and take complex conjugate.
-        self._fv0 = np.dstack((fv0, np.fliplr(np.conj(fv0[:, :, 1:]))))
+        self._w = 2 * np.pi * np.fft.fftfreq(self._npts, self._dt)
 
     # end if
 