54540087b79effde7ba05b13f49e1c6f29d76932
Fei Zhang <fei.zhang@ga.gov.au>
2020-05-01 14:41:19 +1000

    pbs job phase picker using parallel h5py virtual environ

---
+++ seismic/pick_harvester/pbs_pick2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Fei Zhang 2020-05-01
+
+# PBS directives
+#PBS -P vy72
+#PBS -N pick2_pbs_job
+#PBS -q normal
+#PBS -l walltime=2:00:00,mem=190GB,ncpus=48,jobfs=160GB
+#PBS -l storage=scratch/fxz547+gdata/ha3
+#PBS -l wd
+#PBS -j oe
+#PBS -M fei.zhang@ga.gov.au
+#PBS -m bae
+
+
+# this is tested working in NCI gadi system.
+module purge
+module load python3/3.7.4
+module loat hdf5/1.10.5p 
+module load openmpi/3.1.4
+#export GDAL_DATA=/apps/gdal/3.0.2/share/gdal/
+#export PYTHONPATH=/apps/gdal/3.0.2/lib64:/apps/gdal/3.0.2/lib64/python3.7/site-packages
+export PYTHONPATH=/home/547/fxz547/github/hiperseis/:$PYTHONPATH
+export LC_ALL=en_AU.UTF-8
+export LANG=en_AU.UTF-8
+
+
+# the virtual env  with parallel h5py together with many other python libs:
+# pip install ordered_set tqdm  click  ujson  psutil
+# pip install pykml  pyyaml  joblib pywavelets
+source /g/data/ha3/Passive/Software/VENV/para_h5py/bin/activate
+
+
+# files paths 
+ASDF_FILES="/g/data/ha3/GASeisDataArchive/DevSpace/test_asdf_files.txt"
+CATALOGD="/g/data/ha3/Passive/Events/Unified/"
+PICK_OUTPUT="/g/data/ha3/GASeisDataArchive/DevSpace/pick_workflow"
+
+# the cmdline 
+mpirun -np $PBS_NCPUS python3 /home/547/fxz547/github/hiperseis/seismic/pick_harvester/pick.py $ASDF_FILES $PICK_OUTPUT/catalog/ $PICK_OUTPUT > $PICK_OUTPUT/run_output_0501.txt
+# mpirun -np $PBS_NCPUS python3 /home/547/fxz547/github/hiperseis/seismic/pick_harvester/pick.py $ASDF_FILES $CATALOGD $PICK_OUTPUT > $PICK_OUTPUT/run_output.txt 2>&1
+
