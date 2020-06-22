b22971da8e653e17e3ac394b461c95556ae17f4a
Fei Zhang <fei.zhang@ga.gov.au>
2020-06-22 11:15:56 +1000

    updates correct import ub seismic/ml_classifier/data_harvester

--- seismic/ml_classifier/data_harvester/FederatedASDFExample.py
+++ seismic/ml_classifier/data_harvester/FederatedASDFExample.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[1]:
@@ -14,6 +13,7 @@ from obspy.signal.cross_correlation import xcorr
 import numpy as np
 import matplotlib
 import logging
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import scipy
@@ -24,7 +24,7 @@ from netCDF4 import Dataset
 from obspy.signal.detrend import simple, spline
 from obspy.signal.filter import bandpass
 
-from ASDFdatabase.FederatedASDFDataSet import FederatedASDFDataSet
+from seismic.ASDFdatabase.FederatedASDFDataSet import FederatedASDFDataSet
 
 from obspy import UTCDateTime, read_events, read_inventory
 from obspy.taup.taup_geo import calc_dist
@@ -33,30 +33,20 @@ from obspy.clients.fdsn import Client
 from obspy.taup import TauPyModel
 from obspy.signal.trigger import trigger_onset, z_detect, classic_sta_lta, recursive_sta_lta, ar_pick
 from obspy.signal.rotate import rotate_ne_rt
-from obspy.core.event import Pick, CreationInfo, WaveformStreamID, ResourceIdentifier, Arrival, Event,Origin, Arrival, OriginQuality, Magnitude, Comment
+from obspy.core.event import Pick, CreationInfo, WaveformStreamID, ResourceIdentifier, Arrival, Event, Origin, Arrival, \
+    OriginQuality, Magnitude, Comment
 
 
-# In[3]:
-
-
-fds = FederatedASDFDataSet('/g/data/ha3/Passive/SHARED_DATA/Index/asdf_files.txt', variant='db', use_json_db=True,
-                           logger=None)
-
-
-# In[5]:
-
+fds = FederatedASDFDataSet('/g/data/ha3/Passive/SHARED_DATA/Index/asdf_files.txt', logger=None)
 
 stations = fds.get_stations('2009-05-17T00:00:00', '2009-05-18T00:00:00', station='QLP')
 
 print(stations)
 
-
-# In[7]:
-
-
-s = fds.get_waveforms('AU', 'QLP', '', 'BHE', 
-                      '2011-03-15T00:00:00', '2011-03-16T00:00:00',
-                      automerge=True, trace_count_threshold=10)
+s = fds.get_waveforms('AU', 'QLP', '', 'BHE',
+                      '2011-03-15T00:00:00', '2011-03-16T00:00:00', trace_count_threshold=10)
 print(s)
-if(len(s)):s.plot(outfile='/home/118/rlt118/meme.png')
 
+if len(s)>0:
+    print("Plotting traces")
+    s.plot(outfile='stream_plot.png')
--- seismic/ml_classifier/data_harvester/autopick_collector.py
+++ seismic/ml_classifier/data_harvester/autopick_collector.py
@@ -1,33 +1,31 @@
-#script to collect waveforms corresponding to the automated S-wave picks and save them with an index to a directory
+# script to collect waveforms corresponding to the automated S-wave picks and save them with an index to a directory
 import sys
-sys.path.append('/g/data1a/ha3/rakib/seismic/pst/passive-seismic')
-from ASDFdatabase.FederatedASDFDataSet import FederatedASDFDataSet
-from autopicks_approx import pickLoader
+# sys.path.append('/g/data1a/ha3/rakib/seismic/pst/passive-seismic')
+from seismic.ASDFdatabase.FederatedASDFDataSet import FederatedASDFDataSet
+from seismic.ml_classifier.data_harvester.autopicks_approx import pickLoader
 from obspy.core import UTCDateTime
 
-fds=FederatedASDFDataSet('/g/data/ha3/Passive/SHARED_DATA/Index/asdf_files.txt', variant='db', use_json_db=True, logger=None)
-
+fds = FederatedASDFDataSet('/g/data/ha3/Passive/SHARED_DATA/Index/asdf_files.txt', variant='db', use_json_db=True,
+                           logger=None)
 
 import numpy as np
 
-saveDir='/g/data/ha3/rlt118/neural-datasets/autopicks/'
-with open(saveDir+'ensemble.s.txt','r') as f:
-    picks=f.readlines()
-    pickCtr=len(picks)-1
-    lastpick=picks[-2].split()
-    mintime=UTCDateTime(float(lastpick[9]))
+saveDir = '/g/data/ha3/rlt118/neural-datasets/autopicks/'
+with open(saveDir + 'ensemble.s.txt', 'r') as f:
+    picks = f.readlines()
+    pickCtr = len(picks) - 1
+    lastpick = picks[-2].split()
+    mintime = UTCDateTime(float(lastpick[9]))
 
-pickIt=pickLoader(fds,mintime=mintime)
-    
-    
-with open(saveDir+'ensemble.s.txt','w') as f:
-    #write the existing picks
+pickIt = pickLoader(fds, mintime=mintime)
+
+with open(saveDir + 'ensemble.s.txt', 'w') as f:
+    # write the existing picks
     f.writelines([item for item in picks[:-1]])
-    
-    for pick,data in pickIt:
-        pickCtr+=1
-        f.write(' '.join(pick)+'\n')
-        np.save(saveDir+str(pickCtr)+'.npy',data)
-        
 
-#the waveform corresponding to the nth line of the pick file is saved as '${n}.npy'
+    for pick, data in pickIt:
+        pickCtr += 1
+        f.write(' '.join(pick) + '\n')
+        np.save(saveDir + str(pickCtr) + '.npy', data)
+
+# the waveform corresponding to the nth line of the pick file is saved as '${n}.npy'
