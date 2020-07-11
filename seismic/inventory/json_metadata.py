6ade6ba9e1be8cb8f96f1cc1bbe2b4daf529a61c
Fei Zhang <feizhang01@gmail.com>
2020-07-11 15:20:58 +1000

    update json_metadata.py

--- seismic/inventory/json_metadata.py
+++ seismic/inventory/json_metadata.py
@@ -1,27 +1,35 @@
+"""
+Use Json file to store station extra metadata
+
+Fei Zhang
+2020-07-11
+
+# TODO: Design a proper json metadata structure to encaptulate all the metadata items. store in the extra tag of stationxml file
+"""
 import json
 
-person = '{"name": "Bob", "languages": ["English", "Fench"]}'
-person_dict = json.loads(person)
+def read_orientation_correction(in_json_file):
+  """ Read in a json file
+  :param in_json_file:
+  :return:
+  """
+  # load a file
 
-# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
-print( person_dict)
+  with open(in_json_file) as f:
+    data = json.load(f)
 
-# Output: ['English', 'French']
-print(person_dict['languages'])
+  print(type(data))
 
-# load a file
-# json_file = "/Datasets/Orientation_Correction_json/7X_ori_error_estimates.json"
-json_file = "OA_ori_error_estimates.json" #"7X_ori_error_estimates.json"
-with open(json_file) as f:
-  data = json.load(f)
+  print(data.keys()) # dict_keys(['7X.MA01', '7X.MA11', '7X.MA12', '7X.MA13', '7X.MA14', '7X.MA21', '7X.MA22'.....)]
 
-print(type(data))
+  netsta = "OA.CF28" # '7X.MA01'
 
-print(data.keys()) # dict_keys(['7X.MA01', '7X.MA11', '7X.MA12', '7X.MA13', '7X.MA14', '7X.MA21', '7X.MA22'.....)]
+  print(data[netsta])
 
-netsta = "OA.CF28" # '7X.MA01'
+  print(data[netsta]['date_range'], type(data[netsta]['date_range'])) # ['2009-09-10T02:57:07.160000Z', '2010-06-05T05:35:00.623400Z'] <class 'list'>
+  print(data[netsta]['azimuth_correction'], type(data[netsta]['azimuth_correction'])) # 38.0 <class 'float'>
 
-print(data[netsta])
-
-print(data[netsta]['date_range'], type(data[netsta]['date_range'])) # ['2009-09-10T02:57:07.160000Z', '2010-06-05T05:35:00.623400Z'] <class 'list'>
-print(data[netsta]['azimuth_correction'], type(data[netsta]['azimuth_correction'])) # 38.0 <class 'float'>
+if __name__ == "__main__":
+  # json_file = "/Datasets/Orientation_Correction_json/7X_ori_error_estimates.json"
+  json_file = "OA_ori_error_estimates.json" #"7X_ori_error_estimates.json"
+  read_orientation_correction(json_file)
