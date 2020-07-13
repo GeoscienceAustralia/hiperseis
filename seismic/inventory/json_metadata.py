31349beda9b901de46b4e07f3abcf6eeef11c52c
Fei Zhang <feizhang01@gmail.com>
2020-07-13 16:21:56 +1000

    proposed a new metadata json file structure for gsp clock corr and orientation corr; and update python code to read it.

--- seismic/inventory/json_metadata.py
+++ seismic/inventory/json_metadata.py
@@ -2,7 +2,7 @@
 Use Json file to store station extra metadata
 
 Fei Zhang
-2020-07-11
+2020-07-08
 
 # TODO: Design a proper json metadata structure to encaptulate all the metadata items. store in the extra tag of stationxml file
 """
@@ -29,8 +29,31 @@ def read_orientation_correction(in_json_file):
     print(data[netsta]['date_range'], type(data[netsta]['date_range'])) # ['2009-09-10T02:57:07.160000Z', '2010-06-05T05:35:00.623400Z'] <class 'list'>
     print(data[netsta]['azimuth_correction'], type(data[netsta]['azimuth_correction'])) # 38.0 <class 'float'>
 
+def read_my_metajson(metajson_file):
+    """
+    Read in a new json file proposed to store metadata
+    :param metajson_file: a json file
+    :return:
+    """
+    with open(metajson_file) as jsonfile:
+        mdata = json.load(jsonfile)
+
+    print (mdata)
+
+    print(mdata.keys())
+
+    print(type(mdata['gps_clock_corrections']))
+
+    for corr in mdata['gps_clock_corrections']:
+        print (corr["date"], corr["seconds"] )
+
+    return mdata
+
+
 if __name__ == "__main__":
     # json_file = "/Datasets/Orientation_Correction_json/7X_ori_error_estimates.json"
-    json_file = "OA_ori_error_estimates.json" #"7X_ori_error_estimates.json"
-    read_orientation_correction(json_file)
+    # json_file = "OA_ori_error_estimates.json" #"7X_ori_error_estimates.json"
+    # read_orientation_correction(json_file)
 
+    my_meta_json = "extra_station_metadata.json"
+    read_my_metajson(my_meta_json)
