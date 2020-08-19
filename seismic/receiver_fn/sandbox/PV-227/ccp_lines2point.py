5b1e582fa1b3e4126d2debeff00fa22a98b16ed4
Brenainn Moushall <bm5478@gadi-login-06.gadi.nci.org.au>
2020-08-19 14:20:40 +1000

    Update ccp_lines2point
    - Return Sta,Lon,Lat,Depth (structured array)
    - Read all workbook sheets
    - Raise or warn when profile line is invalid
      or station coords can't be found

--- seismic/receiver_fn/sandbox/PV-227/ccp_lines2point.py
+++ seismic/receiver_fn/sandbox/PV-227/ccp_lines2point.py
@@ -11,19 +11,22 @@ import os
 
 import click
 import numpy as np
+import numpy.lib.recfunctions as nlr
 import pandas as pd
 import xlrd
 
 from seismic.ASDFdatabase.FederatedASDFDataSet import FederatedASDFDataSet
 from seismic.units_utils import KM_PER_DEG
 from seismic.receiver_fn.plot_ccp_batch import LEAD_INOUT_DIST_KM
+from seismic.receiver_fn.sandbox.conversion_helper import NETWORK_CODE_MAPPINGS, SPECIAL_CHARS
 
 
 @click.command()
 @click.option('--fds-file', type=click.Path(dir_okay=False, exists=True), required=True,
               help='Input file for FederatedASDFDataSet containing station coordinates')
+@click.option('--raise-errors', is_flag=True)
 @click.argument('infile', type=click.Path(dir_okay=False, exists=True), required=True)
-def main(infile, fds_file):
+def main(infile, fds_file, raise_errors=False):
     """
     Process Excel spreadsheet into point dataset based on line profiles.
 
@@ -50,56 +53,70 @@ def main(infile, fds_file):
     :param fds_file: Index file used to instantiate FederatedASDFDataSet
     :return: None
     """
+    fds = FederatedASDFDataSet(fds_file)
+    sta_coords = fds.unique_coordinates
+    all_network_codes = {s.split('.')[0] for s in sta_coords.keys()}
+    vol_data_list = []
 
     with xlrd.open_workbook(infile) as wb:
         sheet = wb.sheet_by_index(0)
-        network = sheet.cell_value(0, 3)
-        lines_row = sheet.row_values(3)
-        lines = [line for line in lines_row if line]
-    # end with
-
-    df = pd.read_excel(infile, header=4)
-    df.drop(df.columns[0], axis=1, inplace=True)
-
-    fds = FederatedASDFDataSet(fds_file)
-    sta_coords = fds.unique_coordinates
-    vol_data_dict = {}
-    for i, line in enumerate(lines):
-        line = line.strip()
-        sta_start, sta_end = line.split(',')
-        sta_start = sta_start.strip()
-        sta_end = sta_end.strip()
-        start = '.'.join([network, sta_start])
-        end = '.'.join([network, sta_end])
-        start = np.array(sta_coords[start])
-        end = np.array(sta_coords[end])
-        assert np.any(end != start)
-        dirn = (end - start)
-        dirn = dirn / np.linalg.norm(dirn)
-        dist_col = df.iloc[:, 3*i + 1]
-        dist_col = pd.to_numeric(dist_col, errors='coerce').astype(float)
-        valid = dist_col.notna()
-        if not np.any(valid):
-            continue
-        dist = dist_col[valid].values - LEAD_INOUT_DIST_KM
-        depth = df.iloc[:, 3*i + 2][valid].values
-        stations = df.iloc[:, 3*i].values
-        stations = [s.strip().split('.')[0] for s in stations]
-        lonlat = start + np.outer(dist, dirn)/KM_PER_DEG
-        # Difficult to correct for differences in station elevation because
-        # FDS does not include it in station coords. Ignore for now.
-        vol_data = np.hstack((stations, lonlat, depth[:, np.newaxis]))
-        vol_data_dict[line] = vol_data
-    # end for
+        for i, sheet in enumerate(wb.sheets()):
+            network = sheet.cell_value(0, 3)
+            network = NETWORK_CODE_MAPPINGS.get(network, network)
+            lines_row = sheet.row_values(3)
+            lines = [line for line in lines_row if line]
+            df = pd.read_excel(infile, sheet_name=i, header=4)
+            df.drop(df.columns[0], axis=1, inplace=True)
+    
+            for i, line in enumerate(lines):
+                line = line.strip()
+                sta_start, sta_end = line.split(',')
+                sta_start = sta_start.strip()
+                sta_end = sta_end.strip()
+                netsta_start = '.'.join([network, sta_start])
+                netsta_end = '.'.join([network, sta_end])
+                start = np.array(sta_coords[netsta_start])
+                end = np.array(sta_coords[netsta_end])
+                if start.size == 0 or end.size == 0:
+                    msg = f"Can't get coordinates for {netsta_start} or {netsta_end}"
+                    if raise_errors:
+                        raise Exception(msg) 
+                    else:
+                        print(msg)
+                        continue
+                if not np.any(end != start):
+                    msg =  f"Invalid profile line {netsta_start} to {netsta_end}"
+                    if raise_errors:
+                        raise Exception(msg) 
+                    else:
+                        print(msg)
+                        continue
+                dirn = (end - start)
+                dirn = dirn / np.linalg.norm(dirn)
+                dist_col = df.iloc[:, 3*i + 1]
+                dist_col = pd.to_numeric(dist_col, errors='coerce').astype(float)
+                valid = dist_col.notna()
+                if not np.any(valid):
+                    msg = f"No valid values for profile line {netsta_start} to {netsta_end}"
+                    if raise_errors:
+                        raise Exception(msg) 
+                    else:
+                        print(msg)
+                        continue
+                dist = dist_col[valid].values - LEAD_INOUT_DIST_KM
+                depth = df.iloc[:, 3*i + 2][valid].values
+                stations = df.iloc[:, 3*i][valid].values
+                stations = np.array(['.'.join([network, s.strip().split('.')[0]]) for s in stations])
+                lonlat = start + np.outer(dist, dirn)/KM_PER_DEG
+                vol_data = np.hstack((stations[:, np.newaxis], lonlat, depth[:,np.newaxis]))
+                vol_data_list.append(vol_data)
 
     filebase = os.path.splitext(infile)[0]
     outfile = filebase + '.csv'
-    all_data = np.vstack(tuple(v for v in vol_data_dict.values()))
-    np.savetxt(outfile, all_data, fmt=['%s', '%.6f', '%.6f', '%.1f'], delimiter=',',
+    all_data = np.vstack(tuple(v for v in vol_data_list))
+    np.savetxt(outfile, all_data, fmt=['%s', '%s', '%s', '%s'], delimiter=',',
                header='Sta,Lon,Lat,Depth')
-# end func
 
 
 if __name__ == '__main__':
     main()
-# end if
---
+++ seismic/receiver_fn/sandbox/conversion_helper.py
@@ -0,0 +1,7 @@
+# Map spellings of network codes in hand-digitised data to 
+# their FDS equivalent
+NETWORK_CODE_MAPPINGS = {'AQT': 'AQ',
+                         'OA2': 'OA'}
+
+# Characters on station names to remove
+SPECIAL_CHARS = ['*', '.']
