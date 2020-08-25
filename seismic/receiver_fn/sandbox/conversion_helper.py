2b7116a656ab2025603d019bbcdbdb1c5b477512
Bren Moushall <bmoush@gmail.com>
2020-08-25 14:27:56 +1000

    Add weights from file or dict

--- seismic/receiver_fn/sandbox/conversion_helper.py
+++ seismic/receiver_fn/sandbox/conversion_helper.py
@@ -1,3 +1,5 @@
+import os
+
 import click
 import numpy as np
 
@@ -12,22 +14,35 @@ NETWORK_CODE_MAPPINGS = {'AQT': 'AQ',
 SPECIAL_CHARS = ['*', '.']
 
 
-def add_weights(data_file, weights=None):
+def add_weights(data_file, sample_weights=None):
     col_names = ['Sta', 'Lon', 'Lat', 'Depth']
     data = np.genfromtxt(data_file, delimiter=',', dtype=None, encoding=None,
                          names=col_names, usecols=(0, 1, 2, 3))
-    if weights is not None:
-        pass # TODO: weights from file or dict
+
+    weights = np.ones_like(data['Depth'])
+    if sample_weights is not None:
+        if isinstance(sample_weights, (str, os.PathLike)) and os.path.exists(sample_weights):
+            with open(sample_weights, 'r') as fr:
+                lines = fr.readlines()
+            sta_weight = zip(*[l.split() for l in lines])
+        elif isinstance(sample_weights, dict):
+            sta_weight = sample_weights.items()
+
+        for s, w in sample_weights.items():
+            weights[np.where(data['Sta'] == s)] = w
     else:
-        weights = np.ones_like(data['Depth'])
+        print("'sample_weights' not recognised. Must be a file of format 'STATION WEIGHT' or a "
+              "dict of '{STATION: WEIGHT}'. All weights will be set to 1.0")
+            
     data = np.array((data['Sta'], data['Lon'], data['Lat'], data['Depth'], weights)).T
     np.savetxt(data_file, data, fmt=['%s', '%s', '%s', '%s', '%s'], delimiter=',',
                header=','.join(col_names + ['Weight']))
 
 @click.command()
 @click.argument('data_file', type=click.Path(exists=True, dir_okay=False), required=True)
-def main(data_file):
-    add_weights(data_file)
+@click.option('--weights-file', type=click.Path(exists=True, dir_okay=False), required=False)
+def main(data_file, weights_file=None):
+    add_weights(data_file, weights_file)
 
 
 if __name__ == '__main__':
